bluestacks/cts-a16

[SettingsProvider] add more tests for invalid audio URIs
Previous test only tested RINTONE. We should also test ALARM_ALERT and
NOTIFICATION_SOUND.

BUG: 300655880
Test: atest android.provider.cts.settings.Settings_SystemTest

Change-Id: I4fece0cb9c8fe2c11fc4a383391e51c3bfc63ce9

tests/tests/provider/src/android/provider/cts/settings/Settings_SystemTest.java
@@ -204,6 +204,33 @@ public class Settings_SystemTest extends StsExtraBusinessLogicTestCase {
     }
 
     @Test
+    @AsbSecurityTest(cveBugId = 227201030)
+    public void testInvalidAlarmAlertUriIsRejected() {
+        final String originalValue = System.getString(mContentResolver, System.NOTIFICATION_SOUND);
+        final String invalidUri = "content://10@media/external/audio/media/1000000019";
+        try {
+            System.putString(mContentResolver, System.NOTIFICATION_SOUND, invalidUri);
+        } catch (Exception ignored) {
+        }
+        // Assert that the insertion didn't take effect
+        assertThat(System.getString(mContentResolver, System.NOTIFICATION_SOUND))
+                .isEqualTo(originalValue);
+    }
+
+    @Test
+    @AsbSecurityTest(cveBugId = 227201030)
+    public void testInvalidNotificationSoundUriIsRejected() {
+        final String originalValue = System.getString(mContentResolver, System.ALARM_ALERT);
+        final String invalidUri = "content://10@media/external/audio/media/1000000019";
+        try {
+            System.putString(mContentResolver, System.ALARM_ALERT, invalidUri);
+        } catch (Exception ignored) {
+        }
+        // Assert that the insertion didn't take effect
+        assertThat(System.getString(mContentResolver, System.ALARM_ALERT)).isEqualTo(originalValue);
+    }
+
+    @Test
     public void testGetDefaultValues() {
         assertEquals(10, System.getInt(mContentResolver, "int", 10));
         assertEquals(20, System.getLong(mContentResolver, "long", 20L));

msft-mirror-aosp/platform.cts

Merged-In: I4fece0cb9c8fe2c11fc4a383391e51c3bfc63ce9
(cherry picked from commit dc646ffc6f80e4179e2c1fccd8aed023b5e23229)

tests/tests/provider/src/android/provider/cts/settings/Settings_SystemTest.java
@@ -193,6 +193,34 @@ public class Settings_SystemTest extends StsExtraBusinessLogicTestCase {
     }
 
     @Test
+    @AsbSecurityTest(cveBugId = 227201030)
+    public void testInvalidAlarmAlertUriIsRejected() {
+        final ContentResolver cr = InstrumentationRegistry.getTargetContext().getContentResolver();
+        final String originalValue = System.getString(cr, System.NOTIFICATION_SOUND);
+        final String invalidUri = "content://10@media/external/audio/media/1000000019";
+        try {
+            System.putString(cr, System.NOTIFICATION_SOUND, invalidUri);
+        } catch (Exception ignored) {
+        }
+        // Assert that the insertion didn't take effect
+        assertThat(System.getString(cr, System.NOTIFICATION_SOUND)).isEqualTo(originalValue);
+    }
+
+    @Test
+    @AsbSecurityTest(cveBugId = 227201030)
+    public void testInvalidNotificationSoundUriIsRejected() {
+        final ContentResolver cr = InstrumentationRegistry.getTargetContext().getContentResolver();
+        final String originalValue = System.getString(cr, System.ALARM_ALERT);
+        final String invalidUri = "content://10@media/external/audio/media/1000000019";
+        try {
+            System.putString(cr, System.ALARM_ALERT, invalidUri);
+        } catch (Exception ignored) {
+        }
+        // Assert that the insertion didn't take effect
+        assertThat(System.getString(cr, System.ALARM_ALERT)).isEqualTo(originalValue);
+    }
+
+    @Test
     public void testGetDefaultValues() {
         final ContentResolver cr = InstrumentationRegistry.getTargetContext().getContentResolver();
 