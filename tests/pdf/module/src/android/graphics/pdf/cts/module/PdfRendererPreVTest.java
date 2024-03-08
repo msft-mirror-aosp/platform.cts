40f873e2f26e096c78a26b8628045063cf42c3ff
mayankkk <mayankkk@google.com>
2024-03-08 08:49:08 +0000

    Assert TextStartIndex for the search page API. It is required to get the coverage of the POJO PageMatchBound.
    BUG:327031439
    Test: atest CtspdfModuleTestCases
    Change-Id: I980abfdf3805c078fed14b5d1e28ec7289f9685c

--- tests/pdf/module/src/android/graphics/pdf/cts/module/PdfRendererPreVTest.java
+++ tests/pdf/module/src/android/graphics/pdf/cts/module/PdfRendererPreVTest.java
@@ -302,6 +302,7 @@ public class PdfRendererPreVTest {
 
         assertThat(firstPageRects.size()).isEqualTo(1);
         assertThat(firstPageRects.get(0).getBounds().size()).isEqualTo(1);
+        assertThat(firstPageRects.get(0).getTextStartIndex()).isEqualTo(2);
         // the rects area should be less than the page area
         assertThat(calculateArea(firstPageRects)).isLessThan(firstPageHeight * firstPageWidth);
         firstPage.close();
