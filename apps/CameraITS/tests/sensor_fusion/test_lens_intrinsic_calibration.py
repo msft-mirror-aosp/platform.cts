0ac13b43768e8ce0af348abd02b6f09440490bcd
Clemenz Portmann <portmannc@google.com>
2024-03-14 14:45:30 -0700

    ITS: pylint cleanup test_lens_intrinsic_calibration.py
    testing: none
    
    bug: 265435893
    Change-Id: I5f15f76536badf5c9533300568989fc74420b11c

--- apps/CameraITS/tests/sensor_fusion/test_lens_intrinsic_calibration.py
+++ apps/CameraITS/tests/sensor_fusion/test_lens_intrinsic_calibration.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Verify that lens intrinsics changes when OIS is triggered"""
+"""Verify that lens intrinsics changes when OIS is triggered."""
 
 import logging
 import math
