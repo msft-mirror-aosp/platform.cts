39a2cc5827dc2d8aae29eea0ca60263ba7a6eb8b
Preethi Kandhalu <pkandhalu@google.com>
2024-03-21 11:54:01 -0700

    [CSAI CTS] Test multiple APIs in TvAdService
    Bug: 325776138
    Bug: 328161837
    Bug: 328161546
    Bug: 328161086
    Bug: 328160971
    Bug: 326837553
    Bug: 326837021
    Bug: 326836522
    Bug: 326837278
    Bug: 325775969
    Bug: 325776585
    Bug: 325776139
    Bug: 325776209
    Test: atest TvAdServiceTest
    Change-Id: I66eec3c61fb6706136ef9e17f8b24fd26adab9a4

--- tests/tests/tv/src/android/media/tv/ad/cts/TvAdServiceTest.java
+++ tests/tests/tv/src/android/media/tv/ad/cts/TvAdServiceTest.java
@@ -19,23 +19,31 @@ package android.media.tv.ad.cts;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import android.app.Instrumentation;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Rect;
 import android.media.tv.TvInputInfo;
 import android.media.tv.TvInputManager;
+import android.media.tv.TvTrackInfo;
 import android.media.tv.TvView;
 import android.media.tv.ad.TvAdManager;
 import android.media.tv.ad.TvAdServiceInfo;
 import android.media.tv.ad.TvAdView;
+import android.net.Uri;
+import android.os.Bundle;
 import android.os.ConditionVariable;
 import android.platform.test.flag.junit.CheckFlagsRule;
 import android.platform.test.flag.junit.DeviceFlagsValueProvider;
 import android.tv.cts.R;
 import android.view.InputEvent;
+import android.view.KeyEvent;
+import android.view.View;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -50,6 +58,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
 import java.util.concurrent.Executor;
 
 /**
@@ -160,6 +169,139 @@ public class TvAdServiceTest {
     }
 
     @Test
+    public void testSendCurrentTvInputId() {
+        assertNotNull(mSession);
+        mSession.resetValues();
+        String inputId = "inputId";
+        mTvAdView.sendCurrentTvInputId(inputId);
+        PollingCheck.waitFor(TIME_OUT_MS, () -> mSession.mCurrentTvInputIdCount > 0);
+        assertThat(mSession.mCurrentTvInputIdCount).isEqualTo(1);
+        assertThat(mSession.mCurrentTvInputId).isEqualTo(inputId);
+    }
+
+    @Test
+    public void testSendCurrentVideoBounds() {
+        assertNotNull(mSession);
+        mSession.resetValues();
+        Rect rect = new Rect(1, 2, 6, 7);
+        mTvAdView.sendCurrentVideoBounds(rect);
+        PollingCheck.waitFor(TIME_OUT_MS, () -> mSession.mCurrentVideoBoundsCount > 0);
+        assertThat(mSession.mCurrentVideoBoundsCount).isEqualTo(1);
+        assertThat(mSession.mCurrentVideoBounds).isEqualTo(rect);
+    }
+
+    @Test
+    public void testSendCurrentChannelUri() {
+        assertNotNull(mSession);
+        mSession.resetValues();
+        Uri testUri = createTestUri();
+        mTvAdView.sendCurrentChannelUri(testUri);
+        PollingCheck.waitFor(TIME_OUT_MS, () -> mSession.mCurrentChannelUriCount > 0);
+        assertThat(mSession.mCurrentChannelUriCount).isEqualTo(1);
+        assertThat(mSession.mCurrentChannelUri).isEqualTo(testUri);
+    }
+
+    @Test
+    public void testSendSigningResult() {
+        assertNotNull(mSession);
+        mSession.resetValues();
+        String resultId = "id";
+        byte[] resultByte = new byte[1];
+        mTvAdView.sendSigningResult(resultId, resultByte);
+        PollingCheck.waitFor(TIME_OUT_MS, () -> mSession.mSigningResultCount > 0);
+        assertThat(mSession.mSigningResultCount).isEqualTo(1);
+        assertThat(mSession.mSigningResultId).isEqualTo(resultId);
+        assertArrayEquals(mSession.mSigningResultByte, resultByte);
+    }
+
+    @Test
+    public void testSendTrackInfoList() {
+        assertNotNull(mSession);
+        mSession.resetValues();
+        TvTrackInfo testTvTrack =
+                new TvTrackInfo.Builder(TvTrackInfo.TYPE_VIDEO, "testTrackId")
+                        .setVideoWidth(1920)
+                        .setVideoHeight(1080)
+                        .setLanguage("und")
+                        .build();
+        ArrayList<TvTrackInfo> info = new ArrayList<>();
+        info.add(testTvTrack);
+        mTvAdView.sendTrackInfoList(info);
+        PollingCheck.waitFor(TIME_OUT_MS, () -> mSession.mTrackInfoListCount > 0);
+        assertThat(mSession.mTrackInfoListCount).isEqualTo(1);
+        assertThat(mSession.mTvTrackInfo).isEqualTo(info);
+    }
+
+    @Test
+    public void testDispatchUnhandledInputEvent() {
+        final KeyEvent event = new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_I);
+        assertTrue(mTvAdView.dispatchUnhandledInputEvent(event));
+    }
+
+    @Test
+    public void testOnMeasure() {
+        mTvAdView.onMeasure(5, 10);
+    }
+
+    @Test
+    public void testOnUnhandledInputEvent() {
+        final KeyEvent event = new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_Q);
+        mTvAdView.onUnhandledInputEvent(event);
+    }
+
+    @Test
+    public void testOnLayout() {
+        mTvAdView.onLayout(true, 1, 10, 5, 20);
+    }
+
+    @Test
+    public void testOnVisibilityChanged() {
+        mTvAdView.onVisibilityChanged(mTvAdView, View.VISIBLE);
+    }
+
+    @Test
+    public void testNotifyTvMessage() {
+        Bundle testBundle = createTestBundle();
+        assertNotNull(mSession);
+        mSession.resetValues();
+        mTvAdView.notifyTvMessage(TvInputManager.TV_MESSAGE_TYPE_WATERMARK, testBundle);
+        PollingCheck.waitFor(TIME_OUT_MS, () -> mSession.mTvMessageCount > 0);
+        assertThat(mSession.mTvMessageCount).isEqualTo(1);
+        assertThat(mSession.mTvMessageType).isEqualTo(TvInputManager.TV_MESSAGE_TYPE_WATERMARK);
+        assertBundlesAreEqual(mSession.mTvMessageData, testBundle);
+    }
+
+    @Test
+    public void testResetAdService() {
+        assertNotNull(mSession);
+        mSession.resetValues();
+        mTvAdView.resetAdService();
+        PollingCheck.waitFor(TIME_OUT_MS, () -> mSession.mResetAdServiceCount > 0);
+        assertThat(mSession.mResetAdServiceCount).isEqualTo(1);
+    }
+
+    @Test
+    public void testOnDetachedFromWindow() {
+        mTvAdView.onDetachedFromWindow();
+    }
+
+    @Test
+    public void testNotifyError() {
+        assertNotNull(mSession);
+        mSession.resetValues();
+
+        String errorMessage = "msg";
+        Bundle params = new Bundle();
+        mTvAdView.notifyError(errorMessage, params);
+        mInstrumentation.waitForIdleSync();
+        PollingCheck.waitFor(TIME_OUT_MS, () -> mSession.mErrorCount > 0);
+
+        assertThat(mSession.mErrorCount).isEqualTo(1);
+        assertThat(mSession.mErrMessage).isEqualTo(errorMessage);
+        assertBundlesAreEqual(mSession.mErrBundle, params);
+    }
+
+    @Test
     public void testStartAdService() throws Throwable {
         assertNotNull(mSession);
         mSession.resetValues();
@@ -199,6 +341,31 @@ public class TvAdServiceTest {
         });
         if (exceptions[0] != null) {
             throw exceptions[0];
+        }
+    }
+
+    private static Uri createTestUri() {
+        return createTestUri("content://com.example/");
+    }
+
+    private static Uri createTestUri(String uriString) {
+        return Uri.parse(uriString);
+    }
+
+    private static Bundle createTestBundle() {
+        Bundle b = new Bundle();
+        b.putString("stringKey", new String("Test String"));
+        return b;
+    }
+
+    private static void assertBundlesAreEqual(Bundle actual, Bundle expected) {
+        if (expected != null && actual != null) {
+            assertThat(actual.keySet()).isEqualTo(expected.keySet());
+            for (String key : expected.keySet()) {
+                assertThat(actual.get(key)).isEqualTo(expected.get(key));
+            }
+        } else {
+            assertThat(actual).isEqualTo(expected);
         }
     }
 }
