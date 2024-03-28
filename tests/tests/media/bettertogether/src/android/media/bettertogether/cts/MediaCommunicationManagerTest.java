39649af92488e867b43afda8de6aefe67b1b456c
Iván Budnik <ivanbuper@google.com>
2024-03-28 12:40:47 +0000

    Move MediaCommunicationManagerTest into CtsMediaBetterTogetherTestCases
    Test: atest CtsMediaBetterTogetherTestCases
    Bug: 205124386
    Change-Id: Ia386cc1c8a68c3ccee1aba72450f506ffd08ef80

--- tests/tests/media/bettertogether/src/android/media/bettertogether/cts/MediaCommunicationManagerTest.java
+++ tests/tests/media/bettertogether/src/android/media/bettertogether/cts/MediaCommunicationManagerTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package android.media.misc.cts;
+package android.media.bettertogether.cts;
 
 import static android.Manifest.permission.MEDIA_CONTENT_CONTROL;
 
