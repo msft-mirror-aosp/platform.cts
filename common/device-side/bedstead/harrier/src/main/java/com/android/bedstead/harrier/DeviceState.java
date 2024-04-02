a4a8c091577b64f0213160480cf56dd11985ac44
Adekola Akano <adekolaa@google.com>
2024-04-02 09:18:28 +0000

    Fixed bug on PasswordTest
    Fixes: 331245600
    
    Test:   atest CtsDevicePolicyTestCases:android.devicepolicy.cts.PasswordTest
    
    Ignore-AOSP-First: N/A
    Change-Id: Iad6df8d76de9f3b7600dd57692c1df8e37ced02f

--- common/device-side/bedstead/harrier/src/main/java/com/android/bedstead/harrier/DeviceState.java
+++ common/device-side/bedstead/harrier/src/main/java/com/android/bedstead/harrier/DeviceState.java
@@ -3800,7 +3800,7 @@ public final class DeviceState extends HarrierRule {
     private void ensurePasswordSet(UserType forUser, String password) {
         UserReference user = resolveUserTypeToUser(forUser);
 
-        if (user.hasLockCredential()) {
+        if (user.hasLockCredential() && user.lockCredentialEquals(password)) {
             return;
         }
 
