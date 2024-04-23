58f321ee46f784faf29f76178c9e659ba97ced17
Rucha Katakwar <ruchamk@google.com>
2024-04-23 16:43:26 -0700

    ITS:Cap the max preview test size at 1920x1440
    Currently if the device does not support 4K resolution then we use
    max preview size from the supported size.
    This change caps the max preview testing size to 1920x1440 if the device
    does not support the 4K resolution.
    
    Test: Verified locally that if the device does not support 4K then the
    max preview test size returned is 1920x1440 if the device supports it.
    
    Bug: 329352025
    Change-Id: I5722e6288c451d9db6164340a2ac56b9978b7070

--- apps/CameraITS/utils/preview_stabilization_utils.py
+++ apps/CameraITS/utils/preview_stabilization_utils.py
@@ -32,6 +32,8 @@ _MAX_ZOOM_TOL = 0.1   # add Zoom tolerance to enable capture at max zoom
 _MIN_PHONE_MOVEMENT_ANGLE = 5  # degrees
 _NUM_ROTATIONS = 24
 _NUM_STEPS = 10
+_PREVIEW_MAX_TESTED_AREA = 1920 * 1440
+_PREVIEW_MIN_TESTED_AREA = 320 * 240
 _PREVIEW_STABILIZATION_FACTOR = 0.7  # 70% of gyro movement allowed
 _SKIP_INITIAL_FRAMES = 15
 _START_FRAME = 30  # give 3A some frames to warm up
@@ -250,7 +252,7 @@ def get_max_preview_test_size(cam, camera_id):
 
   If the device supports the _HIGH_RES_SIZE preview size then
   it uses that for testing, otherwise uses the max supported
-  preview size.
+  preview size capped at _PREVIEW_MAX_TESTED_AREA.
 
   Args:
     cam: camera object
@@ -269,7 +271,15 @@ def get_max_preview_test_size(cam, camera_id):
   if _HIGH_RES_SIZE in supported_preview_sizes:
     preview_test_size = _HIGH_RES_SIZE
   else:
-    preview_test_size = supported_preview_sizes[-1]
+    capped_supported_preview_sizes = [
+        size
+        for size in supported_preview_sizes
+        if (
+            resolution_to_area(size) <= _PREVIEW_MAX_TESTED_AREA
+            and resolution_to_area(size) >= _PREVIEW_MIN_TESTED_AREA
+        )
+    ]
+    preview_test_size = capped_supported_preview_sizes[-1]
 
   logging.debug('Selected preview resolution: %s', preview_test_size)
 
