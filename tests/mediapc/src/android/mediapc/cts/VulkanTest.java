52e7a83ee4d24fe8ee0f5b4405d55f859d1f95e1
Nick Chalko <nchalko@google.com>
2024-05-07 01:03:56 +0000

    Revert "Use generated classes for req 7.1.4.1/H-1-3"
    This reverts commit cdd7925c11593249fa2cab464c71653e26dfa640.
    
    Reason for revert: breaks build https://android-build.corp.google.com/builds/11807415/branches/git_main/targets/test_suites_x86_64-trunk_staging/cls?end=11807455
    
    Change-Id: I7b503a419373b3095a3b65a83cb51570cfcfad6a

--- tests/mediapc/src/android/mediapc/cts/VulkanTest.java
+++ tests/mediapc/src/android/mediapc/cts/VulkanTest.java
@@ -16,8 +16,7 @@
 package android.mediapc.cts;
 
 import android.mediapc.cts.common.PerformanceClassEvaluator;
-import android.mediapc.cts.common.Requirements;
-import android.mediapc.cts.common.Requirements.VulkanRequirement;
+import android.mediapc.cts.common.VulkanRequirement;
 import android.util.Log;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -75,12 +74,12 @@ public class VulkanTest {
      * {@code VkPhysicalDeviceProtectedMemoryFeatures.protectedMemory} and
      * {@code VK_EXT_global_priority}.
      */
-    @CddTest(requirements = {"7.1.4.1/H-1-3"})
+    @CddTest(requirements = {"7.1.4.1/H-1-2"})
     @Test
     public void checkVulkanProtectedMemoryAndGlobalPrioritySupport() throws Exception {
 
         PerformanceClassEvaluator pce = new PerformanceClassEvaluator(this.mTestName);
-        VulkanRequirement req = pce.addRequirement(Requirements.createR7_1_4_1__H_1_3());
+        VulkanRequirement req = pce.addR7_1_4_1__H_1_3();
 
         var filteredDevices = mVulkanDevices.stream().filter(this::notCpuDevice).toList();
         final boolean extGlobalPriority = filteredDevices.stream().allMatch(
@@ -89,9 +88,9 @@ public class VulkanTest {
         final boolean hasProtectedMemory = filteredDevices.stream().allMatch(
                 this::hasProtectedMemory);
 
-        req.setVkNonCpuDeviceCount(filteredDevices.size());
-        req.setVkPhysicalDeviceProtectedMemory(hasProtectedMemory);
-        req.setVkExtGlobalPriority(extGlobalPriority);
+        req.setNonCpuVulcanDeviceCount(filteredDevices.size());
+        req.setDeviceProtectedMemorySupported(hasProtectedMemory);
+        req.setGlobalPrioritySupported(extGlobalPriority);
 
         pce.submitAndCheck();
     }
