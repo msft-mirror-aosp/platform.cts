9b9d4f54536de03ef538d5c856d093d4bd49ea04
Sanjana Sunil <sanjanasunil@google.com>
2024-05-14 14:54:19 +0000

    Allow activity start for animation CTS tests in the sandbox
    See go/sdk-sandbox-cts-tag and go/cts-in-sandbox-how for more details
    about running tests in the SDK sandbox mode (does not affect tests
    running in regular app mode).
    
    Starting activities in the SDK sandbox are normally not allowed.
    However, for the purposes of CTS testing, they can be temporarily
    enabled to allow the test to proceed. This requires a
    START_ACTIVITIES_FROM_SDK_SANDBOX permission. Modify any CTS tests that
    fail when run in the sandbox mode to allow the activity to be started.
    
    Additionally, mark the activity as exported so that the sandbox process
    can see this activity.
    
    Bug: 319428537
    Test: atest CtsAnimationTestCases -- --enable-optional-parameterization --enable-parameterized-modules --module-parameter run_on_sdk_sandbox
    Change-Id: I359bb1e039fb3af8f93212a9464e90348fa945ca

--- tests/tests/animation/src/android/animation/cts/PropertyValuesHolderTest.java
+++ tests/tests/animation/src/android/animation/cts/PropertyValuesHolderTest.java
@@ -48,6 +48,8 @@ import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
+import com.android.compatibility.common.util.AdoptShellPermissionsRule;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -79,7 +81,14 @@ public class PropertyValuesHolderTest {
     private Object mObject;
     private String mProperty;
 
-    @Rule
+    @Rule(order = 0)
+    public AdoptShellPermissionsRule mAdoptShellPermissionsRule =
+            new AdoptShellPermissionsRule(
+                    androidx.test.platform.app.InstrumentationRegistry
+                            .getInstrumentation().getUiAutomation(),
+                    android.Manifest.permission.START_ACTIVITIES_FROM_SDK_SANDBOX);
+
+    @Rule(order = 1)
     public ActivityTestRule<AnimationActivity> mActivityRule =
             new ActivityTestRule<>(AnimationActivity.class);
 
