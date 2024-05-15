15d4eb10948527424896e08e0749b81652dc4cb2
Eghosa Ewansiha-Vlachavas <eevlachavas@google.com>
2024-05-15 14:16:03 +0000

    [3/n] Re-enable tests that were disabled as part of desktop mode flag rollout
    Bug: 343179709
    Fixes: 339969180
    Fixes: 339936650
    Test: atest CtsWindowManagerDeviceActivity:ActivityLifecycleFreeformTests
    Test: atest CtsWindowManagerDeviceAnimations:ManifestLayoutTests
    Test: atest CtsWindowManagerDeviceAnimations:SplashscreenTests
    Test: atest CtsWindowManagerDeviceWindow:CrossAppDragAndDropTests
    Test: atest CtsWindowManagerDeviceWindow:WindowMetricsActivityTests
    Test: atest CtsWindowManagerDeviceDisplay:FreeformWindowingModeTests
    
    Change-Id: I29d2c0e87cc64f1adec669cba8da5199fb5b4775

--- tests/framework/base/windowmanager/src/android/server/wm/animations/ManifestLayoutTests.java
+++ tests/framework/base/windowmanager/src/android/server/wm/animations/ManifestLayoutTests.java
@@ -33,7 +33,6 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeFalse;
 import static org.junit.Assume.assumeTrue;
 
 import android.content.ComponentName;
@@ -45,8 +44,6 @@ import android.server.wm.WindowManagerState.WindowState;
 import android.util.DisplayMetrics;
 import android.view.DisplayCutout;
 import android.view.WindowMetrics;
-
-import com.android.window.flags.Flags;
 
 import org.junit.Test;
 
@@ -79,45 +76,21 @@ public class ManifestLayoutTests extends ActivityManagerTestBase {
 
     @Test
     public void testGravityAndDefaultSizeTopLeft() throws Exception {
-        // TODO(b/340172101): Remove check once desktopmode windows support activity layout
-        //  declaring gravity, minWidth/minHeight, and defaultWidth/defaultHeight.
-        // Ignore test if desktop windowing is enabled on tablets as legacy freeform window
-        // behaviour will not be respected
-        assumeFalse(Flags.enableDesktopWindowingMode() && isTablet());
-
         testLayout(GRAVITY_VER_TOP, GRAVITY_HOR_LEFT, false /*fraction*/);
     }
 
     @Test
     public void testGravityAndDefaultSizeTopRight() throws Exception {
-        // TODO(b/340172101): Remove check once desktopmode windows support activity layout
-        //  declaring gravity, minWidth/minHeight, and defaultWidth/defaultHeight.
-        // Ignore test if desktop windowing is enabled on tablets as legacy freeform window
-        // behaviour will not be respected
-        assumeFalse(Flags.enableDesktopWindowingMode() && isTablet());
-
         testLayout(GRAVITY_VER_TOP, GRAVITY_HOR_RIGHT, true /*fraction*/);
     }
 
     @Test
     public void testGravityAndDefaultSizeBottomLeft() throws Exception {
-        // TODO(b/340172101): Remove check once desktopmode windows support activity layout
-        //  declaring gravity, minWidth/minHeight, and defaultWidth/defaultHeight.
-        // Ignore test if desktop windowing is enabled on tablets as legacy freeform window
-        // behaviour will not be respected
-        assumeFalse(Flags.enableDesktopWindowingMode() && isTablet());
-
         testLayout(GRAVITY_VER_BOTTOM, GRAVITY_HOR_LEFT, true /*fraction*/);
     }
 
     @Test
     public void testGravityAndDefaultSizeBottomRight() throws Exception {
-        // TODO(b/340172101): Remove check once desktopmode windows support activity layout
-        //  declaring gravity, minWidth/minHeight, and defaultWidth/defaultHeight.
-        // Ignore test if desktop windowing is enabled on tablets as legacy freeform window
-        // behaviour will not be respected
-        assumeFalse(Flags.enableDesktopWindowingMode() && isTablet());
-
         testLayout(GRAVITY_VER_BOTTOM, GRAVITY_HOR_RIGHT, false /*fraction*/);
     }
 
