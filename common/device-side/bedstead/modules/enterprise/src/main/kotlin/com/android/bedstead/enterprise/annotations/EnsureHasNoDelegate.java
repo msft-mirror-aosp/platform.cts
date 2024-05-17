1e13df302b3820e7a2434a4e0182d436ab2fe86b
Piotr Karzelek <karzelek@google.com>
2024-05-17 09:40:53 +0000

    move code handling enterprise annotations to enterprise module
    Test: atest bedstead-enterprise-test
    Test: atest HarrierTest:com.android.bedstead.harrier.DeviceStateTest
    Flag: TEST_ONLY
    Bug: 336991736
    Change-Id: Ia31a2dc7ea2f74984b0948b60989c1d8b99f4ed5

--- common/device-side/bedstead/modules/enterprise/src/main/kotlin/com/android/bedstead/enterprise/annotations/EnsureHasNoDelegate.java
+++ common/device-side/bedstead/modules/enterprise/src/main/kotlin/com/android/bedstead/enterprise/annotations/EnsureHasNoDelegate.java
@@ -20,6 +20,7 @@ import static com.android.bedstead.enterprise.annotations.EnsureHasDelegate.ENSU
 
 import com.android.bedstead.harrier.annotations.AnnotationPriorityRunPrecedence;
 import com.android.bedstead.harrier.annotations.RequireNotInstantApp;
+import com.android.bedstead.harrier.annotations.UsesAnnotationExecutor;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -36,6 +37,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 // TODO(b/206441366): Add instant app support
 @RequireNotInstantApp(reason = "Instant Apps cannot run Enterprise Tests")
+@UsesAnnotationExecutor(UsesAnnotationExecutor.ENTERPRISE)
 public @interface EnsureHasNoDelegate {
 
     enum AdminType {
