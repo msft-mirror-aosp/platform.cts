31dca3c1aa710f87a1aab2c94e710eea153885f8
Hui Wang <huiwang@google.com>
2024-05-21 18:01:03 -0500

    Skip emergency call related test if no telephony feature defined
    Bug: 331876185
    Test: atest android.telecom.cts.ThirdPartyCallScreeningServiceTest
    Change-Id: I26a330c671883fb46fc8065a0fa0b85fba163526

--- tests/tests/telecom/src/android/telecom/cts/ThirdPartyCallScreeningServiceTest.java
+++ tests/tests/telecom/src/android/telecom/cts/ThirdPartyCallScreeningServiceTest.java
@@ -16,6 +16,7 @@
 
 package android.telecom.cts;
 
+import static android.telecom.cts.TestUtils.hasTelephonyFeature;
 import static android.telecom.cts.TestUtils.shouldTestTelecom;
 import static android.telecom.cts.TestUtils.waitOnAllHandlers;
 
@@ -331,7 +332,7 @@ public class ThirdPartyCallScreeningServiceTest extends BaseTelecomTestWithMockS
     }
 
     public void testNoPostCallActivityForOutgoingEmergencyCall() throws Exception {
-        if (!shouldTestTelecom(mContext)) {
+        if (!shouldTestTelecom(mContext) || !hasTelephonyFeature(mContext)) {
             return;
         }
 
@@ -347,7 +348,7 @@ public class ThirdPartyCallScreeningServiceTest extends BaseTelecomTestWithMockS
     }
 
     public void testNoPostCallActivityForIncomingEmergencyCall() throws Exception {
-        if (!shouldTestTelecom(mContext)) {
+        if (!shouldTestTelecom(mContext) || !hasTelephonyFeature(mContext)) {
             return;
         }
         setupForEmergencyCalling(TEST_EMERGENCY_NUMBER);
