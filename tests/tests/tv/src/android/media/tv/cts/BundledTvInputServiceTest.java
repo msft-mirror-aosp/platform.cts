12c1200a32f83f02e531be752958fefe546ffef3
Preethi Kandhalu <pkandhalu@google.com>
2024-05-30 12:11:14 -0700

    [Trunk Stable] Separating out Tuner target
    Test: atest CtsTvTunerTestCases
    
    Change-Id: I29fbb199a7920f8937413c98178a7f800a94f9d1

--- tests/tests/tv/src/android/media/tv/cts/BundledTvInputServiceTest.java
+++ tests/tests/tv/src/android/media/tv/cts/BundledTvInputServiceTest.java
@@ -25,9 +25,8 @@ import android.media.tv.TvInputManager;
 import android.media.tv.TvView;
 import android.platform.test.annotations.Presubmit;
 import android.test.ActivityInstrumentationTestCase2;
-import android.util.ArrayMap;
-
 import android.tv.cts.R;
+import android.util.ArrayMap;
 
 import com.android.compatibility.common.util.PollingCheck;
 
--- tests/tests/tv/src/android/media/tv/tuner/cts/SharedFilterTestService.java
+++ tests/tests/tv/src/android/media/tv/tuner/cts/SharedFilterTestService.java
@@ -19,9 +19,8 @@ package android.media.tv.tuner.cts;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.media.tv.tuner.dvr.DvrPlayback;
 import android.media.tv.tuner.Tuner;
-import android.media.tv.tuner.cts.ISharedFilterTestServer;
+import android.media.tv.tuner.dvr.DvrPlayback;
 import android.media.tv.tuner.dvr.OnPlaybackStatusChangedListener;
 import android.media.tv.tuner.filter.Filter;
 import android.media.tv.tuner.filter.FilterCallback;
@@ -31,7 +30,6 @@ import android.media.tv.tuner.filter.SharedFilterCallback;
 import android.os.IBinder;
 import android.util.Log;
 
-import java.util.List;
 import java.util.concurrent.Executor;
 
 public class SharedFilterTestService extends Service {
