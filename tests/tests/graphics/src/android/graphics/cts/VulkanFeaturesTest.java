7fb8d8f26a9812f219e33a58cd4c4ab16372bfb4
Chris Forbes <chrisforbes@google.com>
2024-06-25 09:24:16 +1200

    Update VulkanFeaturesTest extension lists
    Adds a number of additional extensions which are testable by
    vulkan-cts-1.3.8 at the Android V conformance level.
    
    Bug: b/343304468
    Flag: TEST_ONLY
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:4043c1cbdd8cd287f3fa60b9cb8b49d15961bdd9)
    Merged-In: I073f0173ba85924a0ca0346ae9e3864c17d7f1a1
    Change-Id: I073f0173ba85924a0ca0346ae9e3864c17d7f1a1

--- tests/tests/graphics/src/android/graphics/cts/VulkanFeaturesTest.java
+++ tests/tests/graphics/src/android/graphics/cts/VulkanFeaturesTest.java
@@ -117,7 +117,16 @@ public class VulkanFeaturesTest {
                     "VK_KHR_shader_expect_assume",
                     "VK_KHR_shader_quad_control",
                     "VK_KHR_vertex_attribute_divisor",
-                    "VK_ANDROID_external_format_resolve"});
+                    "VK_ANDROID_external_format_resolve",
+                    "VK_KHR_dynamic_rendering_local_read",
+                    "VK_KHR_shader_float_controls2",
+                    "VK_KHR_shader_maximal_reconvergence",
+                    "VK_KHR_shader_subgroup_rotate",
+                    "VK_KHR_video_decode_av1",
+                    "VK_KHR_video_encode_h264",
+                    "VK_KHR_video_encode_h265",
+                    "VK_KHR_video_encode_queue",
+                    "VK_KHR_video_maintenance1"});
         DEQP_EXTENSIONS_MAP.put(
                 DEQP_LEVEL_FOR_U,
                 new String[] {
@@ -417,7 +426,7 @@ public class VulkanFeaturesTest {
                     "externalFenceFeatures", 0x3 /* importable + exportable */));
     }
 
-    @CddTest(requirement = "7.1.4.2/C-1-7, 3.3.1/C-0-12")
+    @CddTest(requirement = "7.1.4.2/C-1-7,3.3.1/C-0-12")
     @Test
     public void testVulkanRequiredExtensions() throws JSONException {
         assumeTrue("Skipping because Vulkan is not supported", mVulkanDevices.length > 0);
