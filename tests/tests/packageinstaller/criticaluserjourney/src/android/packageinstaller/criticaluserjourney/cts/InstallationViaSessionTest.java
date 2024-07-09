e09235ea69c2c93514b1e25a058d1de8e16d9959
Ivan Chiang <chiangi@google.com>
2024-07-09 02:39:28 +0000

    [PM] Add PackageInstaller CUJ test case (13/N)
    - Copy PackageInstallerCujTestBase to new InstallationTestBase class.
    - Make some fields and methods to be public
    
    Flag: TEST_ONLY
    Bug: 332322946
    Test: atest CtsPackageInstallerCUJTestCases
    Change-Id: Ib66ba7928480c19ac108f794a1dab0ccb1c912a4

--- tests/tests/packageinstaller/criticaluserjourney/src/android/packageinstaller/criticaluserjourney/cts/InstallationViaSessionTest.java
+++ tests/tests/packageinstaller/criticaluserjourney/src/android/packageinstaller/criticaluserjourney/cts/InstallationViaSessionTest.java
@@ -28,7 +28,7 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 @AppModeFull
-public class InstallationViaSessionTest extends PackageInstallerCujTestBase {
+public class InstallationViaSessionTest extends InstallationTestBase {
 
     @Test
     public void newInstall_launchGrantPermission_installButton_success() throws Exception {
