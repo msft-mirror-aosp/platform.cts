13324006dbbe3ffb34b723ce0b9bc43f2ac6d7e7
Gen (Daniel) Li <genli@google.com>
2024-07-24 21:30:05 +0000

    Add SystemUiHelper#hasNoTraditionalStatusBar
    Some CTS tests don't apply to devices with no traditional status bar.
    Examples of such devices are Android TV, Wear OS, etc.  Currently
    these tests rely on device form-factor feature flags to gate test
    execution when this should be a system API instead.
    
    This CL adds SystemUiHelper#hasNoTraditionalStatusBar to indicate
    the lack of a status bar until an SDK API is available to do the same.
    
    Flag: persist.sysui.nostatusbar identifies platforms that do not
    implement the traditionl android status bar.
    
    Bug: 322222011
    Test: Test passes on devices that match criteria.
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:480c852638e155cd10451c9ae6f42a16a5a46799)
    Merged-In: I29ae4cf862e8285c890498dfd2f6ef15d806dc07
    Change-Id: I29ae4cf862e8285c890498dfd2f6ef15d806dc07

--- tests/tests/notificationlegacy/notificationlegacy29/src/android/app/notification/legacy29/cts/NotificationAssistantServiceTest.java
+++ tests/tests/notificationlegacy/notificationlegacy29/src/android/app/notification/legacy29/cts/NotificationAssistantServiceTest.java
@@ -21,6 +21,8 @@ import static android.Manifest.permission.REVOKE_POST_NOTIFICATIONS_WITHOUT_KILL
 import static android.Manifest.permission.REVOKE_RUNTIME_PERMISSIONS;
 import static android.service.notification.NotificationAssistantService.FEEDBACK_RATING;
 
+import static com.android.compatibility.common.preconditions.SystemUiHelper.hasNoTraditionalStatusBar;
+
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 import static junit.framework.TestCase.assertFalse;
@@ -43,7 +45,6 @@ import android.app.stubs.shared.TestNotificationAssistant;
 import android.app.stubs.shared.TestNotificationListener;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Process;
 import android.os.SystemClock;
@@ -88,10 +89,6 @@ public class NotificationAssistantServiceTest {
     private UiAutomation mUi;
     private NotificationHelper mHelper;
     private String mPreviousAssistant;
-
-    private boolean isWatch() {
-      return mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_WATCH);
-    }
 
     @Before
     public void setUp() throws Exception {
@@ -545,7 +542,7 @@ public class NotificationAssistantServiceTest {
 
     @Test
     public void testOnNotificationVisibilityChanged() throws Exception {
-        assumeFalse("Status bar service not supported", isWatch() || isTelevision());
+        assumeFalse("Status bar service not supported", hasNoTraditionalStatusBar(mContext));
         setUpListeners();
         turnScreenOn();
         mUi.adoptShellPermissionIdentity("android.permission.EXPAND_STATUS_BAR");
@@ -585,7 +582,7 @@ public class NotificationAssistantServiceTest {
 
     @Test
     public void testOnNotificationClicked() throws Exception {
-        assumeFalse("Status bar service not supported", isWatch() || isTelevision());
+        assumeFalse("Status bar service not supported", hasNoTraditionalStatusBar(mContext));
 
         setUpListeners();
         turnScreenOn();
@@ -613,7 +610,7 @@ public class NotificationAssistantServiceTest {
 
     @Test
     public void testOnNotificationFeedbackReceived() throws Exception {
-        assumeFalse("Status bar service not supported", isWatch());
+        assumeFalse("Status bar service not supported", hasNoTraditionalStatusBar(mContext));
 
         setUpListeners(); // also enables assistant
         mUi.adoptShellPermissionIdentity("android.permission.STATUS_BAR_SERVICE",
@@ -705,13 +702,6 @@ public class NotificationAssistantServiceTest {
         Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
         mHelper.runCommand("input keyevent KEYCODE_WAKEUP", instrumentation);
         mHelper.runCommand("wm dismiss-keyguard", instrumentation);
-    }
-
-    private boolean isTelevision() {
-        PackageManager packageManager = mContext.getPackageManager();
-        return packageManager != null
-                && (packageManager.hasSystemFeature(PackageManager.FEATURE_LEANBACK)
-                || packageManager.hasSystemFeature(PackageManager.FEATURE_TELEVISION));
     }
 
     private int getAssistantCancellationReason(String key) {
