6e4257927617870021bab04c798b7eff1207c7f3
Hyosun <hyosunkim@google.com>
2024-08-13 03:39:55 +0000

    Update APIs which is the provisioning per SubscriberId related according to the design doc changes.
    1. Rename requestProvisionSubscriberIds
    2. Remove api requestIsProvisioned
    
    Bug: 359366004
    Test: atest SatelliteManagerTest, SatelliteManagerTestOnMockService
    Flag: com.android.internal.telephony.flags.carrier_roaming_nb_iot_ntn
    
    Change-Id: I815ac6145c6401154cb20e1318643ec53319e2d0

--- tests/tests/telephony/current/src/android/telephony/satellite/cts/SatelliteManagerTest.java
+++ tests/tests/telephony/current/src/android/telephony/satellite/cts/SatelliteManagerTest.java
@@ -17,6 +17,7 @@
 package android.telephony.satellite.cts;
 
 import static android.telephony.satellite.SatelliteManager.SATELLITE_COMMUNICATION_RESTRICTION_REASON_GEOLOCATION;
+import static android.telephony.satellite.SatelliteSubscriberInfo.IMSI_MSISDN;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -32,6 +33,7 @@ import android.os.OutcomeReceiver;
 import android.platform.test.annotations.RequiresFlagsEnabled;
 import android.platform.test.flag.junit.CheckFlagsRule;
 import android.platform.test.flag.junit.DeviceFlagsValueProvider;
+import android.telephony.SubscriptionManager;
 import android.telephony.satellite.EnableRequestAttributes;
 import android.telephony.satellite.NtnSignalStrength;
 import android.telephony.satellite.NtnSignalStrengthCallback;
@@ -40,6 +42,7 @@ import android.telephony.satellite.SatelliteCapabilitiesCallback;
 import android.telephony.satellite.SatelliteDatagram;
 import android.telephony.satellite.SatelliteManager;
 import android.telephony.satellite.SatelliteSubscriberInfo;
+import android.telephony.satellite.SatelliteSubscriberProvisionStatus;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -902,16 +905,18 @@ public class SatelliteManagerTest extends SatelliteManagerTestBase {
 
     @Test
     @RequiresFlagsEnabled(Flags.FLAG_CARRIER_ROAMING_NB_IOT_NTN)
-    public void testRequestProvisionSubscriberIds() {
+    public void testRequestSatelliteSubscriberProvisionStatus() {
         if (!shouldTestSatellite()) return;
 
-        final AtomicReference<List<SatelliteSubscriberInfo>> enabled = new AtomicReference<>();
+        final AtomicReference<List<SatelliteSubscriberProvisionStatus>> enabled =
+                new AtomicReference<>();
         final AtomicReference<Integer> errorCode = new AtomicReference<>();
-        OutcomeReceiver<List<SatelliteSubscriberInfo>, SatelliteManager.SatelliteException>
+        OutcomeReceiver<List<SatelliteSubscriberProvisionStatus>,
+                SatelliteManager.SatelliteException>
                 receiver =
                 new OutcomeReceiver<>() {
                     @Override
-                    public void onResult(List<SatelliteSubscriberInfo> result) {
+                    public void onResult(List<SatelliteSubscriberProvisionStatus> result) {
                         Log.d(TAG, "onResult: result.size=" + result.size());
                         enabled.set(result);
                     }
@@ -925,36 +930,7 @@ public class SatelliteManagerTest extends SatelliteManagerTestBase {
 
         // Throws SecurityException as we do not have SATELLITE_COMMUNICATION permission.
         assertThrows(SecurityException.class,
-                () -> sSatelliteManager.requestProvisionSubscriberIds(
-                        getContext().getMainExecutor(), receiver));
-    }
-
-    @Test
-    @RequiresFlagsEnabled(Flags.FLAG_CARRIER_ROAMING_NB_IOT_NTN)
-    public void testRequestIsProvisioned() {
-        if (!shouldTestSatellite()) return;
-
-        final AtomicReference<Boolean> enabled = new AtomicReference<>();
-        final AtomicReference<Integer> errorCode = new AtomicReference<>();
-        final String subscriberId = "12345";
-        OutcomeReceiver<Boolean, SatelliteManager.SatelliteException> receiver =
-                new OutcomeReceiver<>() {
-                    @Override
-                    public void onResult(Boolean result) {
-                        Log.d(TAG, "onResult: result=" + result);
-                        enabled.set(result);
-                    }
-
-                    @Override
-                    public void onError(SatelliteManager.SatelliteException exception) {
-                        Log.d(TAG, "onError: onError=" + exception);
-                        errorCode.set(exception.getErrorCode());
-                    }
-                };
-
-        // Throws SecurityException as we do not have SATELLITE_COMMUNICATION permission.
-        assertThrows(SecurityException.class,
-                () -> sSatelliteManager.requestIsProvisioned(subscriberId,
+                () -> sSatelliteManager.requestSatelliteSubscriberProvisionStatus(
                         getContext().getMainExecutor(), receiver));
     }
 
@@ -965,8 +941,13 @@ public class SatelliteManagerTest extends SatelliteManagerTestBase {
 
         final AtomicReference<Boolean> enabled = new AtomicReference<>();
         final AtomicReference<Integer> errorCode = new AtomicReference<>();
+        final int slotId0 = 0;
+        final int idType = IMSI_MSISDN;
         final List<SatelliteSubscriberInfo> list = new ArrayList<>(
-                Collections.singleton(new SatelliteSubscriberInfo("09876543", 12345, "")));
+                Collections.singleton(new SatelliteSubscriberInfo.Builder()
+                        .setSubscriberId("09876543").setCarrierId(12345).setNiddApn("")
+                        .setSubId(SubscriptionManager.getSubscriptionId(
+                                slotId0)).setSubscriberIdType(idType).build()));
         OutcomeReceiver<Boolean, SatelliteManager.SatelliteException> receiver =
                 new OutcomeReceiver<>() {
                     @Override
