cc87c767a24b028acf49ced6e630e6f53a9f71b8
Tomasz Wasilczyk <twasilczyk@google.com>
2024-09-06 11:09:55 -0700

    Telecom: drop TestUtils.waitForCount(timeout)
    This method is flake-prone: it's equivalent of taking current call count
    and waiting for it to increase by 1. But in all use cases it happens after
    trigger (callback setup) has already happened, so we're waiting for the
    other thread to catch up (which might have already happened).
    
    This also fixes IndexOutOfBoundsException if timeout hits without satisfying
    counter tick condition.
    
    Bug: 365146607
    Test: atest CtsTelecomTestCases:android.telecom.cts.ExtendedInCallServiceTest#testSwitchCallEndpoint
          on device without telephony
    Flag: TEST_ONLY
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:c351f3f32629a745712070b9f9622951f513aeda
    Merged-In: I4fef361ee0aff3024a790665216bc41927435e8f
    Change-Id: I4fef361ee0aff3024a790665216bc41927435e8f

--- tests/tests/telecom/src/android/telecom/cts/TestUtils.java
+++ tests/tests/telecom/src/android/telecom/cts/TestUtils.java
@@ -43,6 +43,7 @@ import android.telecom.VideoProfile;
 
 import androidx.test.InstrumentationRegistry;
 
+import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
 
 import java.io.BufferedReader;
@@ -862,16 +863,6 @@ public class TestUtils {
             waitForCount(count, timeoutMillis, null);
         }
 
-        public void waitForCount(long timeoutMillis) {
-             synchronized (mLock) {
-             try {
-                  mLock.wait(timeoutMillis);
-             }catch (InterruptedException ex) {
-                  ex.printStackTrace();
-             }
-           }
-        }
-
         public void waitForCount(int count, long timeoutMillis, String message) {
             synchronized (mLock) {
                 final long startTimeMillis = SystemClock.uptimeMillis();
@@ -892,6 +883,19 @@ public class TestUtils {
                         /* ignore */
                     }
                 }
+            }
+        }
+
+        /**
+         * Try waiting for count to reach desired number, but instead of failing test on timeout,
+         * return false silently.
+         */
+        public boolean tryWaitForCount(int count, long timeoutMillis) {
+            try {
+                waitForCount(count, timeoutMillis);
+                return true;
+            } catch (AssertionFailedError e) {
+                return false;
             }
         }
 
