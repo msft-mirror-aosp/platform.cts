a1c291dd84c9b75f774bcc82c35206bb4ce4d432
ziyiw <ziyiw@google.com>
2024-09-20 21:15:29 +0000

    [cts] Add cts tests for more extension APIs.
    Bug: 338255533
    Test: atest CtsNfcTestCases
    Change-Id: I8e3d6f977e08991542e765a7e386c5c38392eaf9

--- tests/tests/nfc/src/android/nfc/cts/NfcAdapterTest.java
+++ tests/tests/nfc/src/android/nfc/cts/NfcAdapterTest.java
@@ -698,6 +698,11 @@ public class NfcAdapterTest {
             nfcOemExtension.clearPreference();
             nfcOemExtension.synchronizeScreenState();
             assertThat(nfcOemExtension.getActiveNfceeList()).isNotEmpty();
+            nfcOemExtension.triggerInitialization();
+            nfcOemExtension.hasUserEnabledNfc();
+            nfcOemExtension.isTagPresent();
+            nfcOemExtension.pausePolling(1000);
+            nfcOemExtension.resumePolling();
         } finally {
             nfcOemExtension.unregisterCallback(cb);
         }
