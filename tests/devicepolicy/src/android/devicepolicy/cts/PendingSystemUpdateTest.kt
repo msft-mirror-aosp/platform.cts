db22422eb79dfd0a168151b5975359a524b2c1eb
Pavel Grafov <pgrafov@google.com>
2024-09-20 10:29:30 +0000

    Clean up fully rolled out PermissionMigrationForZeroTrustImplEnabled
    Flag was rolled out in Android V.
    
    Bug: 364998990
    Test: TH
    Flag: EXEMPT flag cleanup
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:dfdf7fb5acd44bcccb2d119793727683fe1654a7)
    Merged-In: I25f472268b075d79cc0a0378a55b515694109cbe
    Change-Id: I25f472268b075d79cc0a0378a55b515694109cbe

--- tests/devicepolicy/src/android/devicepolicy/cts/PendingSystemUpdateTest.kt
+++ tests/devicepolicy/src/android/devicepolicy/cts/PendingSystemUpdateTest.kt
@@ -3,22 +3,19 @@ package android.devicepolicy.cts
 import android.app.admin.SystemUpdateInfo.SECURITY_PATCH_STATE_FALSE
 import android.app.admin.SystemUpdateInfo.SECURITY_PATCH_STATE_TRUE
 import android.app.admin.SystemUpdateInfo.SECURITY_PATCH_STATE_UNKNOWN
-import android.app.admin.flags.Flags
 import android.content.pm.PackageManager.FEATURE_DEVICE_ADMIN
 import android.content.pm.PackageManager.FEATURE_MANAGED_USERS
+import com.android.bedstead.enterprise.annotations.CanSetPolicyTest
+import com.android.bedstead.enterprise.annotations.CannotSetPolicyTest
 import com.android.bedstead.harrier.BedsteadJUnit4
 import com.android.bedstead.harrier.DeviceState
 import com.android.bedstead.harrier.annotations.RequireFeature
-import com.android.bedstead.enterprise.annotations.CanSetPolicyTest
-import com.android.bedstead.enterprise.annotations.CannotSetPolicyTest
 import com.android.bedstead.harrier.policies.ReceiveSystemUpdateCallback
 import com.android.bedstead.nene.TestApis
 import com.android.bedstead.nene.utils.Assert.assertThrows
 import com.android.compatibility.common.util.ApiTest
 import com.android.eventlib.truth.EventLogsSubject.assertThat
 import com.google.common.truth.Truth.assertThat
-import org.junit.Assume.assumeTrue
-import org.junit.Before
 import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.runner.RunWith
@@ -30,22 +27,6 @@ import org.junit.runner.RunWith
 // Always filter by the 'receivedTime' since there can be false positives where the real system
 // update service triggers the callback on the device.
 class PendingSystemUpdateTest {
-
-    // TODO(324373863): Replace with infra support for flagged policy changes
-    @Before
-    fun skipDpmRoleHolderTestIfFlagIsNotEnabled() {
-        try {
-            if (deviceState.dpc() == deviceState.dpmRoleHolder()) {
-                val flagIsEnabled = Flags.permissionMigrationForZeroTrustImplEnabled()
-                assumeTrue("This test only runs with flag "
-                        + Flags.FLAG_PERMISSION_MIGRATION_FOR_ZERO_TRUST_IMPL_ENABLED
-                        + " is enabled", flagIsEnabled)
-            }
-        } catch (e: IllegalStateException) {
-            // Fine - we don't have one
-        }
-    }
-
     @ApiTest(
         apis = ["android.app.admin.DevicePolicyManager#notifyPendingSystemUpdate",
             "android.app.admin.DeviceAdminReceiver#onSystemUpdatePending"]
