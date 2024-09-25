56751361163ceb25ced81d9f6016e591d0608a11
Calvin Huang <calhuang@google.com>
2024-09-24 17:37:35 -0700

    Fix SurfaceControlInputReceiverTests for device with persistent system bars
    On some auto devices, status bar and navigation bar persist even if the
    activity reqeusts to hide them.
    
    So apply offset to the child surface to avoid drawning the surface under
    the status bar.
    
    Bug: 366360567
    Test: atest SurfaceControlInputReceiverTests
    Flag: EXEMPT bug fix
    Change-Id: I64efefd6e45b4b0fa571bb242b952ca83f47a055

--- tests/surfacecontrol/src/android/view/surfacecontrol/cts/SurfaceControlInputReceiverTests.java
+++ tests/surfacecontrol/src/android/view/surfacecontrol/cts/SurfaceControlInputReceiverTests.java
@@ -41,6 +41,7 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Insets;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.IBinder;
@@ -58,6 +59,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.view.cts.util.EmbeddedSCVHService;
 import android.view.cts.util.aidl.IAttachEmbeddedWindow;
@@ -550,8 +552,14 @@ public class SurfaceControlInputReceiverTests {
                 return;
             }
 
+            // On some devices, hiding system bars is disabled. In those cases, apply offset to
+            // the child surface control to ensure the surface is drawn out side of system bar area.
+            Insets insets = getRootWindowInsets().getInsets(WindowInsets.Type.systemBars());
+            float xPosition = insets.left + (float) getWidth() / 2;
+            float yPosition = insets.top + (float) getHeight() / 2;
+
             t.setLayer(mChild, 1).setVisibility(mChild, true).setCrop(mChild, sBounds)
-                    .setPosition(mChild, (float) getWidth() / 2, (float) getHeight() / 2);
+                    .setPosition(mChild, xPosition, yPosition);
             t.addTransactionCommittedListener(Runnable::run, mDrawCompleteLatch::countDown);
             getRootSurfaceControl().applyTransactionOnDraw(t);
             mChildScAttached = true;
@@ -717,7 +725,6 @@ public class SurfaceControlInputReceiverTests {
                     embeddedServiceReady.await(WAIT_TIME_S, TimeUnit.SECONDS));
             assertTrue("Failed to create SurfaceView SurfaceControl",
                     surfaceViewCreatedLatch.await(WAIT_TIME_S, TimeUnit.SECONDS));
-
             mEmbeddedName = mIAttachEmbeddedWindow.attachEmbeddedSurfaceControl(
                     surfaceView.getSurfaceControl(),
                     surfaceView.getRootSurfaceControl().getInputTransferToken(), sBounds.width(),
