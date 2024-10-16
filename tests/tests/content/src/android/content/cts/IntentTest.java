e146c4ca9032f4733c72acda7fae806b93474d51
Nan Wu <wnan@google.com>
2024-10-16 00:37:51 +0000

    Add test for new API to allow app to opt out Intent Redirect Prevention
    Add test for new API Intent.allowUnsafeUnparceledIntentLaunch
    
    Bug: 372886352
    Test: IntentTest#testAllowUnsafeUnparceledIntentLaunch
    Flag: android.security.prevent_intent_redirect
    Change-Id: I742d1b2671fb55ef54d1ea178ccf8a0c8ca65a64

--- tests/tests/content/src/android/content/cts/IntentTest.java
+++ tests/tests/content/src/android/content/cts/IntentTest.java
@@ -16,6 +16,7 @@
 
 package android.content.cts;
 
+import static android.content.Intent.EXTENDED_FLAG_MISSING_CREATOR_OR_INVALID_TOKEN;
 import static android.content.Intent.FLAG_RECEIVER_FOREGROUND;
 import static android.content.Intent.FLAG_RECEIVER_OFFLOAD;
 
@@ -58,6 +59,7 @@ import android.platform.test.flag.junit.CheckFlagsRule;
 import android.platform.test.flag.junit.DeviceFlagsValueProvider;
 import android.platform.test.flag.junit.RavenwoodFlagsValueProvider;
 import android.platform.test.ravenwood.RavenwoodRule;
+import android.security.Flags;
 import android.test.mock.MockContext;
 import android.util.AttributeSet;
 import android.util.Xml;
@@ -2271,6 +2273,17 @@ public class IntentTest {
 
         assertThat(deserialized.toInsecureString())
                 .isEqualTo(intent.toInsecureString());
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_PREVENT_INTENT_REDIRECT)
+    public void testRemoveLaunchSecurityProtection() {
+        Intent intent = new Intent(TEST_ACTION);
+        intent.setCreatorToken(new Binder());
+        intent.addExtendedFlags(EXTENDED_FLAG_MISSING_CREATOR_OR_INVALID_TOKEN);
+        intent.removeLaunchSecurityProtection();
+        assertThat(intent.getExtendedFlags()).isEqualTo(0);
+        assertThat(intent.getCreatorToken()).isNull();
     }
 
     private void roundtrip() {
