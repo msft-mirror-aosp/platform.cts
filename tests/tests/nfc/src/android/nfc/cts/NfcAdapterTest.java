3863678a72413817b326ae82b1ad186ad8326b04
ziyiw <ziyiw@google.com>
2024-11-13 17:35:26 +0000

    [cts] Rename oemCallback onEnable/disable
    Bug: 362476331
    Test: compile
    Change-Id: I941748fcdd6d0d364a9f136abe54931fb844b93e

--- tests/tests/nfc/src/android/nfc/cts/NfcAdapterTest.java
+++ tests/tests/nfc/src/android/nfc/cts/NfcAdapterTest.java
@@ -804,11 +804,11 @@ public class NfcAdapterTest {
         }
 
         @Override
-        public void onEnable(@NonNull Consumer<Boolean> isAllowed) {
+        public void onEnableRequested(@NonNull Consumer<Boolean> isAllowed) {
         }
 
         @Override
-        public void onDisable(@NonNull Consumer<Boolean> isAllowed) {
+        public void onDisableRequested(@NonNull Consumer<Boolean> isAllowed) {
         }
 
         @Override
