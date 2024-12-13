9be2ab56271fc9845b57d7fc96513ed73eeb1ce0
Angus Fung <angusfung@google.com>
2024-12-13 00:59:07 +0000

    Update Framework from Jetpack.
    * e336b856720 Merge "Suppress lint warnings around permissions." into androidx-main
    * b4d7b9a9278 Suppress lint warnings around permissions.
    * 21d2fad2291 Manually apply the Projection API in Android S and Android T.
    * 79e4152cbf3 Merge "Adjusts proguard rules to keep classes conditionally" into androidx-main
    * 224cdb5c1d0 Changes updatedTimestamp to updateTimestampMillis
    * 711c48265bd Adjusts proguard rules to keep classes conditionally
    * 665d9ea4d65 Merge "Move annotations in AppSearchResult" into androidx-main
    * 6f0b0fe9b8b Move annotations in AppSearchResult
    * e84a2d820c6 Switch back to AndroidX nullness annotations for ReportUsageRequest API copied to framework.
    * fd30a49ab48 Switch back to AndroidX nullness annotations for appsearch APIs copied to framework
    * bb7e2dffca9 Pluralize get blob storage info method name.
    * fb9e74d4836 Adds @RequiredPermission to searchPersonCorpus and searchMobileApplicationCorpus
    * 0ea3091aadd Clean up project references in build.gradle
    * ef3d124b724 Bump AppSearch version to 1.1.0-beta01.
    * 4eb615e676d Merge "Rename references to property to propertyPath." into androidx-main
    * ced7f81ddd4 Clean up prebuilt dependency declarations
    * 465a6aee46f Clean up project references in build.gradle files
    * 1fde311a271 Clean up plugin blocks in build.gradle
    * 542b0d20a24 Rename references to property to propertyPath.
    * cf9ac750c80 Merge "Add more detail to SemanticSearchNode#getVectorIndex Javadoc." into androidx-main
    * 76a802b6033 Replace the `new Locale` with the Locale builder as the former is deprecated.
    * daa50a7618d Merge "Migrate appsearch compiler to use JSpecify annotations" into androidx-main
    * f3af8dec278 Merge "Revert "Update FeaturesImpl for U Mainline Features."" into androidx-main
    * ca8ac6546a3 Revert "Update FeaturesImpl for U Mainline Features."
    * 9535bc2a504 Add more detail to SemanticSearchNode#getVectorIndex Javadoc.
    * 7e0f8189e35 Migrate appsearch compiler to use JSpecify annotations
    * bb42ca1d080 Move to using latest kotlinc for AppSearch compiler project
    * d48179184a3 Switch additional appsearch nullness annotation usages to JSpecify
    * 0998c28fd9b Merge changes I57e384b7,I607a6b39 into androidx-main
    * 354377ff739 Merge "Adds MobileApplication for Jetpack" into androidx-main
    * c2b06a68e77 Merge "Adds a util to get App namespace for Apps Indexer" into androidx-main
    * 4f894e75add Adds MobileApplication for Jetpack
    * 986d1f43224 Remove androidx.appsearch.checker.nullness.qual.Nullable annotation
    * ead0fc68e74 Migrate appsearch to use JSpecify annotations
    * 208293ef55d Adds a util to get App namespace for Apps Indexer
    * ae5dad3a965 Migrate appsearch-platform-storage to use JSpecify annotations
    * 89073e68359 Merge "Turn JSpecify lint on by default" into androidx-main
    * 7795282b4d5 Migrate to using = for property setting
    * 5bbc4304fce Merge "Mark ShortcutInfo integration as experimental." into androidx-main
    * 81a46bc8d19 Turn JSpecify lint on by default
    * 3367547a390 Add suppress for lint error "RequiresPermission".
    * c4d4f10e688 Mark ShortcutInfo integration as experimental.
    
    Bug: 258308777
    Bug: 275592563
    Bug: 326456246
    Bug: 328672505
    Bug: 356661489
    Bug: 356662352
    Bug: 380260949
    Bug: 380917313
    Bug: 384568623
    Bug: 384568937
    Bug: 384721898
    Bug: 384880094
    Flag: com.android.appsearch.flags.enable_abstract_syntax_trees
    Test: Presubmit
    Change-Id: Ia2bbb0cf3c33c373e2f3148eeb8abd37528f3a10

--- tests/appsearch/src/com/android/cts/appsearch/external/app/AppSearchSessionBlobCtsTestBase.java
+++ tests/appsearch/src/com/android/cts/appsearch/external/app/AppSearchSessionBlobCtsTestBase.java
@@ -28,7 +28,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeFalse;
 import static org.junit.Assume.assumeTrue;
 
-import android.annotation.NonNull;
 import android.app.appsearch.AppSearchBatchResult;
 import android.app.appsearch.AppSearchBlobHandle;
 import android.app.appsearch.AppSearchResult;
@@ -57,6 +56,7 @@ import com.android.appsearch.flags.Flags;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import org.jspecify.annotations.NonNull;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
