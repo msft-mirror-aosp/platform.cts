b2de1f48a6168d1bbb8e8ebc6c99ecec3f2875b8
Rucha Katakwar <ruchamk@google.com>
2025-01-08 13:12:28 -0800

    ITS:Extract dynamic range cells from the image.
    This change includes functions to extract dynamic range cell patches
    from the captured image and reorders them from least intensity to
    highest intensity level for both default camera app and JCA app.
    These dynamic range patch cells will be further compared to evaluate
    image parity metrics.
    
    Bug: 380892830
    
    Test: Tested on pixel device.
    FLAG: TEST_ONLY
    Change-Id: Iba2f4a61da242d321edfcf25c8b9e6669c1aab63

--- apps/CameraITS/utils/ip_chart_extraction_utils.py
+++ apps/CameraITS/utils/ip_chart_extraction_utils.py
@@ -15,11 +15,37 @@
 
 import logging
 import os
-import pathlib
-import subprocess
 
 import cv2
 import ip_chart_pattern_detector as pd
+import numpy as np
+
+
+# The order of the dynamic range patch cells in the chart based on
+# their RGB values and corresponding positions in the chart.
+DYNAMIC_RANGE_PATCH_ORDER = [
+    2,
+    3,
+    1,
+    4,
+    0,
+    5,
+    19,
+    6,
+    18,
+    7,
+    17,
+    8,
+    16,
+    9,
+    15,
+    10,
+    14,
+    11,
+    13,
+    12,
+]
+SCALE_FACTOR = 2 / 3
 
 
 def get_feature_from_image(
@@ -58,3 +84,126 @@ def get_feature_from_image(
         feature_image,
     )
     return feature_image, os.path.join(log_path, output_file_name + '.png')
+
+
+def get_dynamic_range_patch_cells(img):
+  """Get list of each cell from the dynamic range chart.
+
+  Returns the list of each cell from the dynamic range chart.
+  Each cell is an instance of `DynamicRangePatch` containing the corner points
+  and the cell image. Each cell image is a numpy array in BGRA format.
+  The list contains the cell in clockwise order from top-left.
+
+  Args:
+    img: path to the full chart captured image.
+
+  Returns:
+    dynamic_range_patch_cells: A list of `DynamicRangePatch` where every cell is
+    a list containing the four corner positions in query image and the original
+    color in test chart.
+  """
+  img_bgr = cv2.imread(img)
+  dynamic_range_patch_cells = pd.find_dynamic_range_patches(img_bgr)
+  logging.debug(
+      'Dynamic range patch cells length: %s', len(dynamic_range_patch_cells)
+  )
+  if len(dynamic_range_patch_cells) < 20:
+    raise AssertionError('All dynamic range patch cells not found')
+  return dynamic_range_patch_cells
+
+
+def get_cropped_patch(
+    patch_image,
+    patch_suffix,
+    log_path,
+    scale_factor=None,
+):
+  """Returns the cropped patch with scale factor or patch length.
+
+  Args:
+    patch_image: numpy array of patch image in BGRA format
+    patch_suffix: suffix of the patch
+    log_path: path to the log directory
+    scale_factor: scale factor of the patch
+
+  Returns:
+    cropped_patch: numpy array of cropped patch image in RGB format
+  """
+  # Extract the alpha channel
+  alpha_channel = patch_image[:, :, 3]
+
+  # Find the non-zero (non-transparent) pixels
+  y_indices, x_indices = np.where(alpha_channel != 0)
+
+  # Get the bounding box of the non-transparent region
+  min_x = np.min(x_indices)
+  min_y = np.min(y_indices)
+  max_x = np.max(x_indices)
+  max_y = np.max(y_indices)
+
+  # Crop the image to the bounding box
+  non_tranpsarent_patch = patch_image[min_y:max_y + 1, min_x:max_x + 1]
+  height, width = non_tranpsarent_patch.shape[:2]
+  target_size = None
+
+  if scale_factor is not None:
+    # Determine the target size, ensuring it's even
+    target_size = int(min(height, width) * scale_factor)
+    target_size = target_size - (target_size % 2)  # Make it even
+    logging.debug(
+        'Exracted %s patch shape: %s x %s', patch_suffix, height, width
+    )
+
+  center_x = height // 2
+  center_y = width // 2
+
+  if target_size is None:
+    raise ValueError(
+        'Target size is None. One of scale_factor or patch_length must be'
+        ' specified.'
+    )
+
+  start_x = center_x - target_size // 2
+  start_y = center_y - target_size // 2
+
+  end_x = start_x + target_size
+  end_y = start_y + target_size
+
+  cropped_patch = non_tranpsarent_patch[start_x:end_x, start_y:end_y]
+  cv2.imwrite(os.path.join(log_path, patch_suffix + '.png'), cropped_patch)
+  height, width = cropped_patch.shape[:2]
+  logging.debug('Cropped %s patch shape: %s x %s', patch_suffix, height, width)
+  cropped_patch = cv2.cvtColor(cropped_patch, cv2.COLOR_BGRA2RGB)
+  return cropped_patch
+
+
+def get_cropped_dynamic_range_patch_cells(img_path, log_path, chart_suffix):
+  """Returns the cropped dynamic range patch cells.
+
+  Args:
+    img_path: path to the full chart captured image.
+    log_path: path to the log directory
+    chart_suffix: suffix of the patch
+  Returns:
+   cropped_dynamic_range_patch_cells: Cropped dynamic range patch cells
+   in the DYNAMIC_RANGE_PATCH_ORDER.
+  """
+  logging.debug('Getting cropped dynamic range patch cells')
+  dynamic_range_patch_cells = get_dynamic_range_patch_cells(img_path)
+  reordered_dynamic_range_patch_cells = []
+  # Reorder the dynamic range patch cells based on their RGB values and
+  # corresponding positions in the chart.
+  for i in DYNAMIC_RANGE_PATCH_ORDER:
+    reordered_dynamic_range_patch_cells.append(dynamic_range_patch_cells[i])
+  cropped_dynamic_range_patch_cells = []
+  for i, cell in enumerate(reordered_dynamic_range_patch_cells):
+    chart_name = f'{chart_suffix}_dynamic_range_patch_cell_{i}'
+    center_patch = get_cropped_patch(
+        cell.image,
+        chart_name,
+        log_path,
+        scale_factor=SCALE_FACTOR,
+    )
+    cv2.imwrite(os.path.join(log_path, chart_name + '.png'), center_patch)
+    cropped_dynamic_range_patch_cells.append(center_patch)
+  return cropped_dynamic_range_patch_cells
