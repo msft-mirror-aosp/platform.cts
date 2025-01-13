2746ef4bc0c3b482992576f9eda4fe6da2300548
Haripriya Deshmukh <haripriya.deshmukh@ittiam.com>
2025-01-13 10:38:31 +0530

    videocodec: Extend tests for APV encoder
    As of now, the current suite video encoder tests run only for hardware
    accelerated components. Considering the coverage for APV encoder is
    less, this suite is modified to run for software components as well but
    only for APV codec.
    
    Bug: 376770121
    Test: atest CtsVideoCodecTestCases -- --module-arg \
    CtsVideoCodecTestCases: instrumentation-arg:codec-prefix:=c2.android.apv.encoder
    
    Change-Id: I1044cea78aaad9a5fe4b34c7f5e36057c0503fa7

--- tests/media/common/src/android/mediav2/common/cts/CodecTestBase.java
+++ tests/media/common/src/android/mediav2/common/cts/CodecTestBase.java
@@ -1045,7 +1045,7 @@ public abstract class CodecTestBase {
                 mustTestAllCodecs, selectSwitch, features, false);
     }
 
-    private static List<Object[]> prepareParamList(List<Object[]> exhaustiveArgsList,
+    public static List<Object[]> prepareParamList(List<Object[]> exhaustiveArgsList,
             boolean isEncoder, boolean needAudio, boolean needVideo, boolean mustTestAllCodecs,
             ComponentClass selectSwitch, String[] features, boolean ignoreModeDuringSelection) {
         ArrayList<String> mediaTypes = compileCompleteTestMediaTypesList(isEncoder,
