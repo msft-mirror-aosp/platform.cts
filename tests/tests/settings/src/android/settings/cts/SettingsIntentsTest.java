eac962469ef1cd772a7d6a204bd113a5ee1e1632
Hakjun Choi <hakjunc@google.com>
2025-02-05 06:20:17 +0000

    Clean up aconfig flag carrier_enabled_satellite_flag
    Bug: 393951373
    FLAG: EXEMPT flag clean up
    Test: atest SatelliteManagerTestOnMockService ImsMmTelManagerTest TelephonyCallbackTest TelephonyRegistryManagerTest CarrierRoamingSatelliteSmsTest CarrierRoamingSatelliteTest SatelliteManagerTest SettingsIntentsTest
    
    Change-Id: Ie9a262fa8e64538533645cb47548bf955082aee2

--- tests/tests/settings/src/android/settings/cts/SettingsIntentsTest.java
+++ tests/tests/settings/src/android/settings/cts/SettingsIntentsTest.java
@@ -61,7 +61,6 @@ public class SettingsIntentsTest {
     }
 
     @Test
-    @RequiresFlagsEnabled(Flags.FLAG_CARRIER_ENABLED_SATELLITE_FLAG)
     public void settingActivity_launchSatelliteSettingIntent() {
         assumeFalse(
                 "Skipping test: Satellite settings are not supported in AAOS",
