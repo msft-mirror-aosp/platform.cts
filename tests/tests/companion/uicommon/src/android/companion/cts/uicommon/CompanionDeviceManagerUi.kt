08cddba80e36fd2a68aa99f697936e0aeb0301de
Evan Chen <evanxinchen@google.com>
2025-02-14 17:57:40 +0000

    Fix StaleObjectException for multiple device test
    Need to refresh the UiObject reference before click
    
    Test: cts
    bug: 394108349
    Flag: EXEMPT bugfix
    Change-Id: I215cc3332a5c31d31b958c1216645c270891042a

--- tests/tests/companion/uicommon/src/android/companion/cts/uicommon/CompanionDeviceManagerUi.kt
+++ tests/tests/companion/uicommon/src/android/companion/cts/uicommon/CompanionDeviceManagerUi.kt
@@ -58,16 +58,16 @@ open class CompanionDeviceManagerUi(private val ui: UiDevice) {
     fun waitUntilGone() = ui.waitShort(Until.gone(CONFIRMATION_UI), "CDM UI has not disappeared")
 
     fun waitAndClickOnFirstFoundDevice() {
-        val firstDevice = ui.waitLongAndFind(
-                Until.findObject(DEVICE_LIST_WITH_ITEMS),
-                "The item in the Device List not found or empty"
-        ).children[0]
-
         val startTime = SystemClock.uptimeMillis()
         var elapsedTime = 0L
         // Keep trying to click the first item in the list until the device_list is disappeared
         // or it times out after 5s.
-        while (ui.hasObject(DEVICE_LIST) && elapsedTime < 5.seconds.inWholeMilliseconds) {
+        while (ui.hasObject(DEVICE_LIST) && elapsedTime < 10.seconds.inWholeMilliseconds) {
+            val firstDevice = ui.waitLongAndFind(
+                Until.findObject(DEVICE_LIST_WITH_ITEMS),
+                "The item in the Device List not found or empty"
+            ).children[0]
+
             firstDevice.click()
             sleep(0.2.seconds.inWholeMilliseconds)
             elapsedTime = SystemClock.uptimeMillis() - startTime
