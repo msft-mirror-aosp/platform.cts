8ea2b4afe981db6a4b1811f1d82a19123ea01655
Aaqib Ismail <aaqibismail@google.com>
2025-02-15 22:35:55 -0800

    RESTRICT AUTOMERGE: Update testIsPropertyAvailable to test all configs
    It's not guaranteed that all properties will be available so remove assert true.
    
    Bug: 396631627
    Change-Id: Idccf25573dacf0f7f707e7f88bc241881a913d12
    Test: atest CtsCarTestCases:CarPropertyManagerTest

--- tests/tests/car/src/android/car/cts/CarPropertyManagerTest.java
+++ tests/tests/car/src/android/car/cts/CarPropertyManagerTest.java
@@ -6351,16 +6351,29 @@ public final class CarPropertyManagerTest extends AbstractCarTestCase {
     public void testIsPropertyAvailable() {
         runWithShellPermissionIdentity(
                 () -> {
-                    List<CarPropertyConfig> configs =
-                            mCarPropertyManager.getPropertyList(mPropertyIds);
-
+                    List<CarPropertyConfig> configs = mCarPropertyManager.getPropertyList();
                     for (CarPropertyConfig cfg : configs) {
-                        int[] areaIds = getAreaIdsHelper(cfg);
-                        for (int areaId : areaIds) {
-                            assertThat(
-                                            mCarPropertyManager.isPropertyAvailable(
-                                                    cfg.getPropertyId(), areaId))
-                                    .isTrue();
+                        int propertyId = cfg.getPropertyId();
+                        List<AreaIdConfig<?>> areaIdConfigs = cfg.getAreaIdConfigs();
+                        for (AreaIdConfig<?> areaIdConfig : areaIdConfigs) {
+                            int areaId = areaIdConfig.getAreaId();
+                            try {
+                                mCarPropertyManager.isPropertyAvailable(propertyId, areaId);
+                            } catch (IllegalArgumentException e) {
+                                expectWithMessage(
+                                                "Should not throw IllegalArgumentException for"
+                                                        + " property: "
+                                                        + VehiclePropertyIds.toString(propertyId)
+                                                        + ", area ID: "
+                                                        + areaId
+                                                        + ", access: "
+                                                        + cfg.getAccess()
+                                                        + ", error: "
+                                                        + e)
+                                        .that(cfg.getAccess())
+                                        .isIn(NO_READ_ACCESS_SET);
+                                continue;
+                            }
                         }
                     }
                 });
@@ -7138,15 +7151,6 @@ public final class CarPropertyManagerTest extends AbstractCarTestCase {
             return ONCHANGE_RATE_EVENT_COUNTER;
         } else {
             return 0;
-        }
-    }
-
-    // Returns {0} if the property is global property, otherwise query areaId for CarPropertyConfig
-    private int[] getAreaIdsHelper(CarPropertyConfig config) {
-        if (config.isGlobalProperty()) {
-            return new int[]{0};
-        } else {
-            return config.getAreaIds();
         }
     }
 
