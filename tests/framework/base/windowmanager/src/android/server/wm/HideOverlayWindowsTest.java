defcfd837c65219d61f9df1ce6abeb2c02ebfed1
Annie Lin <theannielin@google.com>
2025-02-24 11:02:17 -0800

    RESTRICT AUTOMERGE: Skip WM tests for custom window occlusion.
    Bug: 396173903
    Fix: 349374915
    Bug: 396168998
    Fix: 349403766
    Bug: 396165850
    Fix: 349396128
    Bug: 398861504
    Test: Devices with appropriate property are skipped.
    Flag: TEST_ONLY
    Change-Id: I87b50ed41e744655c7ffcd2798d3c25a76864258

--- tests/framework/base/windowmanager/src/android/server/wm/HideOverlayWindowsTest.java
+++ tests/framework/base/windowmanager/src/android/server/wm/HideOverlayWindowsTest.java
@@ -27,6 +27,8 @@ import static android.view.WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import static org.junit.Assume.assumeFalse;
+
 import android.Manifest;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
@@ -49,6 +51,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import com.android.compatibility.common.util.FeatureUtil;
 import com.android.compatibility.common.util.SystemUtil;
 
 import org.junit.After;
@@ -204,7 +207,11 @@ public class HideOverlayWindowsTest extends ActivityManagerTestBase {
     }
 
     @Test
-    public void testApplicationOverlay_touchIsObscuredWithoutCorrectPermission() {
+    public void testApplicationOverlay_touchIsObscuredWithoutCorrectPermission() throws Throwable {
+        // TODO(b/398861504): Ensure this test case is covered by the CTS Verifier.
+        assumeFalse("XR device uses a custom window occlusion check tested via CTS Verifier.",
+                FeatureUtil.isXrHeadset());
+
         String windowName = "SYSTEM_APPLICATION_OVERLAY";
         ComponentName componentName = new ComponentName(
                 mContext, SystemWindowActivity.class);
