655338b39bf9ae533c6c9a2d26e4b0dcd5195ca7
Tomasz Wasilczyk <twasilczyk@google.com>
2025-02-26 09:49:13 -0800

    Use isDeviceVoiceCapable instead of isVoiceCapable
    Bug: 393424911
    Bug: 398045526
    Test: TelephonyManagerTest
    Flag: TEST_ONLY
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:98a5e0d025fed1dc6a9b6d02609452b095e50e9a
    Merged-In: I6dd0058f0ca751fc798c88fba99bc01aaaebd5e4
    Change-Id: I6dd0058f0ca751fc798c88fba99bc01aaaebd5e4

--- tests/tests/telephony/current/src/android/telephony/cts/TelephonyManagerTest.java
+++ tests/tests/telephony/current/src/android/telephony/cts/TelephonyManagerTest.java
@@ -1140,6 +1140,7 @@ public class TelephonyManagerTest {
         mTelephonyManager.getLine1Number();
         mTelephonyManager.getPhoneCount();
         mTelephonyManager.isVoiceCapable();
+        mTelephonyManager.isDeviceVoiceCapable();
         mTelephonyManager.isSmsCapable();
         mTelephonyManager.getDeviceSoftwareVersion();
 
@@ -1527,7 +1528,7 @@ public class TelephonyManagerTest {
 
     @Test
     public void testCreateForPhoneAccountHandle() {
-        if (!mTelephonyManager.isVoiceCapable()) {
+        if (!mTelephonyManager.isDeviceVoiceCapable()) {
             Log.d(TAG, "Skipping test that requires device to be voice capable");
             return;
         }
@@ -7144,7 +7145,7 @@ public class TelephonyManagerTest {
         List<String> emergencyRoleHolders = ShellIdentityUtils.invokeMethodWithShellPermissions(
                 getContext().getSystemService(RoleManager.class),
                 (rm) -> rm.getRoleHolders(RoleManager.ROLE_EMERGENCY));
-        if (mTelephonyManager.isVoiceCapable()
+        if (mTelephonyManager.isDeviceVoiceCapable()
             && ShellIdentityUtils.invokeMethodWithShellPermissions(mTelephonyManager,
                 (tm) -> tm.isEmergencyAssistanceEnabled())) {
             String emergencyAssistancePackageName =
