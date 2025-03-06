4c6d8d405097fb357a4bac55468e5c5a79450fc8
Marvin Ramin <marvinramin@google.com>
2025-03-06 15:05:13 +0100

    Migrate MediaProjectionSDK tests to MediaProjectionRule
    Test: atest CtsMediaProjectionSDK34TestCases CtsMediaProjectionSDK33TestCases
    Bug: 400932931
    Flag: TEST_ONLY
    Change-Id: If2c0cf180665be7ebc4a7e2dd05e2ce92a85aa33

--- tests/tests/media/projection/sdk34/src/android/media/projection/MediaProjectionSDK34Test.java
+++ tests/tests/media/projection/sdk34/src/android/media/projection/MediaProjectionSDK34Test.java
@@ -16,29 +16,18 @@
 package android.media.projection;
 
 import static android.app.Activity.RESULT_OK;
-import static android.hardware.display.DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.PixelFormat;
 import android.hardware.display.VirtualDisplay;
-import android.media.ImageReader;
-import android.media.cts.MediaProjectionActivity;
+import android.media.cts.MediaProjectionRule;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
-import android.view.Surface;
-
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
 
 import com.android.compatibility.common.util.ApiTest;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -52,58 +41,23 @@ import org.junit.Test;
  * atest CtsMediaProjectionSDK34TestCases:MediaProjectionSDK34Test
  */
 public class MediaProjectionSDK34Test {
-    private static final String TAG = "MediaProjectionSDK34Test";
-
-    private static final int RECORDING_WIDTH = 500;
-    private static final int RECORDING_HEIGHT = 700;
-    private static final int RECORDING_DENSITY = 200;
-
-    @Rule
-    public ActivityTestRule<MediaProjectionActivity> mActivityRule =
-            new ActivityTestRule<>(MediaProjectionActivity.class, false, false);
-
-    private MediaProjectionActivity mActivity;
-    private MediaProjection mMediaProjection;
-    private MediaProjection.Callback mCallback = null;
-    private ImageReader mImageReader;
-    private VirtualDisplay mVirtualDisplay;
-    private Context mContext;
-
-    @Before
-    public void setUp() {
-        mContext = InstrumentationRegistry.getInstrumentation().getContext();
-    }
-
-    @After
-    public void cleanup() {
-        if (mMediaProjection != null) {
-            if (mCallback != null) {
-                mMediaProjection.unregisterCallback(mCallback);
-            }
-            mMediaProjection.stop();
-        }
-    }
+    @Rule public MediaProjectionRule mMediaProjectionRule = new MediaProjectionRule();
 
     @ApiTest(apis = "android.media.projection.MediaProjection#createVirtualDisplay")
     @Test
     public void testCreateVirtualDisplay_withoutCallback_throwsException() throws Exception {
-        startMediaProjection();
+        mMediaProjectionRule.startMediaProjection(/* skipDefaultCallback */ true);
 
         // Try to start capture with no callback registered.
-        assertThrows(IllegalStateException.class, this::createVirtualDisplay);
+        assertThrows(IllegalStateException.class, mMediaProjectionRule::createVirtualDisplay);
 
-        mCallback = new MediaProjection.Callback() {
-            @Override
-            public void onStop() {
-                cleanupVirtualDisplay();
-            }
-        };
-        mMediaProjection.registerCallback(mCallback, new Handler(Looper.getMainLooper()));
+        mMediaProjectionRule.registerCallback(new MediaProjection.Callback() {});
 
         // Now starting capture should succeed.
-        createVirtualDisplay();
-        assertNotNull("VirtualDisplay should be a non-null object if capture started "
-                + "successfully", mVirtualDisplay);
+        VirtualDisplay virtualDisplay = mMediaProjectionRule.createVirtualDisplay();
+        assertNotNull(
+                "VirtualDisplay should be a non-null object if capture started " + "successfully",
+                virtualDisplay);
     }
 
     /**
@@ -119,14 +73,15 @@ public class MediaProjectionSDK34Test {
     @Test
     public void testCreateVirtualDisplay_reusedResultData_throwsException() throws Exception {
         // Navigate the dialog and retrieve the first projection instance.
-        startMediaProjection();
-        assertNotNull("MediaProjection should be a non-null object if projection started "
-                + "successfully", mMediaProjection);
+        MediaProjection mediaProjection = mMediaProjectionRule.startMediaProjection();
+        assertNotNull(
+                "MediaProjection should be a non-null object if projection started "
+                        + "successfully",
+                mediaProjection);
 
         // Retrieve the result data used to create the first media projection instance.
-        Intent resultWithConsent = mActivity.getResultData();
-        MediaProjectionManager projectionManager = mContext.getSystemService(
-                MediaProjectionManager.class);
+        Intent resultWithConsent = mMediaProjectionRule.getActivity().getResultData();
+        MediaProjectionManager projectionManager = mMediaProjectionRule.getMediaProjectionManager();
 
         // Re-use the result data to retrieve a new media projection instance.
         MediaProjection anotherProjection = projectionManager.getMediaProjection(RESULT_OK,
@@ -134,25 +89,13 @@ public class MediaProjectionSDK34Test {
         assertNotNull("MediaProjection should be a non-null object if projection started "
                 + "successfully", anotherProjection);
 
-        mCallback = new MediaProjection.Callback() {
-            @Override
-            public void onStop() {
-                cleanupVirtualDisplay();
-            }
-        };
-        anotherProjection.registerCallback(mCallback, new Handler(Looper.getMainLooper()));
-        mImageReader = ImageReader.newInstance(RECORDING_WIDTH, RECORDING_HEIGHT,
-                PixelFormat.RGBA_8888, /* maxImages= */ 1);
-
+        anotherProjection.registerCallback(
+                new MediaProjection.Callback() {}, new Handler(Looper.getMainLooper()));
         // There should be an exception thrown when trying to start capture on the second
         // projection.
-        assertThrows(SecurityException.class,
-                () -> anotherProjection.createVirtualDisplay(TAG + "VirtualDisplay",
-                        RECORDING_WIDTH, RECORDING_HEIGHT, RECORDING_DENSITY,
-                        VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                        mImageReader.getSurface(),
-                        /* VirtualDisplay.Callback= */ null,
-                        new Handler(Looper.getMainLooper())));
+        assertThrows(
+                SecurityException.class,
+                () -> mMediaProjectionRule.createVirtualDisplay(anotherProjection));
     }
 
     /**
@@ -166,67 +109,13 @@ public class MediaProjectionSDK34Test {
     @ApiTest(apis = "android.media.projection.MediaProjection#createVirtualDisplay")
     @Test
     public void testCreateVirtualDisplay_reusedMediaProjection_throwsException() throws Exception {
-        startMediaProjection();
+        mMediaProjectionRule.startMediaProjection();
 
         // Start capture once.
-        mCallback = new MediaProjection.Callback() {
-            @Override
-            public void onStop() {
-                cleanupVirtualDisplay();
-            }
-        };
-        mMediaProjection.registerCallback(mCallback, new Handler(Looper.getMainLooper()));
-        createVirtualDisplay();
+        mMediaProjectionRule.createVirtualDisplay();
 
         // Now try to capture again on the same mMediaProjection instance; this should throw an
         // exception.
-        assertThrows(SecurityException.class,
-                () -> mMediaProjection.createVirtualDisplay(TAG + "VirtualDisplay",
-                        RECORDING_WIDTH, RECORDING_HEIGHT, RECORDING_DENSITY,
-                        VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                        mImageReader.getSurface(),
-                        /* VirtualDisplay.Callback= */ null,
-                        new Handler(Looper.getMainLooper())));
-    }
-
-    void startMediaProjection() throws Exception {
-        mActivityRule.launchActivity(null);
-        mActivity = mActivityRule.getActivity();
-        mMediaProjection = mActivity.waitForMediaProjection();
-    }
-
-    void createVirtualDisplay() {
-        mImageReader = ImageReader.newInstance(RECORDING_WIDTH, RECORDING_HEIGHT,
-                PixelFormat.RGBA_8888, /* maxImages= */ 1);
-        mVirtualDisplay = mMediaProjection.createVirtualDisplay(TAG + "VirtualDisplay",
-                RECORDING_WIDTH, RECORDING_HEIGHT, RECORDING_DENSITY,
-                VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                mImageReader.getSurface(),
-                new VirtualDisplay.Callback() {
-                    @Override
-                    public void onStopped() {
-                        super.onStopped();
-                        // VirtualDisplay stopped by the system; no more frames incoming. Must
-                        // release VirtualDisplay
-                        Log.v(TAG, "handleVirtualDisplayStopped");
-                        cleanupVirtualDisplay();
-                    }
-                }, new Handler(Looper.getMainLooper()));
-    }
-
-    void cleanupVirtualDisplay() {
-        if (mImageReader != null) {
-            mImageReader.close();
-            mImageReader = null;
-        }
-
-        if (mVirtualDisplay != null) {
-            final Surface surface = mVirtualDisplay.getSurface();
-            if (surface != null) {
-                surface.release();
-            }
-            mVirtualDisplay.release();
-            mVirtualDisplay = null;
-        }
+        assertThrows(SecurityException.class, () -> mMediaProjectionRule.createVirtualDisplay());
     }
 }
