bd980f8752cfea5813b079d4e5886a1d309aac84
Daniel Norman <danielnorman@google.com>
2025-03-07 23:02:39 +0000

    Add a test for Security bug b/386950836
    Bug: 386950836
    Test: atest android.security.cts.Bug_386950836;
          test fails without fix; test passes with fix
    Flag: EXEMPT security bugfix
    Merged-In: I57fac74e802e3ddc0aa4a06f741ce999899eb09f
    Change-Id: I57fac74e802e3ddc0aa4a06f741ce999899eb09f

---
+++ hostsidetests/securitybulletin/test-apps/BUG-386950836/src/android/security/cts/BUG_386950836/MainActivity.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.security.cts.BUG_386950836;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openSettings(View view) {
+        Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        startActivity(intent);
+    }
+}
