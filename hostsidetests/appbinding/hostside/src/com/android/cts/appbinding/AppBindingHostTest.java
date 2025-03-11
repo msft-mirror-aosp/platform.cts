4350808e21225069f42ff295f9ad7595de912379
Michael Wachenschwanz <mwachens@google.com>
2025-03-11 10:19:47 -0700

    Revert "Fix testOomAdjustment to account for IME importance change"
    Revert submission 32128793-hostside_input_flag_fix
    
    Reason for revert: b/401474709
    
    Reverted changes: /q/submissionid:32128793-hostside_input_flag_fix
    
    Change-Id: I712f76c3bb10ecdcbcb95e0f6a3b36c07e41172e

--- hostsidetests/appbinding/hostside/src/com/android/cts/appbinding/AppBindingHostTest.java
+++ hostsidetests/appbinding/hostside/src/com/android/cts/appbinding/AppBindingHostTest.java
@@ -755,13 +755,6 @@ ACTIVITY MANAGER RUNNING PROCESSES (dumpsys activity processes)
         }
 
         installAndCheckBound(APK_1, PACKAGE_A, SERVICE_1, mCurrentUserId);
-
-        final int expectedOomscore;
-        if (android.view.inputmethod.Flags.lowerImeOomImportance()) {
-            expectedOomscore = 201;
-        } else {
-            expectedOomscore = 200;
-        }
-        assertOomAdjustment(PACKAGE_A, PACKAGE_A_PROC, expectedOomscore);
+        assertOomAdjustment(PACKAGE_A, PACKAGE_A_PROC, 200);
     }
 }
