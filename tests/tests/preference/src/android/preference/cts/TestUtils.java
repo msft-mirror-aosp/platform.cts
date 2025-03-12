5c63a62a96b153b43e8da52b00e2c62f3f8b7677
Eric Chiang <eschiang@google.com>
2025-03-12 14:52:30 -0700

    Fix flaky wear preference test
    When the test scrolls to look for certain views, the view may be skipped
    if the system is running slow or laggy. This fix changes the scrolling
    logic to more incrementally scroll so there is a lower chance to skip
    the view.
    
    Bug: 397728119
    Test: atest
    android.preference.cts.PreferenceActivityFlowPortraitTest#listDialogPortraitTest
    on cf_gwear_x86-trunk_staging-userdebug
    Flag: EXEMPT test_only
    
    Change-Id: I85d0b8f03fc93e20d5314543fbc3dfe2825005fe

--- tests/tests/preference/src/android/preference/cts/TestUtils.java
+++ tests/tests/preference/src/android/preference/cts/TestUtils.java
@@ -180,22 +180,24 @@ public class TestUtils {
     private boolean scrollToAndGetTextObject(String text) {
         UiObject2 appArea = mDevice.findObject(By.pkg(mPackageName).res("android:id/content"));
         List<UiObject2> scrollableViewList = appArea.findObjects(By.scrollable(true));
-        UiObject2 foundObject = null;
         for (UiObject2 scrollableView : scrollableViewList) {
             // Swipe far away from the edges to avoid triggering navigation gestures
             scrollableView.setGestureMarginPercentage(0.25f);
             // Scroll from the top to the bottom until the text object is found.
             scrollableView.scroll(Direction.UP, 1);
-            scrollableView.scrollUntil(Direction.DOWN, Until.findObject(By.textContains(text)));
-            foundObject = mDevice.findObject(By.text(text));
-            if (foundObject != null) {
-                // No need to look at other scrollables.
-                break;
+            boolean moreToScroll = true;
+            while (moreToScroll) {
+                if (mDevice.findObject(By.text(text)) != null) {
+                    return true;
+                }
+
+                // Scroll down incrementally to avoid skipping the view on slower systems
+                moreToScroll = scrollableView.scroll(Direction.DOWN, 0.2f);
+            }
+            if (mDevice.findObject(By.text(text)) != null) {
+                return true;
             }
         }
-        if (foundObject == null) {
-            throw new AssertionError("View with text '" + text + "' was not found!");
-        }
-        return true;
+        throw new AssertionError("View with text '" + text + "' was not found!");
     }
 }
