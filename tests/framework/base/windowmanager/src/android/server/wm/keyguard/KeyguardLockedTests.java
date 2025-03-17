acae77d081a4bb4fecbaff927dff6bc492c4a955
Annie Lin <theannielin@google.com>
2025-03-17 10:17:50 -0700

    Skip testDismissKeyguardActivity_method_cancelled for XR.
    See b/349394307 for details.
    
    Bug: 349394307
    Test: atest KeyguardLockedTests
    Flag: TEST_ONLY
    (cherry picked from https://android-review.googlesource.com/q/commit:1a0302402c528c571bddc30d4fb1406d5fd27dac)
    Merged-In: Id133c174957cfd9c00a79e53323e5c9d8f0ded08
    Change-Id: Id133c174957cfd9c00a79e53323e5c9d8f0ded08

--- tests/framework/base/windowmanager/src/android/server/wm/keyguard/KeyguardLockedTests.java
+++ tests/framework/base/windowmanager/src/android/server/wm/keyguard/KeyguardLockedTests.java
@@ -61,6 +61,7 @@ import androidx.test.filters.FlakyTest;
 import androidx.test.uiautomator.UiDevice;
 
 import com.android.compatibility.common.util.CtsTouchUtils;
+import com.android.compatibility.common.util.FeatureUtil;
 import com.android.compatibility.common.util.PollingCheck;
 import com.android.cts.mockime.ImeEventStream;
 import com.android.cts.mockime.MockImeSession;
@@ -223,8 +224,9 @@ public class KeyguardLockedTests extends KeyguardTestBase {
 
     @Test
     public void testDismissKeyguardActivity_method_cancelled() {
-        // Pressing the back button does not cancel Keyguard in AAOS.
+        // Pressing the back button does not cancel Keyguard in AAOS or XR.
         assumeFalse(isCar());
+        assumeFalse(FeatureUtil.isXrHeadset());
 
         final LockScreenSession lockScreenSession = createManagedLockScreenSession();
         lockScreenSession.setLockCredential();
