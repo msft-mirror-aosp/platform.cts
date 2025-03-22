d276c1d6a68e7e2f892ff6f902a61d4719ca32d4
Antony Sargent <asargent@google.com>
2025-03-21 18:28:23 -0700

    Fix a race condition in two MediaRouter2Test cases
    Both tests were assuming they could safely register an event listener
    after taking an action that would trigger the event, and not have it
    fire. Then later they took a different action, and would assert that
    it did not cause the event to fire. But sometimes we got unlucky and
    the event (caused by the first action) would end up firing, and the
    tests would fail.
    
    Test: atest MediaRouter2Test
    Flag: TEST_ONLY
    Fixes: 403913552
    Fixes: 399019050
    Bug: 291800179
    Change-Id: I0bbaebffc4a650eb5502b284029c5bfe188fe309

--- tests/tests/media/router/src/android/media/router/cts/MediaRouter2Test.java
+++ tests/tests/media/router/src/android/media/router/cts/MediaRouter2Test.java
@@ -109,9 +109,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
@@ -1260,7 +1262,6 @@ public class MediaRouter2Test {
         assertThat(routeTransferFrom).isNotNull();
 
         final CountDownLatch onTransferLatch = new CountDownLatch(1);
-        final CountDownLatch onControllerUpdatedLatch = new CountDownLatch(1);
         final CountDownLatch onStopLatch = new CountDownLatch(1);
         final List<RoutingController> controllers = new ArrayList<>();
 
@@ -1285,41 +1286,50 @@ public class MediaRouter2Test {
             }
         };
 
-        ControllerCallback controllerCallback = new ControllerCallback() {
-            @Override
-            public void onControllerUpdated(RoutingController controller) {
-                if (onTransferLatch.getCount() != 0
-                        || !TextUtils.equals(controllers.get(0).getId(), controller.getId())) {
-                    return;
-                }
-                onControllerUpdatedLatch.countDown();
-            }
-        };
+        // We want to verify that our ControllerCallback only fires once, for the call to transferTo
+        // for ROUTE_ID1. This queue stores the selected routes for any invocations of the
+        // callback for our stub controller.
+        BlockingQueue<List<String>> controllerUpdatedCalls = new LinkedBlockingQueue<>();
+
+        ControllerCallback controllerCallback =
+                new ControllerCallback() {
+                    @Override
+                    public void onControllerUpdated(RoutingController controller) {
+                        if (TextUtils.equals(controllers.get(0).getId(), controller.getId())) {
+                            controllerUpdatedCalls.add(
+                                    controller.getSelectedRoutes().stream()
+                                            .map(i -> i.getOriginalId())
+                                            .toList());
+                        }
+                    }
+                };
 
         // TODO: Remove this once the MediaRouter2 becomes always connected to the service.
         RouteCallback routeCallback = new RouteCallback() {};
         mRouter2.registerRouteCallback(mExecutor, routeCallback, EMPTY_DISCOVERY_PREFERENCE);
 
         try {
+            mRouter2.registerControllerCallback(mExecutor, controllerCallback);
+
             mRouter2.registerTransferCallback(mExecutor, transferCallback);
             mRouter2.transferTo(routeTransferFrom);
             assertThat(onTransferLatch.await(TIMEOUT_MS, TimeUnit.MILLISECONDS)).isTrue();
+            assertThat(controllerUpdatedCalls.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS))
+                    .containsExactlyElementsIn(List.of(ROUTE_ID1));
 
             assertThat(controllers).hasSize(1);
             RoutingController controller = controllers.get(0);
 
-            // Registering the callback here to avoid unrelated calls related to the transfer above.
-            mRouter2.registerControllerCallback(mExecutor, controllerCallback);
             mRouter2.stop();
 
             // Select ROUTE_ID5_TO_TRANSFER_TO
             MediaRoute2Info routeToSelect = routes.get(ROUTE_ID4_TO_SELECT_AND_DESELECT);
             assertThat(routeToSelect).isNotNull();
 
-            // This call should be ignored.
-            // The onSessionInfoChanged() shouldn't be called.
+            // The selectRoute call should be ignored, and onControllerUpdated() shouldn't be
+            // called a second time.
             controller.selectRoute(routeToSelect);
-            assertThat(onControllerUpdatedLatch.await(WAIT_MS, TimeUnit.MILLISECONDS)).isFalse();
+            assertThat(controllerUpdatedCalls.poll(WAIT_MS, TimeUnit.MILLISECONDS)).isNull();
 
             // onStop should be called.
             assertThat(onStopLatch.await(TIMEOUT_MS, TimeUnit.MILLISECONDS)).isTrue();
@@ -1331,7 +1341,6 @@ public class MediaRouter2Test {
         }
     }
 
-    @Ignore // TODO(b/291800179): Diagnose flakiness and re-enable.
     @Test
     public void testRoutingControllerRelease() throws Exception {
         setUpStubProvider();
@@ -1344,7 +1353,6 @@ public class MediaRouter2Test {
         assertThat(routeTransferFrom).isNotNull();
 
         final CountDownLatch onTransferLatch = new CountDownLatch(1);
-        final CountDownLatch onControllerUpdatedLatch = new CountDownLatch(1);
         final CountDownLatch onStopLatch = new CountDownLatch(1);
         final List<RoutingController> controllers = new ArrayList<>();
 
@@ -1369,16 +1377,23 @@ public class MediaRouter2Test {
             }
         };
 
-        ControllerCallback controllerCallback = new ControllerCallback() {
-            @Override
-            public void onControllerUpdated(RoutingController controller) {
-                if (onTransferLatch.getCount() != 0
-                        || !TextUtils.equals(controllers.get(0).getId(), controller.getId())) {
-                    return;
-                }
-                onControllerUpdatedLatch.countDown();
-            }
-        };
+        // We want to verify that our ControllerCallback only fires once, for the call to transferTo
+        // for ROUTE_ID1. This queue stores the selected routes for any invocations of the
+        // callback for our stub controller.
+        BlockingQueue<List<String>> controllerUpdatedCalls = new LinkedBlockingQueue<>();
+
+        ControllerCallback controllerCallback =
+                new ControllerCallback() {
+                    @Override
+                    public void onControllerUpdated(RoutingController controller) {
+                        if (TextUtils.equals(controllers.get(0).getId(), controller.getId())) {
+                            controllerUpdatedCalls.add(
+                                    controller.getSelectedRoutes().stream()
+                                            .map(i -> i.getOriginalId())
+                                            .toList());
+                        }
+                    }
+                };
 
        // TODO: Remove this once the MediaRouter2 becomes always connected to the service.
         RouteCallback routeCallback = new RouteCallback() {};
@@ -1389,6 +1404,8 @@ public class MediaRouter2Test {
             mRouter2.registerControllerCallback(mExecutor, controllerCallback);
             mRouter2.transferTo(routeTransferFrom);
             assertThat(onTransferLatch.await(TIMEOUT_MS, TimeUnit.MILLISECONDS)).isTrue();
+            assertThat(controllerUpdatedCalls.poll(TIMEOUT_MS, TimeUnit.MILLISECONDS))
+                    .containsExactlyElementsIn(List.of(ROUTE_ID1));
 
             assertThat(controllers).hasSize(1);
             RoutingController controller = controllers.get(0);
@@ -1400,10 +1417,10 @@ public class MediaRouter2Test {
             MediaRoute2Info routeToSelect = routes.get(ROUTE_ID4_TO_SELECT_AND_DESELECT);
             assertThat(routeToSelect).isNotNull();
 
-            // This call should be ignored.
-            // The onSessionInfoChanged() shouldn't be called.
+            // The selectRoute call should be ignored, and onControllerUpdated() shouldn't be
+            // called a second time.
             controller.selectRoute(routeToSelect);
-            assertThat(onControllerUpdatedLatch.await(WAIT_MS, TimeUnit.MILLISECONDS)).isFalse();
+            assertThat(controllerUpdatedCalls.poll(WAIT_MS, TimeUnit.MILLISECONDS)).isNull();
 
             // onStop should be called.
             assertThat(onStopLatch.await(TIMEOUT_MS, TimeUnit.MILLISECONDS)).isTrue();
