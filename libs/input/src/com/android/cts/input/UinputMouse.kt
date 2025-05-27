d7d72a801747c45d6c8e17c2eaba89a4d36a91b3
Harry Cutts <hcutts@google.com>
2025-05-27 16:51:14 +0000

    input: add a test for mouse pointer capture
    This doesn't currently catch b/418908873, since it captures events at
    the activity level rather than from a particular view, but we should
    still have some CTS tests for this anyway.
    
    Bug: 411389468
    Test: $ atest cts/tests/input/src/android/input/cts/MouseCaptureTest.kt
    Test: $ atest CtsInputTestCases
    Flag: TEST_ONLY
    Change-Id: Iff6f317c54068890f9888a619c714b6e82fafcaf

---
+++ libs/input/src/com/android/cts/input/UinputMouse.kt
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.cts.input
+
+import android.app.Instrumentation
+import android.view.InputDevice.SOURCE_MOUSE
+import com.android.cts.input.EvdevInputEventCodes.Companion.EV_KEY
+import com.android.cts.input.EvdevInputEventCodes.Companion.EV_REL
+import com.android.cts.input.EvdevInputEventCodes.Companion.EV_SYN
+import com.android.cts.input.EvdevInputEventCodes.Companion.REL_HWHEEL
+import com.android.cts.input.EvdevInputEventCodes.Companion.REL_HWHEEL_HI_RES
+import com.android.cts.input.EvdevInputEventCodes.Companion.REL_WHEEL
+import com.android.cts.input.EvdevInputEventCodes.Companion.REL_WHEEL_HI_RES
+import com.android.cts.input.EvdevInputEventCodes.Companion.REL_X
+import com.android.cts.input.EvdevInputEventCodes.Companion.REL_Y
+import com.android.cts.input.EvdevInputEventCodes.Companion.SYN_REPORT
+
+private fun createMouseRegisterCommand(): UinputRegisterCommand {
+    val configurationItems = listOf(
+        ConfigurationItem("UI_SET_EVBIT", listOf("EV_KEY", "EV_REL")),
+        ConfigurationItem(
+            "UI_SET_KEYBIT",
+            listOf("BTN_LEFT", "BTN_MIDDLE", "BTN_RIGHT")
+        ),
+        ConfigurationItem(
+            "UI_SET_RELBIT",
+            listOf(
+                "REL_X",
+                "REL_Y",
+                "REL_WHEEL",
+                "REL_WHEEL_HI_RES",
+                "REL_HWHEEL",
+                "REL_HWHEEL_HI_RES",
+            )
+        ),
+    )
+
+    return UinputRegisterCommand(
+        id = 1,
+        name = "Test Mouse (USB)",
+        vid = 0x18d1,
+        pid = 0xabcd,
+        bus = "usb",
+        port = "usb:1",
+        configuration = configurationItems,
+        absInfo = emptyMap(),
+    )
+}
+
+/**
+ * A three-button mouse.
+ */
+class UinputMouse(instrumentation: Instrumentation) : UinputDevice(
+    instrumentation,
+    SOURCE_MOUSE,
+    createMouseRegisterCommand(),
+    null // display
+) {
+    companion object {
+        const val HI_RES_WHEEL_UNITS_PER_TICK = 120
+    }
+
+    private val pressedButtons = mutableSetOf<Int>()
+
+    private fun injectEvent(events: IntArray) {
+        injectEvents(
+            events.joinToString(prefix = "[", postfix = "]", separator = ",")
+        )
+    }
+
+    fun pressButton(button: Int) {
+        if (!pressedButtons.add(button)) {
+            throw IllegalStateException("Button $button is already pressed")
+        }
+        injectEvent(intArrayOf(EV_KEY, button, 1))
+    }
+
+    fun releaseButton(button: Int) {
+        if (!pressedButtons.remove(button)) {
+            throw IllegalStateException("Button $button is not pressed")
+        }
+        injectEvent(intArrayOf(EV_KEY, button, 0))
+    }
+
+    fun move(dx: Int, dy: Int) {
+        injectEvent(intArrayOf(EV_REL, REL_X, dx))
+        injectEvent(intArrayOf(EV_REL, REL_Y, dy))
+    }
+
+    fun scrollVertically(amount: Float) {
+        injectEvent(intArrayOf(EV_REL, REL_WHEEL, amount.toInt()))
+        injectEvent(
+            intArrayOf(EV_REL, REL_WHEEL_HI_RES, (amount * HI_RES_WHEEL_UNITS_PER_TICK).toInt())
+        )
+    }
+
+    fun scrollHorizontally(amount: Float) {
+        injectEvent(intArrayOf(EV_REL, REL_HWHEEL, amount.toInt()))
+        injectEvent(
+            intArrayOf(EV_REL, REL_HWHEEL_HI_RES, (amount * HI_RES_WHEEL_UNITS_PER_TICK).toInt())
+        )
+    }
+
+    fun sync() {
+        injectEvent(intArrayOf(EV_SYN, SYN_REPORT, 0))
+    }
+}
