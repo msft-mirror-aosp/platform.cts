defae323fc25c4cc3a32fd0affa816a906ab8b7f
Clemenz Portmann <portmannc@google.com>
2025-05-30 09:35:35 -0700

    ITS: add S10 FE tablet in its_session_utils.py
    Bug: 418267920
    Flag: TEST_ONLY
    Testing: confirmed at partner and with S25 ultra locally
    Change-Id: I59a3dfac9c958b0443eb2a3a582d28f46ebaf962

--- apps/CameraITS/utils/its_session_utils.py
+++ apps/CameraITS/utils/its_session_utils.py
@@ -78,6 +78,7 @@ TABLET_ALLOWLIST = (
     'gta8',  # Samsung Galaxy Tab A8 LTE
     'gta9pwifi',  # Samsung Galaxy Tab A9+
     'gta9p',  # Samsung Galaxy Tab A9+ 5G
+    'gts10fewifi',  # Samsung Galaxy Tab S10 FE
     'dpd2221',  # Vivo Pad2
     'nabu',  # Xiaomi Pad 5
     'nabu_tw',  # Xiaomi Pad 5
