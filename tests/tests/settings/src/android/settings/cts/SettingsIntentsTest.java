5af30f43db8ed9ab96168e79b334eb1930993eaf
jinjeong <jinjeong@google.com>
2025-06-20 00:31:27 +0000

    Cleanup: removal flags
    com.android.internal.telephony.flags.action_sim_preference_settings flag and references
    
    Bug: b/423931764
    Test: atest
    Flag: EXEMPT (remove flag)
    Change-Id: Ia267a8ea17261f306de37c73e1b9437032619795

--- tests/tests/settings/src/android/settings/cts/SettingsIntentsTest.java
+++ tests/tests/settings/src/android/settings/cts/SettingsIntentsTest.java
@@ -28,7 +28,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.platform.test.annotations.RequiresFlagsEnabled;
 import android.platform.test.flag.junit.CheckFlagsRule;
 import android.platform.test.flag.junit.DeviceFlagsValueProvider;
 import android.provider.Settings;
@@ -38,7 +37,6 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import com.android.compatibility.common.util.UserHelper;
-import com.android.internal.telephony.flags.Flags;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -95,7 +93,6 @@ public class SettingsIntentsTest {
     }
 
     @Test
-    @RequiresFlagsEnabled(Flags.FLAG_ACTION_SIM_PREFERENCE_SETTINGS)
     public void testSimPreferenceIntentReceiverExists() {
         // Skipping for visible background users as SIM is not supported on visible background
         // users.
