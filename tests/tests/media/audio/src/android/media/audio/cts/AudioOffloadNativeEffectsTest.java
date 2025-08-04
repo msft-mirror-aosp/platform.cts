03f87b926d65455283fd2d002c195d05c5ad6404
Aayush Soni <aayushsoni@google.com>
2025-08-04 06:10:59 +0000

    cts: Modify and rename AudioOffloadNativeTest to test mmap pcm offload for Visualizer effect
    Flag: TEST_ONLY
    Bug: 429224473
    Test: atest CtsMediaAudioTestCases:AudioOffloadNativeEffectsTest
    
    Change-Id: I65b07150b010a39ebc8f3aacf8dcfa7ff4e67600

---
+++ tests/tests/media/audio/src/android/media/audio/cts/AudioOffloadNativeEffectsTest.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.media.audio.cts;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeNotNull;
+import static org.junit.Assume.assumeTrue;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.AudioAttributes;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.audiofx.Visualizer;
+import android.os.Build;
+import android.os.SystemClock;
+import android.util.Log;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SdkSuppress;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.android.compatibility.common.util.FrameworkSpecificTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@SdkSuppress(minSdkVersion = Build.VERSION_CODES.VANILLA_ICE_CREAM)
+@FrameworkSpecificTest
+@RunWith(AndroidJUnit4.class)
+public class AudioOffloadNativeEffectsTest {
+    private static final String TAG = "AudioOffloadNativeEffectsTest";
+
+    private static final int STATUS_OK = 0;
+    private static final float AUDIOTRACK_DEFAULT_FREQUENCY_HZ = 910;
+    private static final int AUDIOTRACK_DEFAULT_SAMPLE_RATE = 48000;
+    private static final int PER_TEST_TIMEOUT_SMALL_TEST_MS = 60000;
+    private static final int PLAYBACK_COMPLETION_DELAY_MS = 10000;
+    private static final int VISUALIZER_SILENCE_MB = -9600; // RMS value for silence in millibels
+
+    private final int mStreamType = AudioManager.STREAM_MUSIC;
+    private AudioManager mAudioManager = null;
+    private int mOriginalVolume;
+    private int mSessionId = 0;
+    private long mStreamHandle = 0;
+    private Visualizer mVisualizer = null;
+
+    static {
+        System.loadLibrary("audiocts_aaudio_jni");
+    }
+
+    @Before
+    public void setup() throws Exception {
+        final Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assumeTrue("Skipping test, no audio output found", hasAudioOutput(context));
+
+        // TODO (b/452270871): try all pcm formats and open the stream with first one
+        // supported
+        // The encoding, sample rate and the channel mask should be the same as the
+        // one in native for creating stream.
+        final boolean offloadSupported =
+                AudioManager.isOffloadedPlaybackSupported(
+                        new AudioFormat.Builder()
+                                .setEncoding(AudioFormat.ENCODING_PCM_FLOAT)
+                                .setSampleRate(AUDIOTRACK_DEFAULT_SAMPLE_RATE)
+                                .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
+                                .build(),
+                        new AudioAttributes.Builder().build());
+
+        assumeTrue("Skipping test, offload support not found", offloadSupported);
+
+        mAudioManager = context.getSystemService(AudioManager.class);
+        mOriginalVolume = mAudioManager.getStreamVolume(mStreamType);
+
+        // Set a reasonable volume to ensure the Visualizer captures a non-silent
+        // signal.
+        mAudioManager.setStreamVolume(
+                AudioManager.STREAM_MUSIC,
+                mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) / 2,
+                /* flag= */ 0);
+
+        mStreamHandle = nativeOpenStream();
+        assertTrue("Failed to open native stream", mStreamHandle != 0);
+    }
+
+    @After
+    public void teardown() {
+        if (mAudioManager != null) {
+            mAudioManager.setStreamVolume(mStreamType, mOriginalVolume, /* flag */ 0);
+        }
+        if (mVisualizer != null) {
+            mVisualizer.release();
+        }
+        if (mStreamHandle != 0) {
+            nativeCloseStream(mStreamHandle);
+        }
+    }
+
+    private boolean hasAudioOutput(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_AUDIO_OUTPUT);
+    }
+
+    private void setupVisualizer() {
+        mSessionId = nativeGetSessionId(mStreamHandle);
+        assumeTrue("Failed to get a valid session ID", mSessionId > 0);
+        mVisualizer = new Visualizer(mSessionId);
+        assumeNotNull("Failed to create Visualizer effect", mVisualizer);
+        assumeTrue(
+                Visualizer.SUCCESS
+                        == mVisualizer.setCaptureSize(
+                                Visualizer.getCaptureSizeRange()[1])); // Max size
+        assumeTrue(
+                Visualizer.SUCCESS
+                        == mVisualizer.setMeasurementMode(Visualizer.MEASUREMENT_MODE_PEAK_RMS));
+        assumeTrue(Visualizer.SUCCESS == mVisualizer.setEnabled(true));
+    }
+
+    private int playAndGetRms(float testFrequencyHz) throws InterruptedException {
+        assertNotNull("Visualizer must be initialized first", mVisualizer);
+        assertEquals(Visualizer.SUCCESS, mVisualizer.setEnabled(false)); // Reset measurement
+        assertEquals(Visualizer.SUCCESS, mVisualizer.setEnabled(true));
+
+        assertEquals(
+                "Native playback failed",
+                STATUS_OK,
+                nativePlayAndSignalEnd(mStreamHandle, testFrequencyHz));
+        SystemClock.sleep(PLAYBACK_COMPLETION_DELAY_MS);
+
+        Visualizer.MeasurementPeakRms measurement = new Visualizer.MeasurementPeakRms();
+        assertEquals(Visualizer.SUCCESS, mVisualizer.getMeasurementPeakRms(measurement));
+        return measurement.mRms;
+    }
+
+    @Test(timeout = PER_TEST_TIMEOUT_SMALL_TEST_MS)
+    public void testMmapPcmOffload() throws InterruptedException {
+        assertEquals(
+                "Native playback failed",
+                STATUS_OK,
+                nativePlayAndSignalEnd(mStreamHandle, AUDIOTRACK_DEFAULT_FREQUENCY_HZ));
+        SystemClock.sleep(PLAYBACK_COMPLETION_DELAY_MS);
+    }
+
+    @Test(timeout = PER_TEST_TIMEOUT_SMALL_TEST_MS)
+    public void testMmapPcmOffloadWithVisualizerEffect() throws InterruptedException {
+        setupVisualizer();
+        final int rmsMb = playAndGetRms(AUDIOTRACK_DEFAULT_FREQUENCY_HZ);
+        Log.i(TAG, "Measured Rms: " + rmsMb);
+        assumeTrue("rms should not represent silence", rmsMb != VISUALIZER_SILENCE_MB);
+    }
+
+    private static native long nativeOpenStream();
+
+    private static native int nativeGetSessionId(long streamHandle);
+
+    private static native int nativePlayAndSignalEnd(long streamHandle, float testFrequencyHz);
+
+    private static native void nativeCloseStream(long streamHandle);
+}
