1fbd39b56712d26a8a6b31ac55a798420e0259a6
Victor Gabriel Savu <vsavu@google.com>
2025-08-11 13:33:04 +0000

    Use metalava in remoteframeworkclasses
    Replace the hand-written parser for APIs with metalava.
    
    Bug: 436548677
    Test: atest android.devicepolicy.cts.FunTest
    Flag: TEST_ONLY
    Change-Id: I64b7fbb1aa05f63230bd5115cdb79199137d6e6d

--- common/device-side/bedstead/remoteframeworkclasses/src/processor/main/java/com/android/bedstead/remoteframeworkclasses/processor/Apis.kt
+++ common/device-side/bedstead/remoteframeworkclasses/src/processor/main/java/com/android/bedstead/remoteframeworkclasses/processor/Apis.kt
@@ -15,7 +15,9 @@
  */
 package com.android.bedstead.remoteframeworkclasses.processor
 
-import com.google.common.collect.ImmutableSet
+import com.android.tools.metalava.model.Codebase
+import com.android.tools.metalava.model.text.ApiFile
+import com.android.tools.metalava.model.text.SignatureFile
 import com.google.common.io.Resources
 import java.io.IOException
 import java.nio.charset.StandardCharsets
@@ -27,11 +29,11 @@ import javax.lang.model.util.Types
 /**
  * A collection of [MethodSignature] for accessible methods.
  */
-class Apis private constructor(private val mMethods: ImmutableSet<MethodSignature?>?) {
+class Apis private constructor(private val mMethods: Set<MethodSignature>) {
     /**
      * Get methods in the API set.
      */
-    fun methods(): ImmutableSet<MethodSignature?>? {
+    fun methods(): Set<MethodSignature> {
         return mMethods
     }
 
@@ -43,42 +45,38 @@ class Apis private constructor(private val mMethods: ImmutableSet<MethodSignatur
             "system-current.txt"
         )
 
-        private val API_TXTS: Map<String, String> = initialiseApiTxts()
-        private val sPackageToApi: Map<String?, Apis?> = HashMap<String?, Apis?>()
+        private val CODEBASE = initializeCodebase()
 
-        private fun initialiseApiTxts(): Map<String, String> {
-            return API_FILES.associate {
+        private fun initializeCodebase(): Codebase {
+            val apis = API_FILES.map {
                 try {
-                    it to Resources.toString(
+                     it to Resources.toString(
                         Processor::class.java.getResource("/apis/$it"),
                         StandardCharsets.UTF_8
                     )
                 } catch (e: IOException) {
                     throw IllegalStateException("Could not read file $it", e)
                 }
+            }.map { (name, content) ->
+                SignatureFile.fromText(name, content)
             }
+
+            return ApiFile.parseApi(apis)
         }
 
         /**
          * Get public and test APIs for a given class name.
          */
         @JvmStatic
-        fun forClass(className: String, types: Types, elements: Elements): Apis? {
-            if (sPackageToApi.containsKey(className)) {
-                return sPackageToApi.get(className)
-            }
-            val methods = ImmutableSet.builder<MethodSignature?>()
-            val parents: MutableSet<String> = HashSet<String>()
+        fun forClass(className: String, types: Types, elements: Elements): Apis {
+            val parents: MutableSet<String> = HashSet()
             findParents(parents, className, elements)
-            for (c in parents) {
-                for (apiTxt in API_TXTS.entries) {
-                    methods.addAll(
-                        Companion.parseApiTxt(apiTxt.key, apiTxt.value!!, c, types, elements)
-                    )
-                }
+
+            val methods: List<MethodSignature> = parents.flatMap {
+                getMethodsForClass(it, types, elements)
             }
 
-            return Apis(methods.build())
+            return Apis(methods.toSet())
         }
 
         private fun findParents(
@@ -93,81 +91,32 @@ class Apis private constructor(private val mMethods: ImmutableSet<MethodSignatur
             }
 
             val element = elements.getTypeElement(className)
-            println("Checking " + className + " got " + element)
 
-            val superClass = element.getSuperclass()
-            if (superClass.getKind() != TypeKind.NONE) {
+            val superClass = element.superclass
+            if (superClass.kind != TypeKind.NONE) {
                 findParents(parents, superClass.toString(), elements)
             }
 
-            element.getInterfaces()
-                .stream()
-                .map<String?> { obj: TypeMirror? -> obj.toString() }
-                .forEach { c: String? -> Companion.findParents(parents, c!!, elements) }
+            element.interfaces
+                .map { obj: TypeMirror? -> obj.toString() }
+                .forEach { c: String -> findParents(parents, c, elements) }
         }
 
-        private fun parseApiTxt(
-            filename: String?,
-            apiTxt: String,
+        private fun getMethodsForClass(
             className: String,
             types: Types,
             elements: Elements
-        ): MutableSet<MethodSignature?> {
-            println("Parsing for " + className)
+        ): List<MethodSignature> {
+            val clazz = CODEBASE.findClass(className)
 
-            val separatorPosition = className.lastIndexOf(".")
-            val packageName = className.substring(0, separatorPosition)
-            val simpleClassName = className.substring(separatorPosition + 1)
-
-            val packageSplit: Array<String?> =
-                apiTxt.split(("package " + packageName + " \\{").toRegex(), limit = 2)
-                    .toTypedArray()
-            if (packageSplit.size < 2) {
-                println("Package " + packageName + " not in file " + filename)
-                // Package not in this file
-                return HashSet<MethodSignature?>()
-            }
-            val classSplit: Array<String?> = packageSplit[1]!!.split(
-                ("class " + simpleClassName + " .*?\n").toRegex(),
-                limit = 2
-            ).toTypedArray()
-            if (classSplit.size < 2) {
-                println("Class " + simpleClassName + " not in file " + filename)
-                // Class not in this file
-                return HashSet<MethodSignature?>()
-            }
-            val lines =
-                classSplit[1]!!.split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-            val methodSignatures: MutableSet<MethodSignature?> = HashSet<MethodSignature?>()
-
-            for (line in lines) {
-                var methodLine = line.trim { it <= ' ' }
-                if (methodLine.isEmpty()) {
-                    continue
-                }
-
-                if (methodLine.startsWith("ctor")) {
-                    // Skip constructors
-                    continue
-                }
-
-                if (!methodLine.startsWith("method")) {
-                    return methodSignatures
-                }
-
-                try {
-                    // Strip "method" and semicolon
-                    methodLine = methodLine.substring(7, methodLine.length - 1)
-                    val signature = MethodSignature.forApiString(methodLine, types, elements)
-                    if (signature != null) {
-                        methodSignatures.add(signature)
-                    }
-                } catch (e: RuntimeException) {
-                    throw IllegalStateException("Error parsing method " + line, e)
-                }
+            if (clazz == null) {
+                println("Failed to find $className")
+                return listOf()
             }
 
-            return methodSignatures
+            return clazz.methods().mapNotNull { method ->
+                MethodSignature.forApi(method, types, elements)
+            }
         }
     }
 }
