f5d327603a7e51ad564d71018c436bbfa42a93de
Devin Moore <devinmoore@google.com>
2025-08-22 20:50:41 +0000

    Add at an STS test for b/438098181
    We will get a 0 from readInt with the fix. Without the fix it's an OOB
    read, so it might get a different value or might segfault.
    
    Test:  atest CtsSecurityTestCases:BinderAppendFromTest
    Bug: 438098181
    Flag: EXEMPT bug fix
    
    Change-Id: Ieb3e19e12aaf69fab9712f12aab4896b2cd6e7e1

--- tests/tests/security/src/android/security/cts/BinderAppendFromTest.java
+++ tests/tests/security/src/android/security/cts/BinderAppendFromTest.java
@@ -15,6 +15,9 @@
  */
 package android.security.cts;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertThrows;
 
 import android.os.Parcel;
@@ -45,5 +48,28 @@ public class BinderAppendFromTest extends StsExtraBusinessLogicTestCase {
                 () -> {
                     p2.appendFrom(p1, 0, 8);
                 });
+    }
+
+    @Test
+    @AsbSecurityTest(cveBugId = 438098181)
+    public void testBinderAppendFromBadSize() {
+        final int numInt32P1 = 32;
+        final int numInt32P2 = 10;
+
+        Parcel p1 = Parcel.obtain();
+        for (int i = 0; i < numInt32P1; i++) {
+            p1.writeInt(i);
+        }
+
+        Parcel p2 = Parcel.obtain();
+        for (int i = 0; i < numInt32P2; i++) {
+            p2.writeInt(i);
+        }
+
+        p1.setDataPosition(0);
+        p1.appendFrom(p2, 0, numInt32P2 * 4);
+
+        p1.setDataPosition(128);
+        assertThat(p1.readInt(), is(equalTo(0)));
     }
 }
