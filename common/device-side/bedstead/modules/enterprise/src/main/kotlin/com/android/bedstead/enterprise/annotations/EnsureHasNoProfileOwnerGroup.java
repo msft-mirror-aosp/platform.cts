704220b0ba8fc9c634013b8fcf1b52b20b2b0e64
Piotr Karzelek <karzelek@google.com>
2025-09-04 12:32:28 +0000

    Bedstead: make bedstead-enterprise annotations not visible in Bedstead
    Bug: 442871663
    Test: atest bedstead-enterprise-test
    Test: m CtsDevicePolicyTestCases
    Flag: TEST_ONLY
    Change-Id: Ib135f28117ba3e5305f66c89c08ce1ccde4df094

--- common/device-side/bedstead/modules/enterprise/src/main/kotlin/com/android/bedstead/enterprise/annotations/EnsureHasNoProfileOwnerGroup.java
+++ common/device-side/bedstead/modules/enterprise/src/main/kotlin/com/android/bedstead/enterprise/annotations/EnsureHasNoProfileOwnerGroup.java
@@ -16,7 +16,7 @@
 
 package com.android.bedstead.enterprise.annotations;
 
-import static com.android.bedstead.enterprise.annotations.EnsureHasDeviceOwner.DO_PO_PRIORITY;
+import static com.android.bedstead.harrier.annotations.AnnotationPriorityRunPrecedence.DO_PO_PRIORITY;
 
 import com.android.bedstead.harrier.annotations.meta.RepeatingAnnotation;
 
