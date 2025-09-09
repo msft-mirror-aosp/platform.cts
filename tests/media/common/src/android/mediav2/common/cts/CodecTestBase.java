7efae2bbc564c165f549a284d8257f00d2614fe8
Ram Mohan M <rammo@google.com>
2025-09-09 15:05:45 +0000

    mediav2: remove codeNameEquals() check for IS_AT_LEAST_B
    Bug: 248315681
    Test: atest MctsMediaV2TestCases
    Flag: TEST_ONLY
    Change-Id: I0d28309d3e05bcb8231749568dbf5b6d7854e011

--- tests/media/common/src/android/mediav2/common/cts/CodecTestBase.java
+++ tests/media/common/src/android/mediav2/common/cts/CodecTestBase.java
@@ -125,10 +125,7 @@ public abstract class CodecTestBase {
     public static final boolean IS_BEFORE_U = !IS_AT_LEAST_U;
     public static final boolean IS_AT_LEAST_V =
             ApiLevelUtil.isAtLeast(Build.VERSION_CODES.VANILLA_ICE_CREAM);
-    //TODO(b/248315681) Change to a proper condition
-    public static final boolean IS_AT_LEAST_B =
-            ApiLevelUtil.isAfter(Build.VERSION_CODES.VANILLA_ICE_CREAM)
-                || ApiLevelUtil.codenameEquals("Baklava");
+    public static final boolean IS_AT_LEAST_B = ApiLevelUtil.isAtLeast(Build.VERSION_CODES.BAKLAVA);
     public static final boolean FIRST_SDK_IS_AT_LEAST_T =
             ApiLevelUtil.isFirstApiAtLeast(Build.VERSION_CODES.TIRAMISU);
     public static final boolean FIRST_SDK_IS_AT_LEAST_V =
