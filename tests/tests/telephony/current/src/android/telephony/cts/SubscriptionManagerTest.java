989695da8f9c6dbe8ca1a256d9a0afe3ac9e031e
Abhinav Yadav <yadavabhinav@google.com>
2025-09-10 01:21:00 -0700

    Fix wrong phone Number set in testSetAndGetPhoneNumber
    1. The testSetAndGetPhoneNumber test modifies the carrier-provided phone
    number using setCarrierPhoneNumber but fails to restore the original
    value.
    2. This lack of restoring original phone number leads to failures in
    subsequent tests that rely on the phone number, such as
    VisualVoicemailServiceTest.
    3. This change corrects the issue by saving the original carrier phone
    number before the test modifies it and restoring it . A typo in the
    method name is also corrected.
    
    Bug: 422268623
    Bug: 312666605
    Flag: NONE CTS fix.
    Test: atestSubscriptionManagerTest#testSetAndGetPhoneNumber followed by atest VisualVoicemailServiceTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:d6f52a14dfbf672304951576c56baf4d8ee0f062)
    Merged-In: I9d8c660865f66aedfb9fda661348fb7cc4952dd9
    Change-Id: I9d8c660865f66aedfb9fda661348fb7cc4952dd9
    
    ATTENTION: Conflicts occurred while applying the patch.
    Please resolve conflict markers.
    
    Change-Id: Ie83660917a1c6d131e117daba1d13898188c43d1

--- tests/tests/telephony/current/src/android/telephony/cts/SubscriptionManagerTest.java
+++ tests/tests/telephony/current/src/android/telephony/cts/SubscriptionManagerTest.java
@@ -1134,7 +1134,7 @@ public class SubscriptionManagerTest {
     }
 
     @Test
-    public void tetsSetAndGetPhoneNumber() throws Exception {
+    public void testSetAndGetPhoneNumber() throws Exception {
         // The phone number may be anything depends on the state of SIM and device.
         // Simply call the getter and make sure no exception.
 
@@ -1174,11 +1174,16 @@ public class SubscriptionManagerTest {
                     mSm.getPhoneNumber(mSubId, SubscriptionManager.PHONE_NUMBER_SOURCE_UICC);
                     mSm.getPhoneNumber(mSubId, SubscriptionManager.PHONE_NUMBER_SOURCE_IMS);
 
+                    String originalPhoneNumber =
+                            mSm.getPhoneNumber(
+                                    mSubId, SubscriptionManager.PHONE_NUMBER_SOURCE_CARRIER);
                     mSm.setCarrierPhoneNumber(mSubId, carrierNumber);
                     assertEquals(
                             carrierNumber,
                             mSm.getPhoneNumber(
                                     mSubId, SubscriptionManager.PHONE_NUMBER_SOURCE_CARRIER));
+                    // Restore original phone number to prevent side effects on other tests.
+                    mSm.setCarrierPhoneNumber(mSubId, originalPhoneNumber);
                 });
 
         // Otherwise, getter and setter will hit SecurityException
