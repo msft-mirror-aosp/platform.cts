6be842482305cd761a748503b0f8def2c43e2bb8
Song Chun Fan <schfan@google.com>
2025-09-24 15:36:50 -0700

    [ADI][cts] fix CtsDeveloperVerificationHostTestCases for updated system apps
    The test assumed that the system apps involved in this test are not
    updated, but in real testing environments they could be updated and
    installed in the data partition in a split APK format. Update the test
    to accommodate that.
    
    Also fixed an issue where the update owner info can be lost during the
    test if the test first resinstalls the verifier via adb.
    
    FLAG: android.content.pm.verification_service
    BUG: 447056196
    FIXES: 447056196
    Test: atest com.android.tests.developerverification.host.DeveloperVerificationHostsideTest
    
    Change-Id: I9dd038d9f8b1873a63670ef2b6fd0296fd7899c1

--- hostsidetests/packagemanager/developerverification/src/com/android/tests/developerverification/host/DeveloperVerificationHostsideTest.java
+++ hostsidetests/packagemanager/developerverification/src/com/android/tests/developerverification/host/DeveloperVerificationHostsideTest.java
@@ -21,6 +21,7 @@ import static android.content.pm.Flags.FLAG_VERIFICATION_SERVICE;
 import static com.google.common.truth.Truth.assertThat;
 
 import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeNotNull;
 
 import android.platform.test.annotations.AppModeFull;
 import android.platform.test.annotations.RequiresFlagsEnabled;
@@ -29,6 +30,7 @@ import android.platform.test.flag.junit.host.HostFlagsValueProvider;
 
 import com.android.compatibility.common.tradefed.build.CompatibilityBuildHelper;
 import com.android.compatibility.common.util.CddTest;
+import com.android.ddmlib.Log;
 import com.android.tradefed.build.IBuildInfo;
 import com.android.tradefed.device.DeviceNotAvailableException;
 import com.android.tradefed.device.ITestDevice;
@@ -44,6 +46,8 @@ import org.junit.runner.RunWith;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @CddTest(requirements = {"9.18/C-3-2"})
 @RunWith(DeviceJUnit4ClassRunner.class)
@@ -55,6 +59,7 @@ public class DeveloperVerificationHostsideTest extends BaseHostJUnit4Test
     public final CheckFlagsRule mCheckFlagsRule =
             HostFlagsValueProvider.createCheckFlagsRule(this::getDevice);
 
+    private static final String TAG = "DeveloperVerificationHostsideTest";
     private static final String TEST_BASE = "DeveloperVerificationHostsideTest";
     private static final String TEST_INSTALL_APK = "CtsStatsdAtomEmptyApp.apk";
     private static final String NO_VERIFIER_RESULT = "No verification service provider specified.";
@@ -98,15 +103,31 @@ public class DeveloperVerificationHostsideTest extends BaseHostJUnit4Test
         String verifierPackageName =
                 getPackageNameFromComponentNameString(verifierComponentNameStr);
         assertThat(verifierPackageName).isNotNull();
-        String verifierApkPathOnDevice = getApkPath(verifierPackageName);
-        assertThat(verifierApkPathOnDevice).isNotNull();
-        File verifierApkLocal = pullFile(verifierApkPathOnDevice);
-        assertThat(verifierApkLocal).isNotNull();
+        String updateOwnerPackageName = getUpdateOwnerPackageName(verifierPackageName);
+        List<String> verifierApksPathsOnDevice = getApksPaths(verifierPackageName);
+        assertThat(verifierApksPathsOnDevice).isNotEmpty();
+        List<File> verifierApksLocal = pullFiles(verifierApksPathsOnDevice);
+        assertThat(verifierApksLocal).isNotEmpty();
+        // Use --update-ownership and -i to make sure the original update ownership info is not lost
+        ArrayList<String> extraArgs = new ArrayList<>();
+        extraArgs.add("--force-verification");
+        if (updateOwnerPackageName != null) {
+            extraArgs.add("--update-ownership");
+            extraArgs.add("-i " + updateOwnerPackageName);
+        }
         // The reinstallation should always go through
-        assertThat(mDevice.installPackage(verifierApkLocal, true, "--force-verification")).isNull();
-        String verifierApkPathOnDeviceAfterReinstall = getApkPath(verifierPackageName);
-        assertThat(verifierApkPathOnDeviceAfterReinstall).isNotNull();
-        assertThat(verifierApkPathOnDeviceAfterReinstall).startsWith("/data");
+        assertThat(
+                        mDevice.installPackages(
+                                verifierApksLocal, true, extraArgs.toArray(new String[0])))
+                .isNull();
+        List<String> verifierApkPathsOnDeviceAfterReinstall = getApksPaths(verifierPackageName);
+        assertThat(verifierApkPathsOnDeviceAfterReinstall).isNotEmpty();
+        assertThat(verifierApkPathsOnDeviceAfterReinstall.getFirst()).startsWith("/data");
+        // Make sure that the update ownership is preserved
+        if (updateOwnerPackageName != null) {
+            assertThat(getUpdateOwnerPackageName(verifierPackageName))
+                    .isEqualTo(updateOwnerPackageName);
+        }
     }
 
     @Test
@@ -122,29 +143,35 @@ public class DeveloperVerificationHostsideTest extends BaseHostJUnit4Test
                 getPackageNameFromComponentNameString(verifierComponentNameStr);
         assertThat(verifierPackageName).isNotNull();
         String updateOwnerPackageName = getUpdateOwnerPackageName(verifierPackageName);
-        assertThat(updateOwnerPackageName).isNotNull();
-        String updateOwnerApkPathOnDevice = getApkPath(updateOwnerPackageName);
-        assertThat(updateOwnerApkPathOnDevice).isNotNull();
-        File updateOwnerApkLocal = pullFile(updateOwnerApkPathOnDevice);
-        assertThat(updateOwnerApkLocal).isNotNull();
+        // It is not required that the verifier must have an update owner. Skip if it doesn't.
+        assumeNotNull(updateOwnerPackageName);
+        List<String> updateOwnerApksPathsOnDevice = getApksPaths(updateOwnerPackageName);
+        assertThat(updateOwnerApksPathsOnDevice).isNotEmpty();
+        List<File> updateOwnerApksLocal = pullFiles(updateOwnerApksPathsOnDevice);
+        assertThat(updateOwnerApksLocal).isNotEmpty();
         // The reinstallation should always go through
-        assertThat(mDevice.installPackage(updateOwnerApkLocal, true, "--force-verification"))
+        assertThat(mDevice.installPackages(updateOwnerApksLocal, true, "--force-verification"))
                 .isNull();
-        String updateOwnerApkPathOnDeviceAfterReinstall = getApkPath(updateOwnerPackageName);
-        assertThat(updateOwnerApkPathOnDeviceAfterReinstall).isNotNull();
-        assertThat(updateOwnerApkPathOnDeviceAfterReinstall).startsWith("/data");
+        List<String> updateOwnerApksPathsOnDeviceAfterReinstall =
+                getApksPaths(updateOwnerPackageName);
+        assertThat(updateOwnerApksPathsOnDeviceAfterReinstall).isNotEmpty();
+        assertThat(updateOwnerApksPathsOnDeviceAfterReinstall.getFirst()).startsWith("/data");
     }
 
-    private File pullFile(String pathOnDevice) {
-        try {
-            final File localTempFile =
-                    File.createTempFile(new File(pathOnDevice).getName(), "", sBasePath);
-            assertThat(localTempFile).isNotNull();
-            assertThat(mDevice.pullFile(pathOnDevice, localTempFile)).isTrue();
-            return localTempFile;
-        } catch (IOException | DeviceNotAvailableException e) {
-            return null;
+    private List<File> pullFiles(List<String> pathsOnDevice) {
+        List<File> localTempFiles = new ArrayList<>();
+        for (String pathOnDevice : pathsOnDevice) {
+            try {
+                final File localTempFile =
+                        File.createTempFile(new File(pathOnDevice).getName(), ".apk", sBasePath);
+                assertThat(localTempFile).isNotNull();
+                assertThat(mDevice.pullFile(pathOnDevice, localTempFile)).isTrue();
+                localTempFiles.add(localTempFile);
+            } catch (IOException | DeviceNotAvailableException e) {
+                Log.e(TAG, "Failed to pull file " + pathOnDevice);
+            }
         }
+        return localTempFiles;
     }
 
     private static String getPackageNameFromComponentNameString(String componentNameStr) {
@@ -157,13 +184,18 @@ public class DeveloperVerificationHostsideTest extends BaseHostJUnit4Test
         return componentNameStr.substring(start + 1, end);
     }
 
-    private String getApkPath(String packageName) throws DeviceNotAvailableException {
-        String apkPathStr = mDevice.executeShellCommand("pm path " + packageName).trim();
-        int start = apkPathStr.indexOf(':');
-        if (start == -1) {
-            return null;
+    private List<String> getApksPaths(String packageName) throws DeviceNotAvailableException {
+        String apksPathsStr = mDevice.executeShellCommand("pm path " + packageName).trim();
+        ArrayList<String> results = new ArrayList<>();
+        String[] apksPaths = apksPathsStr.split("\n");
+        for (String apkPath : apksPaths) {
+            int start = apkPath.indexOf(':');
+            if (start == -1) {
+                continue;
+            }
+            results.add(apkPath.substring(start + 1));
         }
-        return apkPathStr.substring(start + 1);
+        return results;
     }
 
     private String getUpdateOwnerPackageName(String packageName)
