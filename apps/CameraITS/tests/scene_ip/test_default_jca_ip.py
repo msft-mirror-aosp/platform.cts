5d1ea802acb8c38da057a8632199cbffed0063d0
Rucha Katakwar <ruchamk@google.com>
2025-09-29 16:43:52 -0700

    ITS: Color rendering diff between JCA and default
    This cl contains the check for color rendering between the JCA and
    default captured images. It extracts the color checker chart from the
    images and each color cell. These cell patches are then compared to each
    other for the getting the difference.
    
    Bug: 407813642
    Test: Tested on a pixel device
    FLAG: TEST_ONLY
    Change-Id: I43da752c1bcea748721bfa98632ac241eee47334

--- apps/CameraITS/tests/scene_ip/test_default_jca_ip.py
+++ apps/CameraITS/tests/scene_ip/test_default_jca_ip.py
@@ -388,6 +388,9 @@ class DefaultJcaImageParityClassTest(its_base_test.ItsBaseTest):
       jca_cropped_color_cells = ce.get_cropped_color_cells(
           jca_capture_path, self.log_path, 'jca')
 
+      ip_metrics_utils.get_color_rendering_variation(
+          default_cropped_color_cells, jca_cropped_color_cells, gainmap_present)
+
       # logging for data collection
       print(f'{_NAME}_mean_white_balance_diff: {mean_white_balance_diff}')
       logging.debug('mean_white_balance_diff: %f', mean_white_balance_diff)
