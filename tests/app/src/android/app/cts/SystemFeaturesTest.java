2a9da7c50f9a8b3174f06c7c42dbf4aaca039078
Neill Kapron <nkapron@google.com>
2025-10-07 13:16:40 -0700

    CTS: remove testUsbAccessory
    FEATURE_USB_ACCESSORY is only required to be defined "If device implementations include a USB port and implement the AOA specificaiton".
    
    No devices are 'required' to implement the AOA specification per CDD.
    This should be tested in VTS.
    
    Bug: 437289384
    Bug: 443726732
    Flag: EXEMPT TEST_ONLY
    Change-Id: I924863ced287b269ab3c92aaed40dc5efc9af34e

--- tests/app/src/android/app/cts/SystemFeaturesTest.java
+++ tests/app/src/android/app/cts/SystemFeaturesTest.java
@@ -571,25 +571,6 @@ public class SystemFeaturesTest {
     }
 
     @Test
-    public void testUsbAccessory() {
-        if (!mPackageManager.hasSystemFeature(PackageManager.FEATURE_AUTOMOTIVE) &&
-                !mPackageManager.hasSystemFeature(PackageManager.FEATURE_LEANBACK) &&
-                !mPackageManager.hasSystemFeature(PackageManager.FEATURE_WATCH) &&
-                !mPackageManager.hasSystemFeature(PackageManager.FEATURE_EMBEDDED) &&
-                !Build.IS_EMULATOR &&
-                !mPackageManager.hasSystemFeature(PackageManager.FEATURE_PC) &&
-                mPackageManager.hasSystemFeature(PackageManager.FEATURE_MICROPHONE) &&
-                mPackageManager.hasSystemFeature(PackageManager.FEATURE_TOUCHSCREEN) &&
-                !FeatureUtil.isXrHeadset()) {
-            // USB accessory mode is only a requirement for devices with USB ports supporting
-            // peripheral mode. As there is no public API to distinguish a device with only host
-            // mode support from having both peripheral and host support, the test may have
-            // false negatives.
-            assertAvailable(PackageManager.FEATURE_USB_ACCESSORY);
-        }
-    }
-
-    @Test
     public void testWifiFeature() throws Exception {
         if (!mPackageManager.hasSystemFeature(PackageManager.FEATURE_WIFI)) {
             // no WiFi, skip the test
