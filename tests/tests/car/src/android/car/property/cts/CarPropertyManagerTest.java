22f3515d2842dcf3a984eb8e5ed2568abb836ef0
Aaqib Ismail <aaqibismail@google.com>
2025-10-14 22:40:04 -0700

    Catch PropertyAccessDeniedSecurityException which is a valid exception
    Flag: EXEMPT TEST_ONLY
    Bug: 448244105
    Test: atest CtsCarTestCases:CarPropertyManagerTest
    Change-Id: I9fb9170fe8a8f3ae2556a9cd1e88169cf799792a

--- tests/tests/car/src/android/car/property/cts/CarPropertyManagerTest.java
+++ tests/tests/car/src/android/car/property/cts/CarPropertyManagerTest.java
@@ -123,6 +123,7 @@ import android.car.hardware.property.LaneDepartureWarningState;
 import android.car.hardware.property.LaneKeepAssistState;
 import android.car.hardware.property.LowSpeedAutomaticEmergencyBrakingState;
 import android.car.hardware.property.LowSpeedCollisionWarningState;
+import android.car.hardware.property.PropertyAccessDeniedSecurityException;
 import android.car.hardware.property.PropertyNotAvailableAndRetryException;
 import android.car.hardware.property.PropertyNotAvailableException;
 import android.car.hardware.property.Subscription;
@@ -4058,7 +4059,8 @@ public final class CarPropertyManagerTest extends AbstractCarTestCase {
                                 continue;
                             } catch (PropertyNotAvailableAndRetryException
                                     | PropertyNotAvailableException
-                                    | CarInternalErrorException e) {
+                                    | CarInternalErrorException
+                                    | PropertyAccessDeniedSecurityException e) {
                                 Log.w(
                                         TAG,
                                         "Failed to get property:"
@@ -4370,7 +4372,8 @@ public final class CarPropertyManagerTest extends AbstractCarTestCase {
                                 continue;
                             } catch (PropertyNotAvailableAndRetryException
                                     | PropertyNotAvailableException
-                                    | CarInternalErrorException e) {
+                                    | CarInternalErrorException
+                                    | PropertyAccessDeniedSecurityException e) {
                                 Log.w(
                                         TAG,
                                         "Failed getIntArrayProperty for property:"
