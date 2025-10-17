bd05e5b8b8710a993b229b6a1249d63b6307b83d
Yara Hassan <yaraabdullatif@google.com>
2025-10-17 16:27:51 +0100

    CTS test for AppFunctionMetadata
    Test: cts
    Bug: 438413081
    Flag: android.app.appfunctions.flags.enable_contextual_app_functions
    Change-Id: I6f68f50cbf51bbccd0d9fa366f1d9230f70ee775

--- tests/appfunctions/src/android/app/appfunctions/cts/AppFunctionMetadataTest.kt
+++ tests/appfunctions/src/android/app/appfunctions/cts/AppFunctionMetadataTest.kt
@@ -17,11 +17,17 @@ package android.app.appfunctions.cts
 
 import android.Manifest
 import android.app.appfunctions.AppFunctionManager
+import android.app.appfunctions.AppFunctionMetadata
+import android.app.appfunctions.AppFunctionName
+import android.app.appfunctions.AppFunctionPackageMetadata
 import android.app.appfunctions.AppFunctionRuntimeMetadata
+import android.app.appfunctions.AppFunctionSchemaMetadata
+import android.app.appfunctions.AppFunctionStaticMetadataHelper
 import android.app.appfunctions.cts.AppSearchUtils.collectAllSearchResults
 import android.app.appfunctions.flags.Flags
 import android.app.appfunctions.testutils.CtsTestUtil.retryAssert
 import android.app.appfunctions.testutils.CtsTestUtil.runWithShellPermission
+import android.app.appsearch.GenericDocument
 import android.app.appsearch.GlobalSearchSessionShim
 import android.app.appsearch.SearchResultsShim
 import android.app.appsearch.SearchSpec
@@ -32,10 +38,10 @@ import android.platform.test.flag.junit.CheckFlagsRule
 import android.platform.test.flag.junit.DeviceFlagsValueProvider
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.platform.app.InstrumentationRegistry
-import com.android.bedstead.harrier.BedsteadJUnit4
-import com.android.bedstead.harrier.DeviceState
 import com.android.bedstead.enterprise.annotations.parameterized.IncludeRunOnPrimaryUser
 import com.android.bedstead.enterprise.annotations.parameterized.IncludeRunOnSecondaryUser
+import com.android.bedstead.harrier.BedsteadJUnit4
+import com.android.bedstead.harrier.DeviceState
 import com.android.compatibility.common.util.AdoptShellPermissionsRule
 import com.android.compatibility.common.util.DeviceConfigStateChangerRule
 import com.android.compatibility.common.util.SystemUtil
@@ -98,6 +104,54 @@ class AppFunctionMetadataTest {
     fun uninstallTestPackages() {
         uninstallPackage(TEST_APP_A_PKG)
         uninstallPackage(TEST_APP_B_PKG)
+    }
+
+    @Test
+    @IncludeRunOnSecondaryUser
+    @IncludeRunOnPrimaryUser
+    fun createAppFunctionMetadata() = doBlocking {
+        installPackage(TEST_APP_A_V2_PATH)
+        val packageName = TEST_APP_A_PKG
+        val functionId = "com.example.utils#print1"
+        val packageMetadata =
+            AppFunctionPackageMetadata.create(
+                TEST_APP_A_PKG,
+                listOf(
+                    GenericDocument.Builder<GenericDocument.Builder<*>>("", "", "")
+                        .setPropertyString("exampleProperty", "exampleValue")
+                        .build()
+                )
+            )
+
+        retryAssert {
+            val afStaticMetadataGd = queryAppFunctionStaticMetadata(functionId)
+            val afRuntimeMetadataGd =
+                queryAppFunctionRuntimeMetadata(packageName).single { it ->
+                    it.id == String.format("%s/%s", packageName, functionId)
+                }
+            val appFunctionMetadata =
+                AppFunctionMetadata.create(
+                    afStaticMetadataGd,
+                    afRuntimeMetadataGd,
+                    packageMetadata
+                )
+
+            assertThat(appFunctionMetadata.name)
+                .isEqualTo(AppFunctionName(packageName, functionId))
+            assertThat(appFunctionMetadata.getSchemaMetadata())
+                .isEqualTo(
+                    AppFunctionSchemaMetadata(
+                        "utils",
+                        "print",
+                        1L
+                    )
+                )
+            assertThat(appFunctionMetadata.isEnabled()).isFalse()
+            assertThat(appFunctionMetadata.getMetadataDocument())
+                .isEqualTo(afStaticMetadataGd)
+            assertThat(appFunctionMetadata.getPackageMetadata())
+                .isEqualTo(packageMetadata)
+        }
     }
 
     @Test
@@ -244,8 +298,25 @@ class AppFunctionMetadataTest {
             "device_config delete machine_learning allowlisted_app_functions_agents"
         )
     }
+    private fun queryAppFunctionStaticMetadata(functionId: String): GenericDocument {
+        val globalSearchSession: GlobalSearchSessionShim =
+            GlobalSearchSessionShimImpl.createGlobalSearchSessionAsync().get()
 
-    private fun queryAppFunctionInfos(packageName: String): List<AppFunctionInfo> {
+        val searchResults: SearchResultsShim =
+            globalSearchSession.search(
+                String.format("%s:\"%s\"", PROPERTY_FUNCTION_ID, functionId),
+                SearchSpec.Builder()
+                    .addFilterNamespaces(
+                        AppFunctionStaticMetadataHelper.APP_FUNCTION_STATIC_NAMESPACE
+                    )
+                    .addFilterSchemas(AppFunctionStaticMetadataHelper.STATIC_SCHEMA_TYPE)
+                    .setVerbatimSearchEnabled(true)
+                    .build(),
+            )
+        return collectAllSearchResults(searchResults).single()
+    }
+
+    private fun queryAppFunctionRuntimeMetadata(packageName: String): List<GenericDocument> {
         val globalSearchSession: GlobalSearchSessionShim =
             GlobalSearchSessionShimImpl.createGlobalSearchSessionAsync().get()
 
@@ -259,7 +330,11 @@ class AppFunctionMetadataTest {
                     .setVerbatimSearchEnabled(true)
                     .build(),
             )
-        return collectAllSearchResults(searchResults).map {
+        return collectAllSearchResults(searchResults)
+    }
+
+    private fun queryAppFunctionInfos(packageName: String): List<AppFunctionInfo> {
+        return queryAppFunctionRuntimeMetadata(packageName).map {
             AppFunctionInfo(
                 it.getPropertyString(PROPERTY_PACKAGE_NAME)!!,
                 it.getPropertyString(PROPERTY_FUNCTION_ID)!!,
@@ -273,8 +348,6 @@ class AppFunctionMetadataTest {
         @JvmField @ClassRule @Rule val sDeviceState: DeviceState = DeviceState()
 
         const val TEST_APP_ROOT_FOLDER: String = "/data/local/tmp/cts/appfunctions/"
-        const val TEST_APP_A_V1_PATH: String =
-            TEST_APP_ROOT_FOLDER + "CtsAppSearchIndexerTestAppAV1.apk"
         const val TEST_APP_A_V2_PATH: String =
             TEST_APP_ROOT_FOLDER + "CtsAppSearchIndexerTestAppAV2.apk"
         const val TEST_APP_A_V3_PATH: String =
