2b696024130c0e2c8d38a1b9c240a4998622aa4f
Hiroki Sato <hirokisato@google.com>
2025-10-31 21:19:33 +0900

    STS: Add test loading large input method
    This makes sure that
    * unreasonably large input method metadata does not affect the system.
    * large amount of input method subtype does not affect the system.
    
    Bug: 449416164
    Bug: 449181366
    Bug: 449393786
    Bug: 449227003
    Test: sts-tradefed run sts-dynamic-develop -m CtsSecurityTestCases -t "android.security.cts.BUG_449181366#testLoadLargeInputMethod"
          The above command fails without fixes, but passes with fixes
    Flag: EXEMPT BUGFIX
    Change-Id: I8a7ac1ff75b9a561c118feb0ac02a2ddae0574a5

---
+++ tests/tests/security/src/android/security/cts/BUG_449181366.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.security.cts;
+
+import static com.google.common.truth.TruthJUnit.assume;
+
+import static org.junit.Assert.fail;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.UserHandle;
+import android.platform.test.annotations.AsbSecurityTest;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.inputmethod.InputMethodInfo;
+import android.view.inputmethod.InputMethodManager;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.android.compatibility.common.util.PollingCheck;
+import com.android.compatibility.common.util.SystemUtil;
+import com.android.sts.common.util.StsExtraBusinessLogicTestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.time.Duration;
+
+@RunWith(AndroidJUnit4.class)
+public class BUG_449181366 extends StsExtraBusinessLogicTestCase {
+
+    // The APK contains multiple input method services:
+    // * "Method1" service (IME1), which must be loaded successfully, and is used to validate
+    //   input method subtype loading behavior.
+    // * "Method2" and later services, which are invalid input methods. They references a large
+    //   strings. They should be ignored in the framework.
+    private static final String LARGE_IME_APK_PATH =
+            "/data/local/tmp/cts/security/LargeInputMethodTestApp.apk";
+    private static final String LARGE_IME_PKG = "com.android.security.largeime";
+
+    private static final ComponentName IME1 =
+            ComponentName.createRelative(LARGE_IME_PKG, ".Method1");
+    private static final String ADDED_SUBTYPE_NAME = "Additional Type";
+
+    private static final Duration TIMEOUT = Duration.ofSeconds(10);
+
+    private final Context mContext = InstrumentationRegistry.getInstrumentation().getContext();
+    private String mInitiallyEnabledInputMethods;
+
+    @Before
+    public void setUp() {
+        mInitiallyEnabledInputMethods = getEnabledInputMethods();
+        installTestApp();
+    }
+
+    @After
+    public void tearDown() {
+        if (mInitiallyEnabledInputMethods != null) {
+            // The test service uses InputMethodManager#setExplicitlyEnabledInputMethodSubtypes.
+            // The API eventually updates ENABLED_INPUT_METHODS value. This makes sure the value
+            // is reset.
+            setEnabledInputMethods(mInitiallyEnabledInputMethods);
+        }
+
+        uninstallTestApp();
+    }
+
+    @Test
+    @AsbSecurityTest(cveBugId = {449181366, 449227003, 449393786, 449416164})
+    public void testLoadLargeInputMethod() {
+        final var imm = mContext.getSystemService(InputMethodManager.class);
+
+        try {
+            // Should be able to successfully loaded, and added to the list within
+            // the reasonable timeout.
+            PollingCheck.waitFor(
+                    TIMEOUT.toMillis(),
+                    () ->
+                            imm.getInputMethodList().stream()
+                                    .map(InputMethodInfo::getComponent)
+                                    .toList()
+                                    .contains(IME1),
+                    "InputMethod must be loaded");
+
+            enableAndSetIme(IME1.flattenToShortString());
+
+            // IME1 adds subtype dynamically onCreate(). The new subtypes should be
+            // added to the list within the reasonable timeout.
+            PollingCheck.waitFor(
+                    TIMEOUT.toMillis(),
+                    () -> imm.getCurrentInputMethodInfo().getComponent().equals(IME1),
+                    "InputMethod must be enabled");
+
+            PollingCheck.waitFor(
+                    TIMEOUT.toMillis(),
+                    () ->
+                            imm
+                                    .getEnabledInputMethodSubtypeList(
+                                            imm.getCurrentInputMethodInfo(),
+                                            /* allowsImplicitlyEnabledSubtypes= */ false)
+                                    .stream()
+                                    .anyMatch(
+                                            subtype ->
+                                                    TextUtils.equals(
+                                                            ADDED_SUBTYPE_NAME,
+                                                            subtype.getNameOverride())),
+                    "Valid subtype must be exposed as expected");
+        } catch (Exception e) {
+            if (e instanceof android.os.DeadSystemRuntimeException) {
+                fail("The device is vulnerable to b/449181366.");
+            }
+
+            // If the device is vulnerable, the system server crashes or too slow to respond.
+            // Catching other exceptions here normally means the test failed with some other reason.
+            // Following STS guideline, this should just be an assumption failure.
+            assume().that(e).isNull();
+        }
+    }
+
+    private static void installTestApp() {
+        SystemUtil.runShellCommand(
+                "pm install -r --user " + UserHandle.myUserId() + " " + LARGE_IME_APK_PATH);
+    }
+
+    private static void uninstallTestApp() {
+        SystemUtil.runShellCommand(
+                "pm uninstall --user " + UserHandle.myUserId() + " " + LARGE_IME_PKG);
+    }
+
+    private static String getEnabledInputMethods() {
+        return SystemUtil.runShellCommand(
+                "settings --user "
+                        + UserHandle.myUserId()
+                        + " get secure "
+                        + Settings.Secure.ENABLED_INPUT_METHODS);
+    }
+
+    private static void setEnabledInputMethods(String enabledInputMethods) {
+        SystemUtil.runShellCommand(
+                "settings --user "
+                        + UserHandle.myUserId()
+                        + " set secure "
+                        + Settings.Secure.ENABLED_INPUT_METHODS
+                        + " "
+                        + enabledInputMethods);
+    }
+
+    private static void enableAndSetIme(String imeId) {
+        SystemUtil.runShellCommand("ime enable --user " + UserHandle.myUserId() + " " + imeId);
+        SystemUtil.runShellCommand("ime set --user " + UserHandle.myUserId() + " " + imeId);
+    }
+}
