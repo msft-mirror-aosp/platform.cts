c5fb92023f107d449cc53e6051bd29a8591e5815
Artyom Chen <artyomchen@google.com>
2025-11-07 13:23:26 +0000

    Convert DevicePolicyManagerRoleHolder annotations to kotlin
    Convert EnsureHasDevicePolicyManagerRoleHolder* and
    IncludeRunOnDevicePolicyManagementRoleHolder* annotations to kotlin.
    
    Move related @AutoAnnotation methods from Policy.java to their
    respective annotation kotlin class file.
    
    Flag: EXEMPT TEST_ONLY
    Bug: None
    Test: verified that compiles
    Change-Id: I3071227bb48fec43b3d4aef3eedd55e6a7ef4c3f

---
+++ common/device-side/bedstead/modules/enterprise/src/main/kotlin/com/android/bedstead/enterprise/annotations/parameterized/IncludeRunOnDevicePolicyManagementRoleHolderSecondaryUser.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.bedstead.enterprise.annotations.parameterized
+
+import com.android.bedstead.enterprise.annotations.EnsureHasDevicePolicyManagerRoleHolder
+import com.android.bedstead.enterprise.annotations.EnsureHasNoDelegate
+import com.android.bedstead.enterprise.annotations.EnsureHasNoDpc
+import com.android.bedstead.harrier.UserType
+import com.android.bedstead.harrier.annotations.AnnotationPriorityRunPrecedence
+import com.android.bedstead.harrier.annotations.ParameterizedAnnotationScope
+import com.android.bedstead.harrier.annotations.meta.ParameterizedAnnotation
+import com.android.bedstead.multiuser.annotations.RequireRunOnSecondaryUser
+import com.google.auto.value.AutoAnnotation
+import kotlin.annotation.AnnotationRetention.RUNTIME
+import kotlin.annotation.AnnotationTarget.CLASS
+import kotlin.annotation.AnnotationTarget.FUNCTION
+import kotlin.annotation.AnnotationTarget.PROPERTY_GETTER
+import kotlin.annotation.AnnotationTarget.PROPERTY_SETTER
+
+/** Parameterize a test so that it runs on a secondary user of a device with the
+ * device policy management role holder set on the system user.  */
+@Target(FUNCTION, PROPERTY_GETTER, PROPERTY_SETTER, CLASS)
+@Retention(RUNTIME)
+@ParameterizedAnnotation(scope = ParameterizedAnnotationScope.ENTERPRISE)
+@RequireRunOnSecondaryUser
+@EnsureHasNoDpc(onUser = UserType.ANY)
+@EnsureHasDevicePolicyManagerRoleHolder(onUser = UserType.SYSTEM_USER, isPrimary = true)
+@EnsureHasNoDelegate
+annotation class IncludeRunOnDevicePolicyManagementRoleHolderSecondaryUser(
+    /**
+     * Priority sets the order that annotations will be resolved.
+     *
+     * Annotations with a lower priority will be resolved before annotations with a higher
+     * priority.
+     *
+     * If there is an order requirement between annotations, ensure that the priority of the
+     * annotation which must be resolved first is lower than the one which must be resolved later.
+     *
+     * Priority can be set to a [AnnotationPriorityRunPrecedence] constant, or to any [int].
+     */
+    val priority: Int = AnnotationPriorityRunPrecedence.EARLY
+)
+
+@AutoAnnotation
+fun includeRunOnDevicePolicyManagementRoleHolderSecondaryUser(): IncludeRunOnDevicePolicyManagementRoleHolderSecondaryUser {
+    return AutoAnnotation_IncludeRunOnDevicePolicyManagementRoleHolderSecondaryUserKt_includeRunOnDevicePolicyManagementRoleHolderSecondaryUser()
+}
