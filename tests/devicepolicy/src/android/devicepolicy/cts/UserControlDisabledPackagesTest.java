29e68bba51d369156415bef4bb29ef309a6e7839
Jimmy <jimmyxgong@chromium.org>
2025-11-12 23:54:41 +0000

    Ignore flaky DevicePolicyManager tests
    These tests are flaky and are not currently good candidates for
    presubmit. Ignore these tests and re-add them after stabilizing the
    tests.
    
    Bug: 458794474
    Bug: 460509405
    Test: N/A
    Flag: TEST_ONLY
    
    Change-Id: If58d9d4e239fb3bc0afb5eb665be577db9f6de78

--- tests/devicepolicy/src/android/devicepolicy/cts/UserControlDisabledPackagesTest.java
+++ tests/devicepolicy/src/android/devicepolicy/cts/UserControlDisabledPackagesTest.java
@@ -127,6 +127,7 @@ public final class UserControlDisabledPackagesTest {
 
     @CanSetPolicyTest(policy = UserControlDisabledPackages.class)
     @Postsubmit(reason = "b/181993922 automatically marked flaky")
+    @Ignore("b/460509405 flaky test")
     public void setUserControlDisabledPackages_toOneProtectedPackage() {
         dpc(sDeviceState).devicePolicyManager().setUserControlDisabledPackages(
                 dpc(sDeviceState).componentName(), List.of(PACKAGE_NAME));
@@ -158,6 +159,7 @@ public final class UserControlDisabledPackagesTest {
     @EnsureHasPermission(value = permission.FORCE_STOP_PACKAGES)
     @PolicyAppliesTest(policy = UserControlDisabledPackages.class)
     @Postsubmit(reason = "b/181993922 automatically marked flaky")
+    @Ignore("b/460509405 flaky test")
     public void setUserControlDisabledPackages_launchActivity_verifyPackageNotStopped()
             throws Exception {
         Assume.assumeTrue("Needs to launch activity",
@@ -185,6 +187,7 @@ public final class UserControlDisabledPackagesTest {
     @EnsureHasPermission(value = permission.FORCE_STOP_PACKAGES)
     @PolicyDoesNotApplyTest(policy = UserControlDisabledPackages.class)
     @Postsubmit(reason = "new test")
+    @Ignore("b/460509405 flaky test")
     public void setUserControlDisabledPackages_launchActivity_verifyPackageStopped()
             throws Exception {
         Assume.assumeTrue("Needs to launch activity",
