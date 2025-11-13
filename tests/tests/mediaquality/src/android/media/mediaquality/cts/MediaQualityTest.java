cf490b294fdadb229dbd73ff99ce66d4bb40242b
Haofan Wang <haofanw@google.com>
2025-11-13 14:45:55 -0800

    Add CTS for parameter capability
    Bug: 443329020
    Test: atest CtsMediaQualityTestCases
    Flag: android.media.tv.flags.media_quality_fw_c
    Change-Id: I921068b03243146eaeb39ed38cb97f5e6be82a5c

--- tests/tests/mediaquality/src/android/media/mediaquality/cts/MediaQualityTest.java
+++ tests/tests/mediaquality/src/android/media/mediaquality/cts/MediaQualityTest.java
@@ -897,12 +897,13 @@ public class MediaQualityTest {
         @Override
         public void onParameterCapabilitiesChanged(
                 String profileId, List<ParameterCapability> updatedCaps) {
-            boolean isSupported;
+            boolean isSupported, isMutable;
             int paramType;
             for (ParameterCapability paramCap : updatedCaps) {
                 assertNotNull("param cap name is null", paramCap.getParameterName());
                 assertNotNull("param cap is null", paramCap.getCapabilities());
                 isSupported = paramCap.isSupported();
+                isMutable = paramCap.isMutable();
                 paramType = paramCap.getParameterType();
             }
             super.onParameterCapabilitiesChanged(profileId, updatedCaps);
@@ -933,12 +934,13 @@ public class MediaQualityTest {
         @Override
         public void onParameterCapabilitiesChanged(
                 String profileId, List<ParameterCapability> updatedCaps) {
-            boolean isSupported;
+            boolean isSupported, isMutable;
             int paramType;
             for (ParameterCapability paramCap : updatedCaps) {
                 assertNotNull("param cap name is null", paramCap.getParameterName());
                 assertNotNull("param cap is null", paramCap.getCapabilities());
                 isSupported = paramCap.isSupported();
+                isMutable = paramCap.isMutable();
                 paramType = paramCap.getParameterType();
             }
             super.onParameterCapabilitiesChanged(profileId, updatedCaps);
