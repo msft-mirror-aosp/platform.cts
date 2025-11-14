7dd84906e03f4975f77a6737588f56ac84166479
Sorin Dinu <sorindinu@google.com>
2025-11-14 04:12:19 -0800

    add CTS tests for RecentViewHint and UserInputHint
    Bug: 434644900, 454055109
    Test: atest CtsPersonalContextTestCases:RecentViewHintTest && atest CtsPersonalContextTestCases:UserInputHintTest
    Flag: android.service.personalcontext.enable_personal_context_service
    Change-Id: I5c3686366499a867e9da7d7c95b13490cd3f8dcd

---
+++ tests/tests/personalcontext/src/android/service/personalcontext/cts/RecentViewHintTest.java
@@ -0,0 +1,247 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.service.personalcontext.cts;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.graphics.Rect;
+import android.platform.test.annotations.RequiresFlagsEnabled;
+import android.platform.test.flag.junit.CheckFlagsRule;
+import android.platform.test.flag.junit.DeviceFlagsValueProvider;
+import android.service.personalcontext.Flags;
+import android.service.personalcontext.hint.CapturedText;
+import android.service.personalcontext.hint.ContextHint;
+import android.service.personalcontext.hint.RecentViewHint;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.android.compatibility.common.util.ApiTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.time.Instant;
+
+/** Build/Install/Run: atest CtsPersonalContextTestCases:RecentViewHintTest */
+@RequiresFlagsEnabled(Flags.FLAG_ENABLE_PERSONAL_CONTEXT_SERVICE)
+@RunWith(AndroidJUnit4.class)
+public class RecentViewHintTest {
+    @Rule
+    public final CheckFlagsRule mCheckFlagsRule = DeviceFlagsValueProvider.createCheckFlagsRule();
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.RecentViewHint.Builder#build",
+                "android.service.personalcontext.hint.RecentViewHint#getCapturedTexts",
+                "android.service.personalcontext.hint.RecentViewHint#getLocusId",
+                "android.service.personalcontext.hint.ContextHint#createHintFromBundle",
+                "android.service.personalcontext.hint.RecentViewHint#toBundle"
+            })
+    @Test
+    public void testRecentViewHint_bundleUnbundle() {
+        final CapturedText capturedText =
+                new CapturedText.Builder()
+                        .setViewNodeText("text")
+                        .setViewNodeDescription("description")
+                        .setViewId("viewId")
+                        .setViewNodeBoundingBox(new Rect(1, 2, 3, 4))
+                        .setResourceId("resourceId")
+                        .setViewNodeLastUpdated(Instant.ofEpochMilli(12345L))
+                        .build();
+        final RecentViewHint hint =
+                new RecentViewHint.Builder()
+                        .addCapturedText(capturedText)
+                        .setLocusId("locusId")
+                        .build();
+
+        final ContextHint outputHint = bundleUnbundle(hint);
+        assertThat(outputHint).isInstanceOf(RecentViewHint.class);
+        final RecentViewHint outputRecentViewHint = (RecentViewHint) outputHint;
+        assertThat(outputRecentViewHint.getCapturedTexts()).containsExactly(capturedText);
+        assertThat(outputRecentViewHint.getLocusId()).isEqualTo("locusId");
+        assertThat(outputRecentViewHint.getCapturedTexts().get(0).getViewNodeLastSeen()).isNull();
+        assertThat(outputRecentViewHint).isEqualTo(hint);
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.CapturedText.Builder#setViewNodeText",
+                "android.service.personalcontext.hint.CapturedText.Builder#setViewNodeDescription",
+                "android.service.personalcontext.hint.CapturedText.Builder#setViewId",
+                "android.service.personalcontext.hint.CapturedText.Builder#setViewNodeBoundingBox",
+                "android.service.personalcontext.hint.CapturedText.Builder#setResourceId",
+                "android.service.personalcontext.hint.CapturedText.Builder#setViewNodeLastUpdated",
+                "android.service.personalcontext.hint.CapturedText.Builder#setViewNodeLastSeen",
+                "android.service.personalcontext.hint.CapturedText.Builder#build",
+                "android.service.personalcontext.hint.CapturedText#getViewNodeText",
+                "android.service.personalcontext.hint.CapturedText#getViewNodeDescription",
+                "android.service.personalcontext.hint.CapturedText#getViewId",
+                "android.service.personalcontext.hint.CapturedText#getViewNodeBoundingBox",
+                "android.service.personalcontext.hint.CapturedText#getResourceId",
+                "android.service.personalcontext.hint.CapturedText#getViewNodeLastUpdated",
+                "android.service.personalcontext.hint.CapturedText#getViewNodeLastSeen"
+            })
+    @Test
+    public void testCapturedText_getters() {
+        final Rect boundingBox = new Rect(1, 2, 3, 4);
+        final Instant lastSeen = Instant.ofEpochMilli(67890L);
+        final CapturedText capturedText =
+                new CapturedText.Builder()
+                        .setViewNodeText("text")
+                        .setViewNodeDescription("description")
+                        .setViewId("viewId")
+                        .setViewNodeBoundingBox(boundingBox)
+                        .setResourceId("resourceId")
+                        .setViewNodeLastUpdated(Instant.ofEpochMilli(12345L))
+                        .setViewNodeLastSeen(lastSeen)
+                        .build();
+
+        assertThat(capturedText.getViewNodeText()).isEqualTo("text");
+        assertThat(capturedText.getViewNodeDescription()).isEqualTo("description");
+        assertThat(capturedText.getViewId()).isEqualTo("viewId");
+        assertThat(capturedText.getViewNodeBoundingBox()).isEqualTo(boundingBox);
+        assertThat(capturedText.getResourceId()).isEqualTo("resourceId");
+        assertThat(capturedText.getViewNodeLastUpdated()).isEqualTo(Instant.ofEpochMilli(12345L));
+        assertThat(capturedText.getViewNodeLastSeen()).isEqualTo(lastSeen);
+
+        final CapturedText capturedText2 =
+                new CapturedText.Builder()
+                        .setViewNodeText("text")
+                        .setViewNodeDescription("description")
+                        .setViewId("viewId")
+                        .setViewNodeBoundingBox(boundingBox)
+                        .setResourceId("resourceId")
+                        .setViewNodeLastUpdated(Instant.ofEpochMilli(12345L))
+                        .build();
+        assertThat(capturedText2.getViewNodeLastSeen()).isNull();
+    }
+
+    private ContextHint bundleUnbundle(ContextHint hint) {
+        return ContextHint.createHintFromBundle(hint.toBundle());
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.RecentViewHint#equals",
+                "android.service.personalcontext.hint.RecentViewHint#hashCode",
+                "android.service.personalcontext.hint.RecentViewHint#toString"
+            })
+    @Test
+    public void testRecentViewHint_equalsHashCodeToString() {
+        final CapturedText capturedText =
+                new CapturedText.Builder()
+                        .setViewNodeText("text")
+                        .setViewNodeDescription("description")
+                        .setViewId("viewId")
+                        .setViewNodeBoundingBox(new Rect(1, 2, 3, 4))
+                        .setResourceId("resourceId")
+                        .setViewNodeLastUpdated(Instant.ofEpochMilli(12345L))
+                        .build();
+        final RecentViewHint hint =
+                new RecentViewHint.Builder()
+                        .addCapturedText(capturedText)
+                        .setLocusId("locusId")
+                        .build();
+
+        final RecentViewHint unbundledHint = (RecentViewHint) bundleUnbundle(hint);
+        assertThat(unbundledHint).isEqualTo(hint);
+        assertThat(unbundledHint.hashCode()).isEqualTo(hint.hashCode());
+        assertThat(hint.toString()).isNotNull();
+
+        final RecentViewHint differentHint =
+                new RecentViewHint.Builder()
+                        .addCapturedText(capturedText)
+                        .setLocusId("different locusId")
+                        .build();
+        assertThat(hint).isNotEqualTo(differentHint);
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.CapturedText#equals",
+                "android.service.personalcontext.hint.CapturedText#hashCode",
+                "android.service.personalcontext.hint.CapturedText#toString"
+            })
+    @Test
+    public void testCapturedText_equalsHashCodeToString() {
+        final Rect boundingBox = new Rect(1, 2, 3, 4);
+        final Instant lastSeen = Instant.ofEpochMilli(67890L);
+        final CapturedText capturedText1 =
+                new CapturedText.Builder()
+                        .setViewNodeText("text")
+                        .setViewNodeDescription("description")
+                        .setViewId("viewId")
+                        .setViewNodeBoundingBox(boundingBox)
+                        .setResourceId("resourceId")
+                        .setViewNodeLastUpdated(Instant.ofEpochMilli(12345L))
+                        .setViewNodeLastSeen(lastSeen)
+                        .build();
+
+        final CapturedText capturedText2 =
+                new CapturedText.Builder()
+                        .setViewNodeText("text")
+                        .setViewNodeDescription("description")
+                        .setViewId("viewId")
+                        .setViewNodeBoundingBox(boundingBox)
+                        .setResourceId("resourceId")
+                        .setViewNodeLastUpdated(Instant.ofEpochMilli(12345L))
+                        .setViewNodeLastSeen(lastSeen)
+                        .build();
+
+        final CapturedText differentText =
+                new CapturedText.Builder()
+                        .setViewNodeText("different text")
+                        .setViewNodeDescription("description")
+                        .setViewId("viewId")
+                        .setViewNodeBoundingBox(boundingBox)
+                        .setResourceId("resourceId")
+                        .setViewNodeLastUpdated(Instant.ofEpochMilli(12345L))
+                        .setViewNodeLastSeen(lastSeen)
+                        .build();
+
+        final CapturedText differentLastSeen =
+                new CapturedText.Builder()
+                        .setViewNodeText("text")
+                        .setViewNodeDescription("description")
+                        .setViewId("viewId")
+                        .setViewNodeBoundingBox(boundingBox)
+                        .setResourceId("resourceId")
+                        .setViewNodeLastUpdated(Instant.ofEpochMilli(12345L))
+                        .setViewNodeLastSeen(Instant.ofEpochMilli(1L))
+                        .build();
+
+        final CapturedText nullLastSeen =
+                new CapturedText.Builder()
+                        .setViewNodeText("text")
+                        .setViewNodeDescription("description")
+                        .setViewId("viewId")
+                        .setViewNodeBoundingBox(boundingBox)
+                        .setResourceId("resourceId")
+                        .setViewNodeLastUpdated(Instant.ofEpochMilli(12345L))
+                        .build();
+
+        assertThat(capturedText1).isEqualTo(capturedText2);
+        assertThat(capturedText1.hashCode()).isEqualTo(capturedText2.hashCode());
+        assertThat(capturedText1.toString()).isNotNull();
+
+        assertThat(capturedText1).isNotEqualTo(differentText);
+        assertThat(capturedText1).isNotEqualTo(differentLastSeen);
+        assertThat(capturedText1).isNotEqualTo(nullLastSeen);
+        assertThat(nullLastSeen).isNotEqualTo(capturedText1);
+    }
+}
---
+++ tests/tests/personalcontext/src/android/service/personalcontext/cts/UserInputHintTest.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.service.personalcontext.cts;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.graphics.Rect;
+import android.platform.test.annotations.RequiresFlagsEnabled;
+import android.platform.test.flag.junit.CheckFlagsRule;
+import android.platform.test.flag.junit.DeviceFlagsValueProvider;
+import android.service.personalcontext.Flags;
+import android.service.personalcontext.hint.ContextHint;
+import android.service.personalcontext.hint.UserInputHint;
+import android.service.personalcontext.hint.UserInputText;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.android.compatibility.common.util.ApiTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/** Build/Install/Run: atest CtsPersonalContextTestCases:UserInputHintTest */
+@RequiresFlagsEnabled(Flags.FLAG_ENABLE_PERSONAL_CONTEXT_SERVICE)
+@RunWith(AndroidJUnit4.class)
+public class UserInputHintTest {
+    @Rule
+    public final CheckFlagsRule mCheckFlagsRule = DeviceFlagsValueProvider.createCheckFlagsRule();
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.UserInputHint.Builder#build",
+                "android.service.personalcontext.hint.UserInputHint#getUserInputText",
+                "android.service.personalcontext.hint.ContextHint#createHintFromBundle",
+                "android.service.personalcontext.hint.UserInputHint#toBundle"
+            })
+    @Test
+    public void testUserInputHint_bundleUnbundle() {
+        final UserInputText userInputText =
+                new UserInputText.Builder()
+                        .setText("hello")
+                        .setViewNodeBoundingBox(new Rect(1, 2, 3, 4))
+                        .setFieldType(UserInputText.FIELD_TYPE_SEARCH_BOX)
+                        .setUserInputTextSource(UserInputText.USER_INPUT_TEXT_SOURCE_TYPED)
+                        .build();
+        final UserInputHint hint = new UserInputHint.Builder(userInputText).build();
+
+        final ContextHint outputHint = bundleUnbundle(hint);
+        assertThat(outputHint).isInstanceOf(UserInputHint.class);
+        final UserInputText outputUserInputText = ((UserInputHint) outputHint).getUserInputText();
+        assertThat(outputUserInputText).isEqualTo(userInputText);
+        assertThat(outputHint).isEqualTo(hint);
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.UserInputText.Builder#setText",
+                "android.service.personalcontext.hint.UserInputText.Builder#setViewNodeBoundingBox",
+                "android.service.personalcontext.hint.UserInputText.Builder#setFieldType",
+                "android.service.personalcontext.hint.UserInputText.Builder#setUserInputTextSource",
+                "android.service.personalcontext.hint.UserInputText.Builder#build",
+                "android.service.personalcontext.hint.UserInputText#getText",
+                "android.service.personalcontext.hint.UserInputText#getViewNodeBoundingBox",
+                "android.service.personalcontext.hint.UserInputText#getFieldType",
+                "android.service.personalcontext.hint.UserInputText#getUserInputTextSource"
+            })
+    @Test
+    public void testUserInputText_getters() {
+        final Rect boundingBox = new Rect(1, 2, 3, 4);
+        final UserInputText userInputText =
+                new UserInputText.Builder()
+                        .setText("hello")
+                        .setViewNodeBoundingBox(boundingBox)
+                        .setFieldType(UserInputText.FIELD_TYPE_SEARCH_BOX)
+                        .setUserInputTextSource(UserInputText.USER_INPUT_TEXT_SOURCE_TYPED)
+                        .build();
+
+        assertThat(userInputText.getText()).isEqualTo("hello");
+        assertThat(userInputText.getViewNodeBoundingBox()).isEqualTo(boundingBox);
+        assertThat(userInputText.getFieldType()).isEqualTo(UserInputText.FIELD_TYPE_SEARCH_BOX);
+        assertThat(userInputText.getUserInputTextSource())
+                .isEqualTo(UserInputText.USER_INPUT_TEXT_SOURCE_TYPED);
+    }
+
+    private ContextHint bundleUnbundle(ContextHint hint) {
+        return ContextHint.createHintFromBundle(hint.toBundle());
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.UserInputHint#equals",
+                "android.service.personalcontext.hint.UserInputHint#hashCode",
+                "android.service.personalcontext.hint.UserInputHint#toString"
+            })
+    @Test
+    public void testUserInputHint_equalsHashCodeToString() {
+        final UserInputText userInputText =
+                new UserInputText.Builder()
+                        .setText("hello")
+                        .setViewNodeBoundingBox(new Rect(1, 2, 3, 4))
+                        .setFieldType(UserInputText.FIELD_TYPE_SEARCH_BOX)
+                        .setUserInputTextSource(UserInputText.USER_INPUT_TEXT_SOURCE_TYPED)
+                        .build();
+        final UserInputHint hint = new UserInputHint.Builder(userInputText).build();
+
+        final UserInputHint unbundledHint = (UserInputHint) bundleUnbundle(hint);
+        assertThat(unbundledHint).isEqualTo(hint);
+        assertThat(unbundledHint.hashCode()).isEqualTo(hint.hashCode());
+        assertThat(hint.toString()).isNotNull();
+
+        final UserInputText differentUserInputText =
+                new UserInputText.Builder()
+                        .setText("different")
+                        .setViewNodeBoundingBox(new Rect(1, 2, 3, 4))
+                        .setFieldType(UserInputText.FIELD_TYPE_SEARCH_BOX)
+                        .setUserInputTextSource(UserInputText.USER_INPUT_TEXT_SOURCE_TYPED)
+                        .build();
+        final UserInputHint differentHint =
+                new UserInputHint.Builder(differentUserInputText).build();
+        assertThat(hint).isNotEqualTo(differentHint);
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.UserInputText#equals",
+                "android.service.personalcontext.hint.UserInputText#hashCode",
+                "android.service.personalcontext.hint.UserInputText#toString"
+            })
+    @Test
+    public void testUserInputText_equalsHashCodeToString() {
+        final Rect boundingBox = new Rect(1, 2, 3, 4);
+        final UserInputText userInputText1 =
+                new UserInputText.Builder()
+                        .setText("hello")
+                        .setViewNodeBoundingBox(boundingBox)
+                        .setFieldType(UserInputText.FIELD_TYPE_SEARCH_BOX)
+                        .setUserInputTextSource(UserInputText.USER_INPUT_TEXT_SOURCE_TYPED)
+                        .build();
+
+        final UserInputText userInputText2 =
+                new UserInputText.Builder()
+                        .setText("hello")
+                        .setViewNodeBoundingBox(boundingBox)
+                        .setFieldType(UserInputText.FIELD_TYPE_SEARCH_BOX)
+                        .setUserInputTextSource(UserInputText.USER_INPUT_TEXT_SOURCE_TYPED)
+                        .build();
+
+        final UserInputText differentText =
+                new UserInputText.Builder()
+                        .setText("different")
+                        .setViewNodeBoundingBox(boundingBox)
+                        .setFieldType(UserInputText.FIELD_TYPE_SEARCH_BOX)
+                        .setUserInputTextSource(UserInputText.USER_INPUT_TEXT_SOURCE_TYPED)
+                        .build();
+
+        assertThat(userInputText1).isEqualTo(userInputText2);
+        assertThat(userInputText1.hashCode()).isEqualTo(userInputText2.hashCode());
+        assertThat(userInputText1.toString()).isNotNull();
+        assertThat(userInputText1).isNotEqualTo(differentText);
+    }
+}
