7754acc75c42ae2d0ba23e4f32e7863a87dbc7ea
Naval Dubey <navaldubey@google.com>
2025-11-20 11:46:14 +0000

    Skip reboot modem tests on IRadio <= 2.4
    Telephony Manager rebootModem is not enforced until 2.4 now.
    Bug: 446048326
    Test: atest CtsTelephonyTestCases
    
    Change-Id: I23b45c054eb693832e031ce2c5dc8c2da0bb5f01

--- tests/tests/telephony/current/src/android/telephony/cts/TelephonyManagerTest.java
+++ tests/tests/telephony/current/src/android/telephony/cts/TelephonyManagerTest.java
@@ -328,6 +328,7 @@ public class TelephonyManagerTest {
     private static final int RADIO_HAL_VERSION_2_1 = makeRadioVersion(2, 1);
     private static final int RADIO_HAL_VERSION_2_2 = makeRadioVersion(2, 2);
     private static final int RADIO_HAL_VERSION_2_3 = makeRadioVersion(2, 3);
+    private static final int RADIO_HAL_VERSION_2_4 = makeRadioVersion(2, 4);
 
     static {
         EMERGENCY_NUMBER_SOURCE_SET = new HashSet<Integer>();
@@ -2230,8 +2231,8 @@ public class TelephonyManagerTest {
     public void testRebootModem() throws Throwable {
         assumeTrue(hasFeature(PackageManager.FEATURE_TELEPHONY_RADIO_ACCESS));
         assumeTrue(
-                "Skipping test since rebootModem is not supported/enforced until IRadio 2.3.",
-                mModemHalVersion >= RADIO_HAL_VERSION_2_3);
+                "Skipping test since rebootModem is not supported/enforced up to IRadio 2.4.",
+                mModemHalVersion > RADIO_HAL_VERSION_2_4);
 
         RadioCallback rc = new RadioCallback();
 
