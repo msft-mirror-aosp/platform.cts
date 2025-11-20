aa8a53ea695bc916027dd9556200dc7a83c3c3c2
Brayden Hass <braydenhass@google.com>
2025-11-20 10:05:27 -0800

    Add tests for API changes and mock modem implementation.
    Change-Id: I48c901d46fec93d38c8703cbff9a0bc07e857c52
    Bug: 411183432
    Flag: com.android.internal.telephony.flags.network_security_event_indications
    Test: CellularSecurityCallbackTest TelephonyManagerTest

--- tests/tests/telephony/current/src/android/telephony/cts/TelephonyManagerTest.java
+++ tests/tests/telephony/current/src/android/telephony/cts/TelephonyManagerTest.java
@@ -328,6 +328,7 @@ public class TelephonyManagerTest {
     private static final int RADIO_HAL_VERSION_2_1 = makeRadioVersion(2, 1);
     private static final int RADIO_HAL_VERSION_2_2 = makeRadioVersion(2, 2);
     private static final int RADIO_HAL_VERSION_2_3 = makeRadioVersion(2, 3);
+    private static final int RADIO_HAL_VERSION_2_4 = makeRadioVersion(2, 4);
 
     static {
         EMERGENCY_NUMBER_SOURCE_SET = new HashSet<Integer>();
@@ -6767,6 +6768,48 @@ public class TelephonyManagerTest {
         } catch (UnsupportedOperationException e) {
             Log.d(TAG, "Skipping test since modem does not support optional IRadioNetwork APIs");
         }
+    }
+
+    @Test
+    @ApiTest(
+            apis = {
+                "android.telephony.TelephonyManager"
+                        + "#getSupportedNetworkAlertCategories",
+            })
+    @RequiresFlagsEnabled(Flags.FLAG_NETWORK_SECURITY_EVENT_INDICATIONS)
+    public void testGetSupportedNetworkAlertCategoriesPermissions() {
+        assumeTrue(hasFeature(PackageManager.FEATURE_TELEPHONY_RADIO_ACCESS));
+        if (mNetworkHalVersion < RADIO_HAL_VERSION_2_4) {
+            Log.d(TAG,
+                    "Skipping test since modem does not support IRadioNetwork HAL v2.4");
+            return;
+        }
+
+        assertThrows(
+                "getSupportedNetworkAlertCategories",
+                SecurityException.class,
+                () -> mTelephonyManager.getSupportedNetworkAlertCategories());
+    }
+
+    @Test
+    @ApiTest(
+            apis = {
+                "android.telephony.TelephonyManager"
+                        + "#getSupportedNetworkAlertCategories",
+            })
+    @RequiresFlagsEnabled(Flags.FLAG_NETWORK_SECURITY_EVENT_INDICATIONS)
+    public void testGetSupportedNetworkAlertCategories() {
+        assumeTrue(hasFeature(PackageManager.FEATURE_TELEPHONY_RADIO_ACCESS));
+        if (mNetworkHalVersion < RADIO_HAL_VERSION_2_4) {
+            Log.d(TAG,
+                    "Skipping test since modem does not support IRadioNetwork HAL v2.4");
+            return;
+        }
+
+        int[] supportedCategories =
+        ShellIdentityUtils.invokeMethodWithShellPermissions(mTelephonyManager,
+                (tm) -> tm.getSupportedNetworkAlertCategories());
+        assertNotNull("Supported categories is null", supportedCategories);
     }
 
     @Test
