5f21cc9cac198cb08ef75b078cc1a59d9e3f0b02
Ludovic Barman <ludovicb@google.com>
2025-11-25 01:19:08 -0800

    Fix test given new PersistableBundle capability
    In ag/36835652, PersistableBundle gets the ability to get/put byte
    arrays. In this test, reading the `invalid` byte array will now succeed
    (nothing is invalid except the name).
    
    Looking at the CVE and b/247513680, this seems fine; the root cause of
    the CVE was a crash when parsing the bundle containing a (then
    unsupported) byte array. This is now supported and does not cause an
    exception.
    
    Bug: 449713775
    Bug: 437280042
    Bug: 462298510
    Flag: android.app.privatecompute.flags.enable_pcc_framework_support
    Change-Id: I64d4c6106bdffe7b9d737efeb9f000935c9b1c1f

--- tests/tests/security/src/android/security/cts/PersistableBundleTest.java
+++ tests/tests/security/src/android/security/cts/PersistableBundleTest.java
@@ -18,9 +18,13 @@ package android.security.cts;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import android.app.privatecompute.flags.Flags;
 import android.os.PersistableBundle;
 import android.platform.test.annotations.AsbSecurityTest;
+import android.platform.test.annotations.RequiresFlagsDisabled;
+import android.platform.test.annotations.RequiresFlagsEnabled;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -36,6 +40,7 @@ public class PersistableBundleTest extends StsExtraBusinessLogicTestCase {
 
     @AsbSecurityTest(cveBugId = 247513680)
     @Test
+    @RequiresFlagsDisabled(Flags.FLAG_ENABLE_PCC_FRAMEWORK_SUPPORT)
     public void testReadFromStream_invalidType() throws Exception {
         String input = "<bundle><string name=\"key\">value</string>"
                 + "<byte-array name=\"invalid\" num=\"2\">ffff</byte-array></bundle>";
@@ -46,6 +51,28 @@ public class PersistableBundleTest extends StsExtraBusinessLogicTestCase {
 
         // verify invalid type is ignored
         assertFalse(restoredBundle.containsKey("invalid"));
+        // verify valid type exists
+        assertEquals("value", restoredBundle.getString("key"));
+    }
+
+    // 20/11/2025 update: byte array is now a valid type. Instead of being filtered out while
+    // parsing, it should exist in the PersistableBundle. It should not throw an exception
+    // when accessed.
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_ENABLE_PCC_FRAMEWORK_SUPPORT)
+    public void testReadFromStream_invalidTypeIsNowValid() throws Exception {
+        String input = "<bundle><string name=\"key\">value</string>"
+                + "<byte-array name=\"valid\" num=\"2\">abcd</byte-array></bundle>";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+
+        // Reading from the stream with invalid type should not throw an exception
+        PersistableBundle restoredBundle = PersistableBundle.readFromStream(inputStream);
+
+        assertTrue(restoredBundle.containsKey("valid"));
+        byte[] validByteArray = restoredBundle.getByteArray("valid");
+        assertEquals(2, validByteArray.length);
+        // We don't want to tie this test to the internal XML representation of the byte array,
+        // so we won't check the contents.
         // verify valid type exists
         assertEquals("value", restoredBundle.getString("key"));
     }
