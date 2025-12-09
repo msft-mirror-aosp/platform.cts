7f335e637e2c15e7f0c43b66daf98e49906c9221
Atneya Nair <atneya@google.com>
2025-12-09 15:59:44 -0800

    audio: Add volume hardening tests
    Test: atest AudioControlForegroundTests
    Test: I21400010529314973
    Flag: EXEMPT TEST_ONLY
    Bug: 371707137
    Change-Id: I400bfc04935304e0dd1aad6317081940e51f3969

--- tests/tests/media/audio/modules/audiorecordpermissiontests/src/android/media/audio/cts/audiopermissiontests/AudioControlForegroundTests.kt
+++ tests/tests/media/audio/modules/audiorecordpermissiontests/src/android/media/audio/cts/audiopermissiontests/AudioControlForegroundTests.kt
@@ -21,25 +21,39 @@ import android.app.AppOpsManager
 import android.app.AppOpsManager.MODE_IGNORED
 import android.content.Intent
 import android.media.AudioManager
+import android.media.AudioManager.ACTION_VOLUME_CHANGED
 import android.media.AudioManager.AUDIOFOCUS_REQUEST_FAILED
 import android.media.AudioManager.AUDIOFOCUS_REQUEST_GRANTED
 import android.media.audio.cts.audiopermissiontests.common.*
+import android.media.AudioManager.EXTRA_VOLUME_STREAM_TYPE
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.media.AudioManager.STREAM_MUSIC
 import android.platform.test.annotations.AppModeFull
+import com.android.compatibility.common.util.ApiTest
+import org.junit.Assume.assumeTrue
 import android.platform.test.annotations.RequiresFlagsEnabled
 import android.platform.test.flag.junit.DeviceFlagsValueProvider
 import android.provider.Settings
+
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+
 import com.android.compatibility.common.util.AdoptShellPermissionsRule
 import com.android.compatibility.common.util.SystemUtil
 import com.android.media.audio.Flags
 import com.android.media.mediatestutils.TestUtils.getFutureForIntent
+
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.ClassRule
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -91,6 +105,21 @@ class AudioControlForegroundTests {
     fun setup() {
         sPermRule.checkSetup()
         mTestUid = mContext.getPackageManager().getPackageUid(TEST_PACKAGE, /* flags= */ 0)
+
+        val pm = mContext.getPackageManager()
+        val isAutomotive = pm.hasSystemFeature(PackageManager.FEATURE_AUTOMOTIVE)
+        val isTelevision = pm.hasSystemFeature(PackageManager.FEATURE_LEANBACK) ||
+                pm.hasSystemFeature(PackageManager.FEATURE_TELEVISION)
+        val isFixedVolume = mContext.getResources().getBoolean(
+            Resources.getSystem().getIdentifier("config_useFixedVolume", "bool", "android")
+        )
+        val isSingleVolume = mContext.getResources().getBoolean(
+            Resources.getSystem().getIdentifier("config_single_volume", "bool", "android")
+        )
+        assumeTrue("Not supported on automotive", !isAutomotive)
+        assumeTrue("Not supported on television", !isTelevision)
+        assumeTrue("Not supported on fixed volume", !isFixedVolume)
+        assumeTrue("Not supported on single volume", !isSingleVolume)
     }
 
     @After
@@ -100,6 +129,7 @@ class AudioControlForegroundTests {
     }
 
     @Test
+    @ApiTest(apis = ["android.media.AudioManager#requestAudioFocus"])
     @RequiresFlagsEnabled(Flags.FLAG_HARDENING_PARTIAL)
     fun testRequestFocusInBackground_isDenied() {
         startService(false)
@@ -112,6 +142,7 @@ class AudioControlForegroundTests {
     }
 
     @Test
+    @ApiTest(apis = ["android.media.AudioManager#requestAudioFocus"])
     @RequiresFlagsEnabled(Flags.FLAG_HARDENING_PARTIAL)
     fun testRequestFocusInTop_isAllowed() {
         startActivity()
@@ -123,6 +154,7 @@ class AudioControlForegroundTests {
     }
 
     @Test
+    @ApiTest(apis = ["android.media.AudioManager#requestAudioFocus"])
     @RequiresFlagsEnabled(Flags.FLAG_HARDENING_PARTIAL)
     fun testRequestFocusInFgs_isAllowed() {
         startService(true)
@@ -132,6 +164,90 @@ class AudioControlForegroundTests {
         val intent = future.get(FUTURE_WAIT_SECS, TimeUnit.SECONDS)
         assertEquals(intent.getIntExtra(EXTRA_FOCUS_RESULT, -1), AUDIOFOCUS_REQUEST_GRANTED)
     }
+
+    @Test
+    @ApiTest(apis = ["android.media.AudioManager#setStreamVolume"])
+    @RequiresFlagsEnabled(Flags.FLAG_HARDENING_PARTIAL_VOLUME)
+    fun testAdjustVolumeInBackground_isDenied() {
+        startService(false)
+        waitForOpState(OPSTR_CONTROL_AUDIO, MODE_IGNORED)
+
+        val future = getFutureForIntent(mContext, AudioManager.ACTION_VOLUME_CHANGED) {
+            it != null && it.getIntExtra(AudioManager.EXTRA_VOLUME_STREAM_TYPE, -1) == STREAM_MUSIC
+        }
+
+        val mediaVol = triggerVolumeChange()
+
+        try {
+            assertThrows(TimeoutException::class.java) {
+                future.get(FALSE_NEG_SECS, TimeUnit.SECONDS)
+            }
+        } finally {
+            if (future.isDone()) {
+                val cleanupFuture = getFutureForIntent(mContext, ACTION_VOLUME_CHANGED) {
+                    it != null && it.getIntExtra(EXTRA_VOLUME_STREAM_TYPE, -1) == STREAM_MUSIC
+                }
+                mAudioManager.setStreamVolume(STREAM_MUSIC, mediaVol, 0)
+                cleanupFuture.get(FUTURE_WAIT_SECS, TimeUnit.SECONDS)
+            } else {
+                future.cancel(false);
+            }
+        }
+    }
+
+    @Test
+    @ApiTest(apis = ["android.media.AudioManager#setStreamVolume"])
+    fun testAdjustVolumeInFgs_isAllowed() {
+        startService(true)
+        Thread.sleep(FALSE_NEG_SECS * 1000)
+
+        val future = getFutureForIntent(mContext, ACTION_VOLUME_CHANGED) {
+            it != null && it.getIntExtra(EXTRA_VOLUME_STREAM_TYPE, -1) == STREAM_MUSIC
+        }
+
+        val mediaVol = triggerVolumeChange()
+
+        try {
+            future.get(FUTURE_WAIT_SECS, TimeUnit.SECONDS)
+        } finally {
+            if (future.isDone()) {
+                val cleanupFuture = getFutureForIntent(mContext, ACTION_VOLUME_CHANGED) {
+                    it != null && it.getIntExtra(EXTRA_VOLUME_STREAM_TYPE, -1) == STREAM_MUSIC
+                }
+                mAudioManager.setStreamVolume(STREAM_MUSIC, mediaVol, 0)
+                cleanupFuture.get(FUTURE_WAIT_SECS, TimeUnit.SECONDS)
+            } else {
+                future.cancel(false);
+            }
+        }
+    }
+
+    @Test
+    @ApiTest(apis = ["android.media.AudioManager#setStreamVolume"])
+    fun testAdjustVolumeInTop_isAllowed() {
+        startActivity()
+
+        val future = getFutureForIntent(mContext, ACTION_VOLUME_CHANGED) {
+            it != null && it.getIntExtra(EXTRA_VOLUME_STREAM_TYPE, -1) == STREAM_MUSIC
+        }
+
+        val mediaVol = triggerVolumeChange()
+
+        try {
+            future.get(FUTURE_WAIT_SECS, TimeUnit.SECONDS)
+        } finally {
+            if (future.isDone()) {
+                val cleanupFuture = getFutureForIntent(mContext, ACTION_VOLUME_CHANGED) {
+                    it != null && it.getIntExtra(EXTRA_VOLUME_STREAM_TYPE, -1) == STREAM_MUSIC
+                }
+                mAudioManager.setStreamVolume(STREAM_MUSIC, mediaVol, 0)
+                cleanupFuture.get(FUTURE_WAIT_SECS, TimeUnit.SECONDS)
+            } else {
+                future.cancel(false);
+            }
+        }
+    }
+
 
     fun sendCommand(action: String, intentF: (Intent) -> Unit = {}) =
         mContext.startService(getIntentForAction(TEST_PACKAGE, action).also(intentF))
@@ -211,4 +327,17 @@ class AudioControlForegroundTests {
 
     private fun unfreezePackage(packageName: String) =
         SystemUtil.runShellCommand(mInstrumentation, "am unfreeze --sticky $packageName")
+
+    private fun triggerVolumeChange(): Int {
+        val mediaVol = mAudioManager.getStreamVolume(STREAM_MUSIC);
+        val changeVol = if (mediaVol == mAudioManager.getStreamMaxVolume(STREAM_MUSIC)) {
+            mediaVol - 1
+        } else {
+            mediaVol + 1
+        }
+        sendCommand(ACTION_SET_MEDIA_VOLUME) {
+            it.putExtra(EXTRA_VOLUME_INDEX, changeVol)
+        }
+        return mediaVol
+    }
 }
