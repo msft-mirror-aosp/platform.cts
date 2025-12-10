e22076a0623fe90123f60afd7120adb722d2b2ef
Kazuki Takise <takise@google.com>
2025-12-10 11:26:31 +0900

    Flag removal: Remove enable_display_compat_mode
    Flag: EXEMPT FLAG_REMOVAL
    Bug: 467403975
    Test: CompatChangeTests
    Change-Id: I6cfa23cc3fd5468d29d340fe0eca83973505958f

--- tests/framework/base/windowmanager/src/android/server/wm/display/CompatChangeTests.java
+++ tests/framework/base/windowmanager/src/android/server/wm/display/CompatChangeTests.java
@@ -1178,7 +1178,6 @@ public final class CompatChangeTests extends MultiDisplayTestBase {
 
     /** Test that a DCM app does not relaunch with display move. */
     @Test
-    @RequiresFlagsEnabled(Flags.FLAG_ENABLE_DISPLAY_COMPAT_MODE)
     public void testDisplayCompatGameDoesNotRestartWithDisplayMove() {
         runDisplayCompatTest(NO_DISPLAY_CONFIG_CHANGE_SUPPORT_GAME_ACTIVITY);
     }
