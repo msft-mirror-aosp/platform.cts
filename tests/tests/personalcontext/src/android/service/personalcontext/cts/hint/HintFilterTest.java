1f175708218f92d8b248de50dc5a1553973d1737
hounshell <hounshell@google.com>
2026-01-07 20:01:55 +0000

    CTS fixes for add type names to BundleHint/Insight and fixing filters
    Bug: 473883862
    Bug: 434644900
    Bug: 460291894
    Test: I81000010499000179
    Test: atest CtsPersonalContextTestCases
    Flag: android.service.personalcontext.enable_personal_context_service
    Change-Id: I58f6701c4a5597e747f4047452c8f8e56a6a6964

---
+++ tests/tests/personalcontext/src/android/service/personalcontext/cts/hint/HintFilterTest.java
@@ -0,0 +1,237 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.service.personalcontext.cts.hint;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.platform.test.annotations.RequiresFlagsEnabled;
+import android.platform.test.flag.junit.CheckFlagsRule;
+import android.platform.test.flag.junit.DeviceFlagsValueProvider;
+import android.service.personalcontext.Flags;
+import android.service.personalcontext.hint.BundleHint;
+import android.service.personalcontext.hint.ContextHintWithSignature;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.android.compatibility.common.util.ApiTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Set;
+
+import javax.crypto.spec.SecretKeySpec;
+
+/** Build/Install/Run: atest CtsPersonalContextTestCases:HintFilterTest */
+@RequiresFlagsEnabled(Flags.FLAG_ENABLE_PERSONAL_CONTEXT_SERVICE)
+@RunWith(AndroidJUnit4.class)
+public class HintFilterTest {
+    private static final String HINT_CLASS_A =
+            "android.service.personalcontext.hint.HintFilterTest.A";
+    private static final String HINT_CLASS_B =
+            "android.service.personalcontext.hint.HintFilterTest.B";
+    private static final String HINT_CLASS_C =
+            "android.service.personalcontext.hint.HintFilterTest.C";
+    private static final String HINT_CLASS_D =
+            "android.service.personalcontext.hint.HintFilterTest.D";
+    private static final String HINT_CLASS_E =
+            "android.service.personalcontext.hint.HintFilterTest.E";
+
+    @Rule
+    public final CheckFlagsRule checkFlagsRule = DeviceFlagsValueProvider.createCheckFlagsRule();
+
+    private static ContextHintWithSignature makeHint(String hintClass)
+            throws GeneralSecurityException {
+        final byte[] key = new byte[64];
+        new Random().nextBytes(key);
+
+        return new ContextHintWithSignature.Builder(
+                        new BundleHint.Builder().setHintTypeName(hintClass).build(),
+                        new SecretKeySpec(key, ContextHintWithSignature.HMAC_ALGORITHM))
+                .build();
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.HintFilter#getInterestedHintClusters",
+                "android.service.personalcontext.hint.HintFilter.Builder#addHintType",
+                "android.service.personalcontext.hint.HintFilter.Builder#build",
+            })
+    @Test
+    public void testHintFilterRequireAll() throws GeneralSecurityException {
+        ContextHintWithSignature hintA = makeHint(HINT_CLASS_A);
+        ContextHintWithSignature hintB = makeHint(HINT_CLASS_B);
+        ContextHintWithSignature hintC = makeHint(HINT_CLASS_C);
+
+        final Set<ContextHintWithSignature> interestedHintSet =
+                new android.service.personalcontext.hint.HintFilter.Builder()
+                        .addHintType(HINT_CLASS_A, true)
+                        .addHintType(HINT_CLASS_B, true)
+                        .addHintType(HINT_CLASS_C, true)
+                        .build()
+                        .getInterestedHintClusters(
+                                Set.of(hintA, hintB, hintC), Collections.emptySet());
+
+        assertThat(interestedHintSet).containsExactly(hintA, hintB, hintC);
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.HintFilter#getInterestedHintClusters",
+                "android.service.personalcontext.hint.HintFilter.Builder#addHintType",
+                "android.service.personalcontext.hint.HintFilter.Builder#build",
+            })
+    @Test
+    public void testHintFilterRequireSome() throws GeneralSecurityException {
+        ContextHintWithSignature hintA = makeHint(HINT_CLASS_A);
+        ContextHintWithSignature hintB = makeHint(HINT_CLASS_B);
+        ContextHintWithSignature hintC = makeHint(HINT_CLASS_C);
+
+        final Set<ContextHintWithSignature> interestedHintSet =
+                new android.service.personalcontext.hint.HintFilter.Builder()
+                        .addHintType(HINT_CLASS_A, true)
+                        .addHintType(HINT_CLASS_B, true)
+                        .build()
+                        .getInterestedHintClusters(
+                                Set.of(hintA, hintB, hintC), Collections.emptySet());
+
+        assertThat(interestedHintSet).containsExactly(hintA, hintB);
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.HintFilter#getInterestedHintClusters",
+                "android.service.personalcontext.hint.HintFilter.Builder#addHintType",
+                "android.service.personalcontext.hint.HintFilter.Builder#build",
+            })
+    @Test
+    public void testHintFilterRequireMissingSome() throws GeneralSecurityException {
+        ContextHintWithSignature hintA = makeHint(HINT_CLASS_A);
+        ContextHintWithSignature hintB = makeHint(HINT_CLASS_B);
+        ContextHintWithSignature hintC = makeHint(HINT_CLASS_C);
+
+        final Set<ContextHintWithSignature> interestedHintSet =
+                new android.service.personalcontext.hint.HintFilter.Builder()
+                        .addHintType(HINT_CLASS_A, true)
+                        .addHintType(HINT_CLASS_B, true)
+                        .addHintType(HINT_CLASS_C, true)
+                        .addHintType(HINT_CLASS_D, true)
+                        .addHintType(HINT_CLASS_E, true)
+                        .build()
+                        .getInterestedHintClusters(
+                                Set.of(hintA, hintB, hintC), Collections.emptySet());
+
+        assertThat(interestedHintSet).isEmpty();
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.HintFilter#getInterestedHintClusters",
+                "android.service.personalcontext.hint.HintFilter.Builder#addHintType",
+                "android.service.personalcontext.hint.HintFilter.Builder#build",
+            })
+    @Test
+    public void testHintFilterRequireNone() throws GeneralSecurityException {
+        ContextHintWithSignature hintA = makeHint(HINT_CLASS_A);
+        ContextHintWithSignature hintB = makeHint(HINT_CLASS_B);
+        ContextHintWithSignature hintC = makeHint(HINT_CLASS_C);
+
+        final Set<ContextHintWithSignature> interestedHintSet =
+                new android.service.personalcontext.hint.HintFilter.Builder()
+                        .addHintType(HINT_CLASS_D, true)
+                        .addHintType(HINT_CLASS_E, true)
+                        .build()
+                        .getInterestedHintClusters(
+                                Set.of(hintA, hintB, hintC), Collections.emptySet());
+
+        assertThat(interestedHintSet).isEmpty();
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.HintFilter#getInterestedHintClusters",
+                "android.service.personalcontext.hint.HintFilter.Builder#addHintType",
+                "android.service.personalcontext.hint.HintFilter.Builder#build",
+            })
+    @Test
+    public void testHintFilterAllowOne() throws GeneralSecurityException {
+        ContextHintWithSignature hintA = makeHint(HINT_CLASS_A);
+        ContextHintWithSignature hintB = makeHint(HINT_CLASS_B);
+        ContextHintWithSignature hintC = makeHint(HINT_CLASS_C);
+
+        final Set<ContextHintWithSignature> interestedHintSet =
+                new android.service.personalcontext.hint.HintFilter.Builder()
+                        .addHintType(HINT_CLASS_A, false)
+                        .addHintType(HINT_CLASS_D, false)
+                        .build()
+                        .getInterestedHintClusters(
+                                Set.of(hintA, hintB, hintC), Collections.emptySet());
+
+        assertThat(interestedHintSet).containsExactly(hintA);
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.HintFilter#getInterestedHintClusters",
+                "android.service.personalcontext.hint.HintFilter.Builder#addHintType",
+                "android.service.personalcontext.hint.HintFilter.Builder#build",
+            })
+    @Test
+    public void testHintFilterAllowMany() throws GeneralSecurityException {
+        ContextHintWithSignature hintA = makeHint(HINT_CLASS_A);
+        ContextHintWithSignature hintB = makeHint(HINT_CLASS_B);
+        ContextHintWithSignature hintC = makeHint(HINT_CLASS_C);
+
+        final Set<ContextHintWithSignature> interestedHintSet =
+                new android.service.personalcontext.hint.HintFilter.Builder()
+                        .addHintType(HINT_CLASS_A, false)
+                        .addHintType(HINT_CLASS_B, false)
+                        .addHintType(HINT_CLASS_C, false)
+                        .build()
+                        .getInterestedHintClusters(
+                                Set.of(hintA, hintB, hintC), Collections.emptySet());
+
+        assertThat(interestedHintSet).containsExactly(hintA, hintB, hintC);
+    }
+
+    @ApiTest(
+            apis = {
+                "android.service.personalcontext.hint.HintFilter#getInterestedHintClusters",
+                "android.service.personalcontext.hint.HintFilter.Builder#addHintType",
+                "android.service.personalcontext.hint.HintFilter.Builder#build",
+            })
+    @Test
+    public void testHintFilterAllowSome() throws GeneralSecurityException {
+        ContextHintWithSignature hintA = makeHint(HINT_CLASS_A);
+        ContextHintWithSignature hintB = makeHint(HINT_CLASS_B);
+        ContextHintWithSignature hintC = makeHint(HINT_CLASS_C);
+
+        final Set<ContextHintWithSignature> interestedHintSet =
+                new android.service.personalcontext.hint.HintFilter.Builder()
+                        .addHintType(HINT_CLASS_A, false)
+                        .addHintType(HINT_CLASS_B, false)
+                        .build()
+                        .getInterestedHintClusters(
+                                Set.of(hintA, hintB, hintC), Collections.emptySet());
+
+        assertThat(interestedHintSet).containsExactly(hintA, hintB);
+    }
+}
