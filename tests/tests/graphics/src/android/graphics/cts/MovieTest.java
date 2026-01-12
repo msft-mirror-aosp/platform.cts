3ce59651a4165d2336aff9e6c4a2cc3fa8166a75
Gregory Montoir <gmontoir@google.com>
2026-01-12 10:54:29 +0800

    Add regression test for b/471711544
    Adds a test case to MovieTest that validates the decoding
    of a GIF with a corrupted image block.
    
    Bug: 471711544
    Test: atest android.graphics.cts.MovieTest
    Change-Id: I9382f83e0ef535cc55c9676028118507063b63ee

--- tests/tests/graphics/src/android/graphics/cts/MovieTest.java
+++ tests/tests/graphics/src/android/graphics/cts/MovieTest.java
@@ -16,6 +16,7 @@
 
 package android.graphics.cts;
 
+import static junit.framework.Assert.fail;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -44,11 +45,23 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @SmallTest
 @RunWith(AndroidJUnit4.class)
 public class MovieTest {
     private final int MOVIE = R.drawable.animated;
+    private static final byte[] MOVIE_SHA256_HASH = {
+        (byte) 0xEE, (byte) 0xC5, (byte) 0xE7, (byte) 0x45,
+        (byte) 0x03, (byte) 0x2B, (byte) 0x97, (byte) 0x75,
+        (byte) 0xD6, (byte) 0x7F, (byte) 0x04, (byte) 0x0D,
+        (byte) 0x9A, (byte) 0xB9, (byte) 0x5A, (byte) 0xE3,
+        (byte) 0xDC, (byte) 0x29, (byte) 0x61, (byte) 0x00,
+        (byte) 0xCE, (byte) 0x0C, (byte) 0x5D, (byte) 0x6B,
+        (byte) 0xF9, (byte) 0x56, (byte) 0x67, (byte) 0xBF,
+        (byte) 0x2D, (byte) 0x27, (byte) 0xD2, (byte) 0xA6
+    };
 
     private Context mContext;
     private Resources mResources;
@@ -153,5 +166,47 @@ public class MovieTest {
         int expectedWidth = mResources.getDrawable(MOVIE).getIntrinsicWidth();
         int scaledWidth = WidgetTestUtils.convertDipToPixels(mContext, mMovie.width());
         assertEquals(expectedWidth, scaledWidth);
+    }
+
+    @Test
+    public void testCorruptGif() throws IOException {
+        InputStream in = mResources.openRawResource(MOVIE);
+        byte[] data = inputStreamToBytes(in);
+
+        // Ensure the test file remains unchanged, as test logic depends on these
+        // exact contents.
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            digest.update(data, 0, data.length);
+            final byte[] hashBytes = digest.digest();
+            assertTrue("Test data integrity check failed",
+                    MessageDigest.isEqual(hashBytes, MOVIE_SHA256_HASH));
+        } catch (NoSuchAlgorithmException e) {
+            fail("No SHA-256 available: " + e);
+            return;
+        }
+
+        // The test file (animated.gif) follows this structure:
+        //
+        //   Header: GIF89a
+        //   Extension Block (0xff) at 781
+        //   Extension Block (0xf9) at 800
+        //   Image Block 278x183 at 808
+        //   Extension Block (0xf9) at 10392
+        //   Image Block 278x183 at 10400
+        //   Extension Block (0xf9) at 18915
+        //   Image Block 278x183 at 18923
+        //   Extension Block (0xf9) at 27548
+        //   Image Block 278x183 at 27556
+        //   End of file reached.
+        //
+        // To simulate the bitstream corruption reported in b/471711544, we patch
+        // the first image at offset 819. We use a hardcoded offset here to
+        // avoid the overhead of a full GIF parser.
+
+        assertTrue("Unexpected GIF data", data.length > 819 && data[819] == (byte)0xff);
+        data[819] = (byte)0x80;
+
+        Movie.decodeByteArray(data, 0, data.length);
     }
 }
