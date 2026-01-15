82ce8fb4e808a138b214348543e7d2861f102724
Sangyun Yun <sangyun@google.com>
2026-01-15 10:07:03 +0000

    Fix SubscriptionManagerTest security check logic
    In the iteration test, modified the code so that the permission test
    can be performed after the enrollable plan is initialized.
    
    Bug: 475795160
    Test: atest testEnrollableSubscriptionPlans_Security with iteration 100
    Flag: TEST_ONLY
    Change-Id: Ifa3f043a071175d597c7d5f0b97f4aa030602c2b

--- tests/tests/telephony/current/src/android/telephony/cts/SubscriptionManagerTest.java
+++ tests/tests/telephony/current/src/android/telephony/cts/SubscriptionManagerTest.java
@@ -1951,12 +1951,13 @@ public class SubscriptionManagerTest {
         setAndVerifySubscriptionPlans(ctx, mSubId, Collections.emptyList(), expirationDuration);
     }
 
-    @Test
-    @RequiresFlagsEnabled(Flags.FLAG_SUBSCRIPTION_PLAN_ENHANCEMENT)
-    public void testEnrollableSubscriptionPlansExpiration() throws Exception {
+    /**
+     * Sets a subscription plan with an expiration time and verifies that it expires as expected.
+     */
+    private void setAndVerifySubscriptionPlanWithExpiration(long expirationDuration)
+            throws Exception {
         // SetUp: set expiration duration to 1sec to the sample plan
         Context ctx = InstrumentationRegistry.getContext();
-        long expirationDuration = 1000;
         SubscriptionPlan plan =
                 SubscriptionPlan.Builder.createRecurring(ZonedDateTime.now(), Period.ofMonths(1))
                         .setTitle("Expiring Plan")
@@ -1965,10 +1966,10 @@ public class SubscriptionManagerTest {
         // Test1. Initialize.
         setAndVerifySubscriptionPlans(ctx, mSubId, Collections.emptyList(), 0);
 
-        // Test2. set a plan with 1 sec duration.
+        // Test2. set a plan with expirationDuration.
         setAndVerifySubscriptionPlans(ctx, mSubId, Arrays.asList(plan), expirationDuration);
 
-        // Verify that 1 sec duration plan has gone.
+        // Verify that plan has gone.
         CarrierPrivilegeUtils.withCarrierPrivileges(
                 ctx,
                 mSubId,
@@ -1986,36 +1987,36 @@ public class SubscriptionManagerTest {
 
     @Test
     @RequiresFlagsEnabled(Flags.FLAG_SUBSCRIPTION_PLAN_ENHANCEMENT)
+    public void testEnrollableSubscriptionPlansExpiration() throws Exception {
+        long expirationDuration = 1000; // 1 second
+        setAndVerifySubscriptionPlanWithExpiration(expirationDuration);
+    }
+
+    @Test
+    @RequiresFlagsEnabled(Flags.FLAG_SUBSCRIPTION_PLAN_ENHANCEMENT)
     public void testEnrollableSubscriptionPlans_Security() throws Exception {
-        Context ctx = InstrumentationRegistry.getContext();
-        SubscriptionPlan plan =
-                SubscriptionPlan.Builder.createRecurring(
-                                ZonedDateTime.parse("2025-01-01T00:00:00.000Z"), Period.ofMonths(1))
-                        .setTitle("Owner Check Plan")
-                        .build();
+        long expirationDuration = 1000; // 1 second
+        setAndVerifySubscriptionPlanWithExpiration(expirationDuration);
 
         // Test1. Verify access without MANAGE_SUBSCRIPTION_PLANS permission.
         // (Shell does not have MANAGE_SUBSCRIPTION_PLANS permission.)
         try {
             // Act: call set plan API
-            ShellIdentityUtils.invokeMethodWithShellPermissionsNoReturn(
-                    mSm, (sm) -> sm.setEnrollableSubscriptionPlans(mSubId, Arrays.asList(plan), 0));
+            mSm.setEnrollableSubscriptionPlans(mSubId, Collections.emptyList(), 0);
             // Verify that exceptions should be thrown if app does not have the permission.
-            fail("Fails to set enrollable plans with MANAGE_SUBSCRIPTION_PLANS permission.");
+            fail("Fails to set enrollable plans. this test requires SecurityException.");
         } catch (SecurityException e) {
             // Success
         }
 
-        // Test2. Carrier Privileges & Owner Access
-        setAndVerifySubscriptionPlans(ctx, mSubId, Arrays.asList(plan), 0);
-
-        // Test3. get plans with owner access.
+        // Test2. test getEnrollableSubscriptionPlans
         try {
-            // Act: call get plan API without a permission but this app is the owner.
+            // Act: call get plan API
             List<SubscriptionPlan> plans = mSm.getEnrollableSubscriptionPlans(mSubId);
-            // Verifying access as Owner without permission
+            // Verify that exceptions should be thrown if app does not have the permission.
+            fail("Fails to get enrollable plans. this test requires SecurityException.");
         } catch (SecurityException e) {
-            fail("Owner should have access without extra permissions: " + e.getMessage());
+            // Success
         }
     }
 }
