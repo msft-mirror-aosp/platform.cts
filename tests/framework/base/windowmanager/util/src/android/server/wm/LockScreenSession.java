43166d6d4cbfb97e3824da977a2501d8f2d3638d
Derek Huang <hderek@google.com>
2026-01-21 16:09:15 +0800

    Wait for animations to settle before checking keyguard
    (This CL was cherr-picked from aosp/3895298)
    
    Currently, the LockScreenSession close method relies on a sleep-wakeup
    cycle for removing keyguard, and then performs an unlockDevice if the
    keyguard is still up. However, before checking whether this is needed or
    not, the method does not wait for the animations resulting from
    sleep-wakeup cycle to stabilize. This change adds this wait to ensure
    that the unlockDevice steps are only performed if necessary.
    
    On Android XR, this resulted in solving a race condition. Before this
    change, the test sometimes erroneously tapped on SysUIVisibilityBarrier
    because the animations were still in progress, which resulted in it
    being put to the top of task stack. As a result, the test activity was
    minimized, causing assertion failure when the test checked for it to be
    in ON_RESUME state. After this change, the keyguard is always removed
    due to the sleep-wakeup cycle, resulting in the test never needing to
    call the offending method midway through a running transition.
    
    Bug: 447452305
    Test: atest android.server.wm.lifecycle.ActivityLifecycleKeyguardTests
    Change-Id: Ie7c518bbe66393c28f2f960597326a661eebe560

--- tests/framework/base/windowmanager/util/src/android/server/wm/LockScreenSession.java
+++ tests/framework/base/windowmanager/util/src/android/server/wm/LockScreenSession.java
@@ -316,6 +316,9 @@ public class LockScreenSession implements AutoCloseable {
             sleepDevice();
             wakeUpDevice();
         }
+        // After sleep and wakeup cycle, it might take a while for UI to stabilize. Wait for all
+        // animations to complete before checking keyguard state.
+        mWmState.waitForAppTransitionIdleOnDisplay(DEFAULT_DISPLAY);
         if (isKeyguardLocked()) {
             unlockDevice();
         }
