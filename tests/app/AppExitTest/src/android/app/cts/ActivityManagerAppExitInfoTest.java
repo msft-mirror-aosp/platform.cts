4d30574cf82fa0201898fbab239ba40684f1940e
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2026-01-24 20:21:07 -0800

    Revert "Force fullscreen launch for HeartbeatActivity in Activit..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/e0e78286-d1e7-45e2-96bd-194884a1e32f). Please review this revert and approve if it looks good to you.
    
    Bug: 478529517
    Change-Id: If4054eb8d04976d57cb33aaea5434e805f31e2c9
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com
    Bug: 425545138

--- tests/app/AppExitTest/src/android/app/cts/ActivityManagerAppExitInfoTest.java
+++ tests/app/AppExitTest/src/android/app/cts/ActivityManagerAppExitInfoTest.java
@@ -40,13 +40,11 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningAppProcessInfo;
-import android.app.ActivityOptions;
 import android.app.AnrTypes;
 import android.app.ApplicationExitInfo;
 import android.app.ApplicationExitInfo.AnrInfo;
 import android.app.Flags;
 import android.app.Instrumentation;
-import android.app.WindowConfiguration;
 import android.app.stubs.shared.IHeartbeat;
 import android.app.tools.WatchUidRunner;
 import android.content.BroadcastReceiver;
@@ -92,7 +90,6 @@ import android.view.KeyEvent;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
-import com.android.bedstead.nene.TestApis;
 import com.android.compatibility.common.util.AmMonitor;
 import com.android.compatibility.common.util.ApiLevelUtil;
 import com.android.compatibility.common.util.CddTest;
@@ -2168,10 +2165,7 @@ public final class ActivityManagerAppExitInfoTest {
         clientIntent.putExtra(HEARTBEAT_COUNTDOWN_NAME, HEARTBEAT_COUNTDOWN);
         clientIntent.putExtra(HEARTBEAT_INTERVAL_NAME, HEARTBEAT_INTERVAL);
         clientIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        ActivityOptions options = ActivityOptions.makeBasic();
-        options.setLaunchWindowingMode(WindowConfiguration.WINDOWING_MODE_FULLSCREEN);
-        SystemUtil.runWithShellPermissionIdentity(
-                () -> TestApis.activities().startActivity(clientIntent, options.toBundle()));
+        mContext.startActivity(clientIntent);
         sleep(1000);
 
         // Launch another app to bring the HeartbeatActivity to background
