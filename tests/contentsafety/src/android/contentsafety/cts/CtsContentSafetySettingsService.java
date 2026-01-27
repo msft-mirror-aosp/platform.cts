1ea6eb5443f5ac4e2b6824464c4eca504a3f49f7
Sherman Pay <shermanpay@google.com>
2026-01-26 21:42:58 -0800

    Add CTS tests for ContentSafety API
    This change reverts ag/38073717 and includes ag/38073717
    
    Flag: android.app.contentsafety.flags.enable_contentsafety
    Test: presubmits
    BUG: 455653466
    Change-Id: Ic2337838e09dc5ef76d328c4c2f7c35c18dc50a5
    Tests: I44700010503765930

---
+++ tests/contentsafety/src/android/contentsafety/cts/CtsContentSafetySettingsService.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.contentsafety.cts;
+
+import android.app.contentsafety.FeatureException;
+import android.os.CancellationSignal;
+import android.os.OutcomeReceiver;
+import android.os.UserHandle;
+import android.service.contentsafety.ContentSafetySettingsService;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class CtsContentSafetySettingsService extends ContentSafetySettingsService {
+    static final String TAG = "SampleContentSafetySettingsService";
+
+    @Override
+    public void onIsFeatureEnabled(
+            int featureType,
+            @NonNull UserHandle userId,
+            @Nullable CancellationSignal cancellationSignal,
+            @NonNull OutcomeReceiver<Boolean, FeatureException> outcomeReceiver) {
+        Log.i(TAG, "Received onIsFeatureEnabled");
+        outcomeReceiver.onResult(true);
+    }
+}
