8ed5897477e999ac95cffda00f64383e9cb9854a
Yulin Shen <yulinshen@google.com>
2026-02-03 18:28:22 -0800

    Add MUM tests for MaximumTimeToLock API
    We added a new policy class MaximumTimeToLockMultiUserManagement that
    only consider the UC mode.
    
    Bug: 444381053
    Test: I78300010503356688
    Change-Id: Ie3f5e228c60a49c8e2e8b520bb6dac10619a905c

---
+++ common/device-side/bedstead/modules/enterprise/src/main/kotlin/com/android/bedstead/enterprise/policies/MaximumTimeToLockMultiUserManagement.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.bedstead.enterprise.policies
+
+import com.android.bedstead.enterprise.annotations.EnterprisePolicy
+import com.android.bedstead.enterprise.annotations.EnterprisePolicy.APPLIED_BY_USER_CONTROLLER
+import com.android.bedstead.enterprise.annotations.EnterprisePolicy.APPLIES_TO_OWN_USER
+
+/**
+ * Policy for {@code DevicePolicyManager#setMaximumTimeToLock()} with multi-user management.
+ *
+ * TODO(b/461439459): Merge this @EnterprisePolicy with MaximumTimeToLock once MUM feature flags are
+ *   landed. We want to ignore MUM tests for now until the feature flags for MUM are landed in AOSP.
+ */
+@EnterprisePolicy(dpc = [APPLIED_BY_USER_CONTROLLER or APPLIES_TO_OWN_USER])
+class MaximumTimeToLockMultiUserManagement {}
