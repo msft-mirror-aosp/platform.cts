ea2b06f12ca4070bd7bde09a710d435b0c9e9de5
Sergiy Belozorov <sergiyb@google.com>
2026-02-11 05:12:00 -0800

    Update CTS tests after renaming device provisioning method
    The method provisionMultiUserDevice was renamed to
    provisionMultiUserManagedDevice.
    
    Bug: 477901795, 390162247
    Change-Id: I2a496a03f57fa3b3dfbda4e7204ca56b8bf35246

--- common/device-side/bedstead/nene/src/main/java/com/android/bedstead/nene/devicepolicy/DevicePolicy.kt
+++ common/device-side/bedstead/nene/src/main/java/com/android/bedstead/nene/devicepolicy/DevicePolicy.kt
@@ -273,12 +273,12 @@ object DevicePolicy {
                 .use { devicePolicyManager(user).clearOrganizationId() }
     }
 
-    fun clearMultiUserDeviceManagement(dmrhReceiver: ComponentName) {
+    fun clearMultiuserDeviceManagement(deviceControllerPacakgeName: String) {
         TestApis.permissions().withPermission(
             CommonPermissions.MANAGE_PROFILE_AND_DEVICE_OWNERS
         ).use {
             val dpm = devicePolicyManager(TestApis.users().system())
-            dpm.clearMultiUserDeviceManagement(dmrhReceiver)
+            dpm.clearMultiuserDeviceManagement(deviceControllerPacakgeName)
         }
     }
 
