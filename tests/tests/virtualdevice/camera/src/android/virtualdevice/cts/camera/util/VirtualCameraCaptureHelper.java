963a11cde74c347baa9c4d8d4c7e198e3bafe3e1
Priyanka Advani (xWF) <padvani@google.com>
2026-03-09 17:47:25 -0700

    Revert "[virtualcamera] Test latency of abortCapture()"
    Revert submission 38715251-vc-flush
    
    Reason for revert: Droidmonitor created revert due to b/491229664. Will be verifying through ABTD before submission.
    
    Fix: 491229664
    
    Reverted changes: /q/submissionid:38715251-vc-flush
    
    Bug: 487340222
    Change-Id: I1317331863a5b42d9810377cda00af9fd172b50b

--- tests/tests/virtualdevice/camera/src/android/virtualdevice/cts/camera/util/VirtualCameraCaptureHelper.java
+++ tests/tests/virtualdevice/camera/src/android/virtualdevice/cts/camera/util/VirtualCameraCaptureHelper.java
@@ -103,6 +103,11 @@ public class VirtualCameraCaptureHelper {
     private final Handler mImageReaderHandler = VirtualCameraUtils.createHandler(
             "image-reader-callback");
 
+    /** Returns a {@link Handler} for image reader callbacks. */
+    public Handler getImageReaderHandler() {
+        return mImageReaderHandler;
+    }
+
     private final Executor mCameraExecutor = Executors.newSingleThreadExecutor();
     @Mock
     private CameraDevice.StateCallback mCameraStateCallback;
@@ -406,11 +411,6 @@ public class VirtualCameraCaptureHelper {
     private Surface getInputSurface() {
         mVirtualCameraCallback.waitForSessionConfigured();
         return mVirtualCameraCallback.mConfiguredStreams.values().iterator().next();
-    }
-
-    /** Returns the input surface for the specified input stream id. */
-    public Surface getInputSurface(int streamId) {
-        return mVirtualCameraCallback.mConfiguredStreams.get(streamId);
     }
 
     /** Create a new capture session with the provided ImageReader as output targets. */
@@ -792,9 +792,17 @@ public class VirtualCameraCaptureHelper {
                 int streamId, @NonNull Surface surface, int width, int height, int format) {
             Log.d(
                     TAG,
-                    ("onStreamConfigured() called with: streamId = [%d], surface = [%s], width = "
-                                    + "[%d], height = [%d], format = [%d]")
-                            .formatted(streamId, surface, width, height, format));
+                    "onStreamConfigured() called with: streamId = ["
+                            + streamId
+                            + "], surface = ["
+                            + surface
+                            + "], width = ["
+                            + width
+                            + "], height = ["
+                            + height
+                            + "], format = ["
+                            + format
+                            + "]");
             mConfiguredStreams.put(streamId, surface);
             mCallbackDelegate.onStreamConfigured(streamId, surface, width, height, format);
             mSessionConfiguredLatch.countDown();
