b0eb1eb47ff60a153f1e616ac1d0f17b7b3f8672
Ömer Faruk Yılmaz <ofy@google.com>
2026-03-11 11:43:23 +0900

    Convert BluetoothSocketExceptionTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest BluetoothSocketExceptionTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: Iba21b5c44bc42b41d9aaf2ecf3c035763bf29203

--- tests/tests/bluetooth/src/android/bluetooth/cts/BluetoothSocketExceptionTest.kt
+++ tests/tests/bluetooth/src/android/bluetooth/cts/BluetoothSocketExceptionTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,46 +14,40 @@
  * limitations under the License.
  */
 
-package android.bluetooth.cts;
+package android.bluetooth.cts
 
-import static com.google.common.truth.Truth.assertThat;
-
-import android.bluetooth.BluetoothSocketException;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.SmallTest;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import android.bluetooth.BluetoothSocketException
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /** Unit test to test APIs and functionality for {@link BluetoothSocketException}. */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4::class)
 @SmallTest
-public final class BluetoothSocketExceptionTest {
+class BluetoothSocketExceptionTest {
 
     @Test
-    public void getErrorCode_returnsCorrectErrorCode() {
-        BluetoothSocketException exception =
-                new BluetoothSocketException(BluetoothSocketException.SOCKET_CONNECTION_FAILURE);
+    fun getErrorCode_returnsCorrectErrorCode() {
+        val exception = BluetoothSocketException(BluetoothSocketException.SOCKET_CONNECTION_FAILURE)
 
-        assertThat(exception.getErrorCode())
-                .isEqualTo(BluetoothSocketException.SOCKET_CONNECTION_FAILURE);
+        assertThat(exception.errorCode)
+            .isEqualTo(BluetoothSocketException.SOCKET_CONNECTION_FAILURE)
     }
 
     @Test
-    public void getMessage_returnsCustomErrorMsg() {
-        String customErrMsg = "This is a custom error message";
-        BluetoothSocketException exception =
-                new BluetoothSocketException(BluetoothSocketException.UNSPECIFIED, customErrMsg);
+    fun getMessage_returnsCustomErrorMsg() {
+        val customErrMsg = "This is a custom error message"
+        val exception = BluetoothSocketException(BluetoothSocketException.UNSPECIFIED, customErrMsg)
 
-        assertThat(exception.getMessage()).isEqualTo(customErrMsg);
+        assertThat(exception.message).isEqualTo(customErrMsg)
     }
 
     @Test
-    public void getMessage_returnsErrorMsgWhenOnlyCodeIsProvided() {
-        BluetoothSocketException exception =
-                new BluetoothSocketException(BluetoothSocketException.UNSPECIFIED);
+    fun getMessage_returnsErrorMsgWhenOnlyCodeIsProvided() {
+        val exception = BluetoothSocketException(BluetoothSocketException.UNSPECIFIED)
 
-        assertThat(exception.getMessage()).isNotNull();
+        assertThat(exception.message).isNotNull()
     }
 }
