30e0f7a925f0ceec7266f9791293138bf8f0f17a
Ömer Faruk Yılmaz <ofy@google.com>
2026-03-11 11:48:29 +0900

    Convert BluetoothHidDeviceCallbackTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest BluetoothHidDeviceCallbackTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: I6b8e8cfd449d567821b89920b4505c9809bc8ac2

--- tests/tests/bluetooth/src/android/bluetooth/cts/BluetoothHidDeviceCallbackTest.kt
+++ tests/tests/bluetooth/src/android/bluetooth/cts/BluetoothHidDeviceCallbackTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,57 +14,28 @@
  * limitations under the License.
  */
 
-package android.bluetooth.cts;
+package android.bluetooth.cts
 
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothHidDevice;
+import android.bluetooth.BluetoothHidDevice
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import androidx.test.runner.AndroidJUnit4;
+@RunWith(AndroidJUnit4::class)
+class BluetoothHidDeviceCallbackTest {
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-@RunWith(AndroidJUnit4.class)
-public class BluetoothHidDeviceCallbackTest {
-    private BluetoothHidDevice.Callback mHidDeviceCallback =
-            new BluetoothHidDevice.Callback() {
-                @Override
-                public void onGetReport(
-                        BluetoothDevice device, byte type, byte id, int bufferSize) {
-                    super.onGetReport(device, type, id, bufferSize);
-                }
-
-                @Override
-                public void onSetReport(BluetoothDevice device, byte type, byte id, byte[] data) {
-                    super.onSetReport(device, type, id, data);
-                }
-
-                @Override
-                public void onSetProtocol(BluetoothDevice device, byte protocol) {
-                    super.onSetProtocol(device, protocol);
-                }
-
-                @Override
-                public void onInterruptData(BluetoothDevice device, byte reportId, byte[] data) {
-                    super.onInterruptData(device, reportId, data);
-                }
-
-                @Override
-                public void onVirtualCableUnplug(BluetoothDevice device) {
-                    super.onVirtualCableUnplug(device);
-                }
-            };
+    private val hidDeviceCallback = object : BluetoothHidDevice.Callback() {}
 
     @Test
-    public void hidDeviceCallback() {
+    fun hidDeviceCallback() {
         // TODO: Provide a way to simulate BluetoothHidHost for better testing.
         // We may need to have a new BluetoothAdapter.getProfileProxy method with a new test profile
         // like HID_DEVICE_TEST which also takes a mock hid host instance.
-        mHidDeviceCallback.onGetReport(null, (byte) 0, (byte) 0, 0);
-        mHidDeviceCallback.onSetReport(null, (byte) 0, (byte) 0, null);
-        mHidDeviceCallback.onSetProtocol(null, (byte) 0);
-        mHidDeviceCallback.onInterruptData(null, (byte) 0, null);
-        mHidDeviceCallback.onVirtualCableUnplug(null);
+        hidDeviceCallback.onGetReport(null, 0.toByte(), 0.toByte(), 0)
+        hidDeviceCallback.onSetReport(null, 0.toByte(), 0.toByte(), null)
+        hidDeviceCallback.onSetProtocol(null, 0.toByte())
+        hidDeviceCallback.onInterruptData(null, 0.toByte(), null)
+        hidDeviceCallback.onVirtualCableUnplug(null)
         // FYI, onAppStatusChanged and onConnectionStateChanged are covered by CtsVerifier.
     }
 }
