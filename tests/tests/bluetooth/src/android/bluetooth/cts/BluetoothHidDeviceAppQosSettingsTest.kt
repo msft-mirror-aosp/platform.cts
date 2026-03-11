9e40a9b34c1c668502efaedc8ee8f0e1dcaa976c
Ömer Faruk Yılmaz <ofy@google.com>
2026-03-11 11:41:08 +0900

    Convert BluetoothHidDeviceAppQosSettingsTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest BluetoothHidDeviceAppQosSettingsTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: I5f8e0a04ecaf984176d945ba4ac74cf176527e8f

--- tests/tests/bluetooth/src/android/bluetooth/cts/BluetoothHidDeviceAppQosSettingsTest.kt
+++ tests/tests/bluetooth/src/android/bluetooth/cts/BluetoothHidDeviceAppQosSettingsTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,46 +14,43 @@
  * limitations under the License.
  */
 
-package android.bluetooth.cts;
+package android.bluetooth.cts
 
-import static com.google.common.truth.Truth.assertThat;
+import android.bluetooth.BluetoothHidDeviceAppQosSettings
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import android.bluetooth.BluetoothHidDeviceAppQosSettings;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.SmallTest;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4::class)
 @SmallTest
-public class BluetoothHidDeviceAppQosSettingsTest {
-    private final int TEST_SERVICE_TYPE = BluetoothHidDeviceAppQosSettings.SERVICE_BEST_EFFORT;
-    private final int TEST_TOKEN_RATE = 800;
-    private final int TEST_TOKEN_BUCKET_SIZE = 9;
-    private final int TEST_PEAK_BANDWIDTH = 10;
-    private final int TEST_LATENCY = 11250;
-    private final int TEST_DELAY_VARIATION = BluetoothHidDeviceAppQosSettings.MAX;
+class BluetoothHidDeviceAppQosSettingsTest {
+
+    private val TEST_SERVICE_TYPE = BluetoothHidDeviceAppQosSettings.SERVICE_BEST_EFFORT
+    private val TEST_TOKEN_RATE = 800
+    private val TEST_TOKEN_BUCKET_SIZE = 9
+    private val TEST_PEAK_BANDWIDTH = 10
+    private val TEST_LATENCY = 11250
+    private val TEST_DELAY_VARIATION = BluetoothHidDeviceAppQosSettings.MAX
 
     @Test
-    public void allMethods() {
-        BluetoothHidDeviceAppQosSettings bluetoothHidDeviceAppQosSettings =
-                new BluetoothHidDeviceAppQosSettings(
-                        BluetoothHidDeviceAppQosSettings.SERVICE_BEST_EFFORT,
-                        TEST_TOKEN_RATE,
-                        TEST_TOKEN_BUCKET_SIZE,
-                        TEST_PEAK_BANDWIDTH,
-                        TEST_LATENCY,
-                        TEST_DELAY_VARIATION);
-        assertThat(bluetoothHidDeviceAppQosSettings.getServiceType()).isEqualTo(TEST_SERVICE_TYPE);
-        assertThat(bluetoothHidDeviceAppQosSettings.getLatency()).isEqualTo(TEST_LATENCY);
-        assertThat(bluetoothHidDeviceAppQosSettings.getTokenRate()).isEqualTo(TEST_TOKEN_RATE);
-        assertThat(bluetoothHidDeviceAppQosSettings.getPeakBandwidth())
-                .isEqualTo(TEST_PEAK_BANDWIDTH);
-        assertThat(bluetoothHidDeviceAppQosSettings.getDelayVariation())
-                .isEqualTo(TEST_DELAY_VARIATION);
-        assertThat(bluetoothHidDeviceAppQosSettings.getTokenBucketSize())
-                .isEqualTo(TEST_TOKEN_BUCKET_SIZE);
+    fun allMethods() {
+        val bluetoothHidDeviceAppQosSettings =
+            BluetoothHidDeviceAppQosSettings(
+                BluetoothHidDeviceAppQosSettings.SERVICE_BEST_EFFORT,
+                TEST_TOKEN_RATE,
+                TEST_TOKEN_BUCKET_SIZE,
+                TEST_PEAK_BANDWIDTH,
+                TEST_LATENCY,
+                TEST_DELAY_VARIATION,
+            )
+        assertThat(bluetoothHidDeviceAppQosSettings.serviceType).isEqualTo(TEST_SERVICE_TYPE)
+        assertThat(bluetoothHidDeviceAppQosSettings.latency).isEqualTo(TEST_LATENCY)
+        assertThat(bluetoothHidDeviceAppQosSettings.tokenRate).isEqualTo(TEST_TOKEN_RATE)
+        assertThat(bluetoothHidDeviceAppQosSettings.peakBandwidth).isEqualTo(TEST_PEAK_BANDWIDTH)
+        assertThat(bluetoothHidDeviceAppQosSettings.delayVariation).isEqualTo(TEST_DELAY_VARIATION)
+        assertThat(bluetoothHidDeviceAppQosSettings.tokenBucketSize)
+            .isEqualTo(TEST_TOKEN_BUCKET_SIZE)
     }
 }
