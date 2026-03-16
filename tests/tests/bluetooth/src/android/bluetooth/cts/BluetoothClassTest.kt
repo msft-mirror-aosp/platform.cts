fdb073926eac2b93e5844845fa063d9740fc6369
Ömer Faruk Yılmaz <ofy@google.com>
2026-03-16 12:21:59 +0900

    Convert BluetoothClassTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest BluetoothClassTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: Ibcbc1b5cedabff90e41753e2a0b5f3ce7a30569a

--- tests/tests/bluetooth/src/android/bluetooth/cts/BluetoothClassTest.kt
+++ tests/tests/bluetooth/src/android/bluetooth/cts/BluetoothClassTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,95 +14,92 @@
  * limitations under the License.
  */
 
-package android.bluetooth.cts;
+package android.bluetooth.cts
 
-import static com.google.common.truth.Truth.assertThat;
+import android.bluetooth.BluetoothClass
+import android.os.Parcel
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import android.bluetooth.BluetoothClass;
-import android.os.Parcel;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.SmallTest;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-/** Unit test cases for {@link BluetoothClass}. */
-@RunWith(AndroidJUnit4.class)
+/** Unit test cases for [BluetoothClass]. */
+@RunWith(AndroidJUnit4::class)
 @SmallTest
-public class BluetoothClassTest {
+class BluetoothClassTest {
 
-    private BluetoothClass mBluetoothClassHeadphones;
-    private BluetoothClass mBluetoothClassPhone;
-    private BluetoothClass mBluetoothClassService;
+    private lateinit var bluetoothClassHeadphones: BluetoothClass
+    private lateinit var bluetoothClassPhone: BluetoothClass
+    private lateinit var bluetoothClassService: BluetoothClass
 
-    private BluetoothClass createBtClass(int deviceClass) {
-        Parcel p = Parcel.obtain();
-        p.writeInt(deviceClass);
-        p.setDataPosition(0); // reset position of parcel before passing to constructor
+    private fun createBtClass(deviceClass: Int): BluetoothClass {
+        val p = Parcel.obtain()
+        p.writeInt(deviceClass)
+        p.setDataPosition(0) // reset position of parcel before passing to constructor
 
-        BluetoothClass bluetoothClass = BluetoothClass.CREATOR.createFromParcel(p);
-        p.recycle();
-        return bluetoothClass;
+        val bluetoothClass = BluetoothClass.CREATOR.createFromParcel(p)
+        p.recycle()
+        return bluetoothClass
     }
 
     @Before
-    public void setUp() {
-        mBluetoothClassHeadphones = createBtClass(BluetoothClass.Device.AUDIO_VIDEO_HEADPHONES);
-        mBluetoothClassPhone = createBtClass(BluetoothClass.Device.Major.PHONE);
-        mBluetoothClassService = createBtClass(BluetoothClass.Service.NETWORKING);
+    fun setUp() {
+        bluetoothClassHeadphones = createBtClass(BluetoothClass.Device.AUDIO_VIDEO_HEADPHONES)
+        bluetoothClassPhone = createBtClass(BluetoothClass.Device.Major.PHONE)
+        bluetoothClassService = createBtClass(BluetoothClass.Service.NETWORKING)
     }
 
     @Test
-    public void hasService() {
-        assertThat(mBluetoothClassService.hasService(BluetoothClass.Service.NETWORKING)).isTrue();
-        assertThat(mBluetoothClassService.hasService(BluetoothClass.Service.TELEPHONY)).isFalse();
+    fun hasService() {
+        assertThat(bluetoothClassService.hasService(BluetoothClass.Service.NETWORKING)).isTrue()
+        assertThat(bluetoothClassService.hasService(BluetoothClass.Service.TELEPHONY)).isFalse()
     }
 
     @Test
-    public void getMajorDeviceClass() {
-        assertThat(mBluetoothClassHeadphones.getMajorDeviceClass())
-                .isEqualTo(BluetoothClass.Device.Major.AUDIO_VIDEO);
-        assertThat(mBluetoothClassPhone.getMajorDeviceClass())
-                .isEqualTo(BluetoothClass.Device.Major.PHONE);
+    fun getMajorDeviceClass() {
+        assertThat(bluetoothClassHeadphones.majorDeviceClass)
+            .isEqualTo(BluetoothClass.Device.Major.AUDIO_VIDEO)
+        assertThat(bluetoothClassPhone.majorDeviceClass)
+            .isEqualTo(BluetoothClass.Device.Major.PHONE)
     }
 
     @Test
-    public void getDeviceClass() {
-        assertThat(mBluetoothClassHeadphones.getDeviceClass())
-                .isEqualTo(BluetoothClass.Device.AUDIO_VIDEO_HEADPHONES);
-        assertThat(mBluetoothClassPhone.getDeviceClass())
-                .isEqualTo(BluetoothClass.Device.PHONE_UNCATEGORIZED);
+    fun getDeviceClass() {
+        assertThat(bluetoothClassHeadphones.deviceClass)
+            .isEqualTo(BluetoothClass.Device.AUDIO_VIDEO_HEADPHONES)
+        assertThat(bluetoothClassPhone.deviceClass)
+            .isEqualTo(BluetoothClass.Device.PHONE_UNCATEGORIZED)
     }
 
     @Test
-    public void getClassOfDevice() {
-        assertThat(mBluetoothClassHeadphones.getDeviceClass())
-                .isEqualTo(BluetoothClass.Device.AUDIO_VIDEO_HEADPHONES);
-        assertThat(mBluetoothClassPhone.getMajorDeviceClass())
-                .isEqualTo(BluetoothClass.Device.Major.PHONE);
+    fun getClassOfDevice() {
+        assertThat(bluetoothClassHeadphones.deviceClass)
+            .isEqualTo(BluetoothClass.Device.AUDIO_VIDEO_HEADPHONES)
+        assertThat(bluetoothClassPhone.majorDeviceClass)
+            .isEqualTo(BluetoothClass.Device.Major.PHONE)
     }
 
     @Test
-    public void doesClassMatch() {
-        assertThat(mBluetoothClassHeadphones.doesClassMatch(BluetoothClass.PROFILE_A2DP)).isTrue();
-        assertThat(mBluetoothClassHeadphones.doesClassMatch(BluetoothClass.PROFILE_HEADSET))
-                .isFalse();
+    fun doesClassMatch() {
+        assertThat(bluetoothClassHeadphones.doesClassMatch(BluetoothClass.PROFILE_A2DP)).isTrue()
+        assertThat(bluetoothClassHeadphones.doesClassMatch(BluetoothClass.PROFILE_HEADSET))
+            .isFalse()
 
-        assertThat(mBluetoothClassPhone.doesClassMatch(BluetoothClass.PROFILE_OPP)).isTrue();
-        assertThat(mBluetoothClassPhone.doesClassMatch(BluetoothClass.PROFILE_HEADSET)).isFalse();
+        assertThat(bluetoothClassPhone.doesClassMatch(BluetoothClass.PROFILE_OPP)).isTrue()
+        assertThat(bluetoothClassPhone.doesClassMatch(BluetoothClass.PROFILE_HEADSET)).isFalse()
 
-        assertThat(mBluetoothClassService.doesClassMatch(BluetoothClass.PROFILE_PANU)).isTrue();
-        assertThat(mBluetoothClassService.doesClassMatch(BluetoothClass.PROFILE_OPP)).isFalse();
+        assertThat(bluetoothClassService.doesClassMatch(BluetoothClass.PROFILE_PANU)).isTrue()
+        assertThat(bluetoothClassService.doesClassMatch(BluetoothClass.PROFILE_OPP)).isFalse()
     }
 
     @Test
-    public void innerClasses() {
+    fun innerClasses() {
         // Just instantiate static inner classes for exposing constants
         // to make test coverage tool happy.
-        BluetoothClass.Device device = new BluetoothClass.Device();
-        BluetoothClass.Device.Major major = new BluetoothClass.Device.Major();
-        BluetoothClass.Service service = new BluetoothClass.Service();
+        BluetoothClass.Device()
+        BluetoothClass.Device.Major()
+        BluetoothClass.Service()
     }
 }
