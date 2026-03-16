49c33c94c7e98c312fc738ccb303362065b2cac6
Ömer Faruk Yılmaz <ofy@google.com>
2026-03-16 10:58:06 +0900

    Convert BluetoothHapPresetInfoTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest BluetoothHapPresetInfoTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: I3e69aa43126fb0fa1e0e04d28e6548c05213fce3

--- tests/tests/bluetooth/src/android/bluetooth/cts/BluetoothHapPresetInfoTest.kt
+++ tests/tests/bluetooth/src/android/bluetooth/cts/BluetoothHapPresetInfoTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,76 +14,74 @@
  * limitations under the License.
  */
 
-package android.bluetooth.cts;
+package android.bluetooth.cts
 
-import static com.google.common.truth.Truth.assertThat;
+import android.bluetooth.BluetoothHapPresetInfo
+import android.bluetooth.BluetoothProfile
+import android.bluetooth.test_utils.BlockingBluetoothAdapter
+import android.os.Build
+import android.os.Parcel
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.test.platform.app.InstrumentationRegistry
+import com.android.compatibility.common.util.ApiLevelUtil
+import com.android.compatibility.common.util.CddTest
+import com.google.common.truth.Expect
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assume.assumeTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import static org.junit.Assume.assumeTrue;
-
-import android.bluetooth.BluetoothHapPresetInfo;
-import android.bluetooth.BluetoothProfile;
-import android.bluetooth.test_utils.BlockingBluetoothAdapter;
-import android.content.Context;
-import android.os.Build;
-import android.os.Parcel;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.SmallTest;
-import androidx.test.platform.app.InstrumentationRegistry;
-
-import com.android.compatibility.common.util.ApiLevelUtil;
-import com.android.compatibility.common.util.CddTest;
-
-import com.google.common.truth.Expect;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4::class)
 @SmallTest
-public class BluetoothHapPresetInfoTest {
-    @Rule public final Expect expect = Expect.create();
+class BluetoothHapPresetInfoTest {
+    @get:Rule val expect: Expect = Expect.create()
 
-    private static final int TEST_PRESET_INDEX = 15;
-    private static final String TEST_PRESET_NAME = "Test";
-
-    private static final Context sContext =
-            InstrumentationRegistry.getInstrumentation().getContext();
+    private val context = InstrumentationRegistry.getInstrumentation().context
 
     @Before
-    public void setUp() {
-        assumeTrue(ApiLevelUtil.isAtLeast(Build.VERSION_CODES.TIRAMISU));
-        assumeTrue(TestUtils.isBleSupported(sContext));
-        assumeTrue(TestUtils.isProfileEnabled(BluetoothProfile.HAP_CLIENT));
-
-        assertThat(BlockingBluetoothAdapter.enable()).isTrue();
+    fun setUp() {
+        assumeTrue(ApiLevelUtil.isAtLeast(Build.VERSION_CODES.TIRAMISU))
+        assumeTrue(TestUtils.isBleSupported(context))
+        assumeTrue(TestUtils.isProfileEnabled(BluetoothProfile.HAP_CLIENT))
+        assertThat(BlockingBluetoothAdapter.enable()).isTrue()
     }
 
-    @CddTest(requirements = {"7.4.3/C-2-1", "7.4.3/C-3-2"})
+    @CddTest(requirements = ["7.4.3/C-2-1", "7.4.3/C-3-2"])
     @Test
-    public void createHapPresetInfo() {
-        BluetoothHapPresetInfo presetInfo =
-                createHapPresetInfo(
-                        TEST_PRESET_INDEX,
-                        TEST_PRESET_NAME,
-                        true /* isAvailable */,
-                        false /* isWritable */);
-        expect.that(presetInfo.getIndex()).isEqualTo(TEST_PRESET_INDEX);
-        expect.that(presetInfo.getName()).isEqualTo(TEST_PRESET_NAME);
-        expect.that(presetInfo.isAvailable()).isTrue();
-        expect.that(presetInfo.isWritable()).isFalse();
+    fun createHapPresetInfo() {
+        val presetInfo =
+            createHapPresetInfo(
+                TEST_PRESET_INDEX,
+                TEST_PRESET_NAME,
+                isAvailable = true,
+                isWritable = false,
+            )
+        expect.that(presetInfo.index).isEqualTo(TEST_PRESET_INDEX)
+        expect.that(presetInfo.name).isEqualTo(TEST_PRESET_NAME)
+        expect.that(presetInfo.isAvailable).isTrue()
+        expect.that(presetInfo.isWritable).isFalse()
     }
 
-    private static BluetoothHapPresetInfo createHapPresetInfo(
-            int presetIndex, String presetName, boolean isAvailable, boolean isWritable) {
-        Parcel out = Parcel.obtain();
-        out.writeInt(presetIndex);
-        out.writeString(presetName);
-        out.writeBoolean(isWritable);
-        out.writeBoolean(isAvailable);
-        out.setDataPosition(0); // reset position of parcel before passing to constructor
-        return BluetoothHapPresetInfo.CREATOR.createFromParcel(out);
+    private fun createHapPresetInfo(
+        presetIndex: Int,
+        presetName: String,
+        isAvailable: Boolean,
+        isWritable: Boolean,
+    ): BluetoothHapPresetInfo {
+        val out = Parcel.obtain()
+        out.writeInt(presetIndex)
+        out.writeString(presetName)
+        out.writeBoolean(isWritable)
+        out.writeBoolean(isAvailable)
+        out.setDataPosition(0) // reset position of parcel before passing to constructor
+        return BluetoothHapPresetInfo.CREATOR.createFromParcel(out)
+    }
+
+    companion object {
+        private const val TEST_PRESET_INDEX = 15
+        private const val TEST_PRESET_NAME = "Test"
     }
 }
