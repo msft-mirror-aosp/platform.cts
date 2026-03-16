0e33f1a002382269e66bd51d1acbbeec386d78f6
Ömer Faruk Yılmaz <ofy@google.com>
2026-03-16 11:02:33 +0900

    Convert BluetoothGattServerCallbackTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest BluetoothGattServerCallbackTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: I715813f47c87c8d11288bb0838d062d1d335f590

--- tests/tests/bluetooth/src/android/bluetooth/cts/BluetoothGattServerCallbackTest.kt
+++ tests/tests/bluetooth/src/android/bluetooth/cts/BluetoothGattServerCallbackTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,84 +14,100 @@
  * limitations under the License.
  */
 
-package android.bluetooth.cts;
+package android.bluetooth.cts
 
-import static android.bluetooth.BluetoothGatt.GATT_SUCCESS;
-import static android.bluetooth.BluetoothProfile.STATE_CONNECTED;
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGatt.GATT_SUCCESS
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattDescriptor
+import android.bluetooth.BluetoothGattServerCallback
+import android.bluetooth.BluetoothGattService
+import android.bluetooth.BluetoothProfile.STATE_CONNECTED
+import android.bluetooth.GattOffloadSession
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.android.bluetooth.flags.Flags
+import com.android.compatibility.common.util.CddTest
+import org.junit.Assume.assumeTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnit
 
-import static org.junit.Assume.assumeTrue;
+@RunWith(AndroidJUnit4::class)
+class BluetoothGattServerCallbackTest {
+    @get:Rule val mockitoRule = MockitoJUnit.rule()
 
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
-import android.bluetooth.BluetoothGattServerCallback;
-import android.bluetooth.BluetoothGattService;
-import android.bluetooth.GattOffloadSession;
-import android.content.Context;
+    @Mock private lateinit var device: BluetoothDevice
+    @Mock private lateinit var callbacks: BluetoothGattServerCallback
+    @Mock private lateinit var gattOffloadSession: GattOffloadSession
+    @Mock private lateinit var bluetoothGattService: BluetoothGattService
+    @Mock private lateinit var bluetoothGattDescriptor: BluetoothGattDescriptor
+    @Mock private lateinit var bluetoothGattCharacteristic: BluetoothGattCharacteristic
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
+    private val context = InstrumentationRegistry.getInstrumentation().context
 
-import com.android.bluetooth.flags.Flags;
-import com.android.compatibility.common.util.CddTest;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
-
-@RunWith(AndroidJUnit4.class)
-public class BluetoothGattServerCallbackTest {
-    @Rule public final MockitoRule mockito = MockitoJUnit.rule();
-
-    @Mock private BluetoothDevice mDevice;
-    @Mock private BluetoothGattServerCallback mCallbacks;
-    @Mock private GattOffloadSession mGattOffloadSession;
-    @Mock private BluetoothGattService mBluetoothGattService;
-    @Mock private BluetoothGattDescriptor mBluetoothGattDescriptor;
-    @Mock private BluetoothGattCharacteristic mBluetoothGattCharacteristic;
-
-    private static final byte[] BYTES = new byte[] {};
-    private static final int TEST_SESSION_ID = 1;
-    private final Context mContext = InstrumentationRegistry.getInstrumentation().getContext();
-
-    @CddTest(requirements = {"7.4.3/C-2-1", "7.4.3/C-3-2"})
+    @CddTest(requirements = ["7.4.3/C-2-1", "7.4.3/C-3-2"])
     @Test
     // CTS doesn't run with a compatible remote device.
     // In order to trigger the callbacks, there is no alternative to a direct call on mock
-    @SuppressWarnings("DirectInvocationOnMock")
-    public void fakeCallbackCoverage() {
-        assumeTrue(TestUtils.isBleSupported(mContext));
-        mCallbacks.onConnectionStateChange(mDevice, STATE_CONNECTED, STATE_CONNECTED);
-        mCallbacks.onServiceAdded(GATT_SUCCESS, mBluetoothGattService);
-        mCallbacks.onCharacteristicReadRequest(mDevice, 0, 0, mBluetoothGattCharacteristic);
-        mCallbacks.onCharacteristicWriteRequest(
-                mDevice, 0, mBluetoothGattCharacteristic, true, true, 0, BYTES);
-        mCallbacks.onDescriptorReadRequest(mDevice, 0, 0, mBluetoothGattDescriptor);
-        mCallbacks.onDescriptorWriteRequest(
-                mDevice, 0, mBluetoothGattDescriptor, true, true, 0, BYTES);
-        mCallbacks.onExecuteWrite(mDevice, 0, true);
-        mCallbacks.onNotificationSent(mDevice, GATT_SUCCESS);
-        mCallbacks.onMtuChanged(mDevice, 0);
-        mCallbacks.onPhyUpdate(
-                mDevice, BluetoothDevice.PHY_LE_2M, BluetoothDevice.PHY_LE_2M, GATT_SUCCESS);
-        mCallbacks.onPhyRead(
-                mDevice, BluetoothDevice.PHY_LE_2M, BluetoothDevice.PHY_LE_2M, GATT_SUCCESS);
+    @Suppress("DirectInvocationOnMock")
+    fun fakeCallbackCoverage() {
+        assumeTrue(TestUtils.isBleSupported(context))
+        callbacks.onConnectionStateChange(device, STATE_CONNECTED, STATE_CONNECTED)
+        callbacks.onServiceAdded(GATT_SUCCESS, bluetoothGattService)
+        callbacks.onCharacteristicReadRequest(device, 0, 0, bluetoothGattCharacteristic)
+        callbacks.onCharacteristicWriteRequest(
+            device,
+            0,
+            bluetoothGattCharacteristic,
+            true,
+            true,
+            0,
+            BYTES,
+        )
+        callbacks.onDescriptorReadRequest(device, 0, 0, bluetoothGattDescriptor)
+        callbacks.onDescriptorWriteRequest(device, 0, bluetoothGattDescriptor, true, true, 0, BYTES)
+        callbacks.onExecuteWrite(device, 0, true)
+        callbacks.onNotificationSent(device, GATT_SUCCESS)
+        callbacks.onMtuChanged(device, 0)
+        callbacks.onPhyUpdate(
+            device,
+            BluetoothDevice.PHY_LE_2M,
+            BluetoothDevice.PHY_LE_2M,
+            GATT_SUCCESS,
+        )
+        callbacks.onPhyRead(
+            device,
+            BluetoothDevice.PHY_LE_2M,
+            BluetoothDevice.PHY_LE_2M,
+            GATT_SUCCESS,
+        )
 
-        mCallbacks.onSubrateChange(mDevice, BluetoothGatt.SUBRATE_MODE_OFF, GATT_SUCCESS);
-        mCallbacks.onSubrateChange(mDevice, BluetoothGatt.SUBRATE_MODE_LOW, GATT_SUCCESS);
-        mCallbacks.onSubrateChange(mDevice, BluetoothGatt.SUBRATE_MODE_BALANCED, GATT_SUCCESS);
-        mCallbacks.onSubrateChange(mDevice, BluetoothGatt.SUBRATE_MODE_HIGH, GATT_SUCCESS);
+        callbacks.onSubrateChange(device, BluetoothGatt.SUBRATE_MODE_OFF, GATT_SUCCESS)
+        callbacks.onSubrateChange(device, BluetoothGatt.SUBRATE_MODE_LOW, GATT_SUCCESS)
+        callbacks.onSubrateChange(device, BluetoothGatt.SUBRATE_MODE_BALANCED, GATT_SUCCESS)
+        callbacks.onSubrateChange(device, BluetoothGatt.SUBRATE_MODE_HIGH, GATT_SUCCESS)
 
         if (Flags.gattOffloadApi()) {
-            mCallbacks.onCharacteristicsOffloaded(
-                    mDevice, mGattOffloadSession, GattOffloadSession.STATUS_SUCCESS);
+            callbacks.onCharacteristicsOffloaded(
+                device,
+                gattOffloadSession,
+                GattOffloadSession.STATUS_SUCCESS,
+            )
 
-            mCallbacks.onCharacteristicsUnoffloaded(
-                    mDevice, TEST_SESSION_ID, GattOffloadSession.STATUS_SUCCESS);
+            callbacks.onCharacteristicsUnoffloaded(
+                device,
+                TEST_SESSION_ID,
+                GattOffloadSession.STATUS_SUCCESS,
+            )
         }
+    }
+
+    companion object {
+        private val BYTES = byteArrayOf()
+        private const val TEST_SESSION_ID = 1
     }
 }
