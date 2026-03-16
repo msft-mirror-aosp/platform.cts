8463444544f5a8669de2fe925ece3258c9a4bf86
Ömer Faruk Yılmaz <ofy@google.com>
2026-03-16 12:23:32 +0900

    Convert AdvertiseSettingsTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest AdvertiseSettingsTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: If3025aea93d35f42a29ffe54f0c58ebdbf95c764

--- tests/tests/bluetooth/src/android/bluetooth/cts/AdvertiseSettingsTest.kt
+++ tests/tests/bluetooth/src/android/bluetooth/cts/AdvertiseSettingsTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,98 +14,90 @@
  * limitations under the License.
  */
 
-package android.bluetooth.cts;
+package android.bluetooth.cts
 
-import static com.google.common.truth.Truth.assertThat;
+import android.bluetooth.le.AdvertiseSettings
+import android.bluetooth.le.AdvertisingSetParameters
+import android.os.Parcel
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.test.platform.app.InstrumentationRegistry
+import com.android.compatibility.common.util.CddTest
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assert.assertThrows
+import org.junit.Assume
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import static org.junit.Assert.assertThrows;
-
-import android.bluetooth.le.AdvertiseSettings;
-import android.bluetooth.le.AdvertisingSetParameters;
-import android.os.Parcel;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.SmallTest;
-import androidx.test.platform.app.InstrumentationRegistry;
-
-import com.android.compatibility.common.util.CddTest;
-
-import org.junit.Assume;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-/** Test for {@link AdvertiseSettings}. */
-@RunWith(AndroidJUnit4.class)
-public class AdvertiseSettingsTest {
+/** Test for [AdvertiseSettings]. */
+@RunWith(AndroidJUnit4::class)
+class AdvertiseSettingsTest {
 
     @Before
-    public void setUp() {
-        Assume.assumeTrue(
-                TestUtils.isBleSupported(
-                        InstrumentationRegistry.getInstrumentation().getTargetContext()));
+    fun setUp() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        Assume.assumeTrue(TestUtils.isBleSupported(context))
     }
 
-    @CddTest(requirements = {"7.4.3/C-2-1"})
+    @CddTest(requirements = ["7.4.3/C-2-1"])
     @SmallTest
     @Test
-    public void defaultSettings() {
-        AdvertiseSettings settings = new AdvertiseSettings.Builder().build();
-        assertThat(settings.getMode()).isEqualTo(AdvertiseSettings.ADVERTISE_MODE_LOW_POWER);
-        assertThat(settings.getTxPowerLevel())
-                .isEqualTo(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM);
-        assertThat(settings.getTimeout()).isEqualTo(0);
-        assertThat(settings.isConnectable()).isTrue();
+    fun defaultSettings() {
+        val settings = AdvertiseSettings.Builder().build()
+        assertThat(settings.mode).isEqualTo(AdvertiseSettings.ADVERTISE_MODE_LOW_POWER)
+        assertThat(settings.txPowerLevel).isEqualTo(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+        assertThat(settings.timeout).isEqualTo(0)
+        assertThat(settings.isConnectable).isTrue()
     }
 
-    @CddTest(requirements = {"7.4.3/C-2-1"})
+    @CddTest(requirements = ["7.4.3/C-2-1"])
     @SmallTest
     @Test
-    public void describeContents() {
-        AdvertiseSettings settings = new AdvertiseSettings.Builder().build();
-        assertThat(settings.describeContents()).isEqualTo(0);
+    fun describeContents() {
+        val settings = AdvertiseSettings.Builder().build()
+        assertThat(settings.describeContents()).isEqualTo(0)
     }
 
-    @CddTest(requirements = {"7.4.3/C-2-1"})
+    @CddTest(requirements = ["7.4.3/C-2-1"])
     @SmallTest
     @Test
-    public void readWriteParcel() {
-        final int timeoutMillis = 60 * 1000;
-        Parcel parcel = Parcel.obtain();
-        AdvertiseSettings settings =
-                new AdvertiseSettings.Builder()
-                        .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
-                        .setConnectable(false)
-                        .setDiscoverable(false)
-                        .setTimeout(timeoutMillis)
-                        .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
-                        .setOwnAddressType(AdvertisingSetParameters.ADDRESS_TYPE_DEFAULT)
-                        .build();
-        settings.writeToParcel(parcel, 0);
-        parcel.setDataPosition(0);
-        AdvertiseSettings settingsFromParcel = AdvertiseSettings.CREATOR.createFromParcel(parcel);
-        assertThat(settingsFromParcel.getMode())
-                .isEqualTo(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY);
-        assertThat(settingsFromParcel.getTxPowerLevel())
-                .isEqualTo(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM);
-        assertThat(settingsFromParcel.getTimeout()).isEqualTo(timeoutMillis);
-        assertThat(settings.isConnectable()).isFalse();
-        assertThat(settings.isDiscoverable()).isFalse();
-        assertThat(settings.getOwnAddressType())
-                .isEqualTo(AdvertisingSetParameters.ADDRESS_TYPE_DEFAULT);
+    fun readWriteParcel() {
+        val timeoutMillis = 60 * 1000
+        val parcel = Parcel.obtain()
+        val settings =
+            AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
+                .setConnectable(false)
+                .setDiscoverable(false)
+                .setTimeout(timeoutMillis)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+                .setOwnAddressType(AdvertisingSetParameters.ADDRESS_TYPE_DEFAULT)
+                .build()
+        settings.writeToParcel(parcel, 0)
+        parcel.setDataPosition(0)
+        val settingsFromParcel = AdvertiseSettings.CREATOR.createFromParcel(parcel)
+        assertThat(settingsFromParcel.mode).isEqualTo(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
+        assertThat(settingsFromParcel.txPowerLevel)
+            .isEqualTo(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+        assertThat(settingsFromParcel.timeout).isEqualTo(timeoutMillis)
+        assertThat(settings.isConnectable).isFalse()
+        assertThat(settings.isDiscoverable).isFalse()
+        assertThat(settings.ownAddressType).isEqualTo(AdvertisingSetParameters.ADDRESS_TYPE_DEFAULT)
     }
 
-    @CddTest(requirements = {"7.4.3/C-2-1"})
+    @CddTest(requirements = ["7.4.3/C-2-1"])
     @SmallTest
     @Test
-    public void illegalTimeout() {
-        AdvertiseSettings.Builder builder = new AdvertiseSettings.Builder();
-        builder.setTimeout(0).build();
-        builder.setTimeout(180 * 1000).build();
+    fun illegalTimeout() {
+        val builder = AdvertiseSettings.Builder()
+        builder.setTimeout(0).build()
+        builder.setTimeout(180 * 1000).build()
         // Maximum timeout is 3 minutes.
-        assertThrows(
-                IllegalArgumentException.class, () -> builder.setTimeout(180 * 1000 + 1).build());
+        assertThrows(IllegalArgumentException::class.java) {
+            builder.setTimeout(180 * 1000 + 1).build()
+        }
         // Negative time out is not allowed.
-        assertThrows(IllegalArgumentException.class, () -> builder.setTimeout(-1).build());
+        assertThrows(IllegalArgumentException::class.java) { builder.setTimeout(-1).build() }
     }
 }
