3b6b08c42dc6c526a2d3fb371ddeb1c7c08fb64a
brycelee <brycelee@google.com>
2026-03-27 21:48:35 +0000

    Ignore PersonalContext workflow tests.
    Temporarily disable these tests until the test logic is more
    stable.
    
    Fixes: 496398871
    Bug: 497020098
    Test: atest InsightWorkflowTest HintWorkflowTest
    Flag: android.service.personalcontext.enable_personal_context_service
    Change-Id: I053beb99fdc0f1f5d6813c369a09a3998ea20c38

--- tests/tests/personalcontext/src/android/service/personalcontext/cts/workflows/InsightWorkflowTest.java
+++ tests/tests/personalcontext/src/android/service/personalcontext/cts/workflows/InsightWorkflowTest.java
@@ -53,6 +53,7 @@ import com.android.compatibility.common.util.ApiTest;
 import com.android.compatibility.common.util.RequiredServiceRule;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -64,6 +65,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+// TODO(b/497020098): Re-enable.
+@Ignore("b/496398871")
 @RequiresFlagsEnabled(Flags.FLAG_ENABLE_PERSONAL_CONTEXT_SERVICE)
 @RunWith(AndroidJUnit4.class)
 public class InsightWorkflowTest {
