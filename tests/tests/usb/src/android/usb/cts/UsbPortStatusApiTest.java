e697ea4fcecb76906d2752a8622d64fe248efcec
Oleksandr Nikitin <oleksandr.nikitin@bmw.de>
2026-04-02 16:29:40 +0200

    Skip UsbPort tests for devices without usb
    If neither USB host nor USB accessory are declared, there is no
    UsbService running, therefore no tests can use it to run.
    
    Bug: 498804502
    Change-Id: If649b53859b6ae059b7425fe3a0ffa702f992e74

--- tests/tests/usb/src/android/usb/cts/UsbPortStatusApiTest.java
+++ tests/tests/usb/src/android/usb/cts/UsbPortStatusApiTest.java
@@ -96,6 +96,10 @@ public class UsbPortStatusApiTest {
         PackageManager pm = mContext.getPackageManager();
         MockitoAnnotations.initMocks(this);
 
+        boolean hasUsbHost = pm.hasSystemFeature(PackageManager.FEATURE_USB_HOST);
+        boolean hasUsbAccessory =
+            pm.hasSystemFeature(PackageManager.FEATURE_USB_ACCESSORY);
+        Assume.assumeTrue(hasUsbHost || hasUsbAccessory);
         Assert.assertNotNull(mUsbManagerSys);
         Assert.assertNotNull(mUsbManagerMock =
                 new UsbManager(mContext, mMockUsbService));
