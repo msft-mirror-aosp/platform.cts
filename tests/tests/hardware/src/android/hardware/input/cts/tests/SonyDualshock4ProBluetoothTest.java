ad92b23af530edc78ee5006bfad6127913315859
Siarhei Vishniakou <svv@google.com>
2026-06-30 14:24:51 -0700

    Disable Sony motion tests
    They got broken by kernel changes that haven't yet propagated to all
    devices. While the kernel changes are being propagated, tests are
    passing on some devices and not others.
    
    To reduce the churn, disable the tests on older branches.
    
    The relevant kernel commit is
    e9143268d259d98e111a649affa061acb8e13c5b.
    
    Links:
    https://github.com/torvalds/linux/commit/e9143268d259d98e111a649affa061acb8e13c5b
    https://git.kernel.org/pub/scm/linux/kernel/git/hid/hid.git/commit/?id=e9143268d259
    
    Bug: 519587062
    Test: none
    Flag: EXEMPT TEST_ONLY
    Change-Id: I5f2af781cc0fecd6b1d6c75a472ac4345b20e594

--- tests/tests/hardware/src/android/hardware/input/cts/tests/SonyDualshock4ProBluetoothTest.java
+++ tests/tests/hardware/src/android/hardware/input/cts/tests/SonyDualshock4ProBluetoothTest.java
@@ -25,6 +25,7 @@ import androidx.test.filters.SmallTest;
 
 import com.android.cts.kernelinfo.KernelInfo;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -43,6 +44,7 @@ public class SonyDualshock4ProBluetoothTest extends InputHidTestCase {
     }
 
     @Test
+    @Ignore("b/243570601")
     public void testAllMotions() {
         testInputEvents(R.raw.sony_dualshock4_bluetooth_motioneventtests);
     }
